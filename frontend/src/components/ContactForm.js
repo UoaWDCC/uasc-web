78056f634565125b698f7232210488bcb47fbd4b
atharvaarankalle <atharvanz@gmail.com>
2023-04-10 13:13:28 +1200

    feat: Added contact form to page

--- frontend/src/components/ContactForm.js
+++ frontend/src/components/ContactForm.js
@@ -1,6 +1,28 @@
 const ContactForm = () => {
+
     return (
-        <h1>Form</h1>
+        <form>
+            <div>
+                <input 
+                    type="text"
+                    id="name"
+                    placeholder="Name"
+                    required
+                />
+            </div>
+            <div>
+                <input 
+                    type="email"
+                    id="email" 
+                    placeholder="Email Address"
+                
+                />
+            </div>
+            <div>
+                <textarea id="message" rows="5" placeholder="Message"></textarea>
+            </div>
+            <button type="submit">Submit</button>
+        </form>
     );
 };
 
