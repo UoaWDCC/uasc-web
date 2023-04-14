0451cf33317b3d8b001e48af46901f21128e503a
Tony <tyin363@aucklanduni.ac.nz>
2023-04-14 13:33:51 +1200

    Starting creating template

--- frontend/src/pages/Profile.js
+++ frontend/src/pages/Profile.js
@@ -2,6 +2,14 @@ const Profile = () => {
   return (
     <div>
       <h1>Profile page</h1>
+      <div>
+        <h2>Name</h2>
+        <h2>Email</h2>
+        <h2>Membership Type</h2>
+      </div>
+      <div>
+        <h2>Current Bookings</h2>
+      </div>
     </div>
   );
 };
