04e30ba4541f7ff5adb3a5c731678ca6c3d5c065
Mitchell <mitchellwong833@gmail.com>
2023-04-23 19:06:34 +1200

    Changed Event component to use MUI + Firebase auth

--- frontend/src/components/Event.js
+++ frontend/src/components/Event.js
@@ -1,22 +1,52 @@
 import React from "react";
+import {
+  Card,
+  CardHeader,
+  CardMedia,
+  CardContent,
+  Typography,
+  Button,
+} from "@mui/material";
+import { useNavigate } from "react-router-dom";
+import app from "../firebase";
+import "firebase/compat/firestore";
+import "firebase/compat/auth";
 
 const Event = (props) => {
-  const handleSignUp = (eventId) => {
-    console.log("Signed up");
+  const navigate = useNavigate();
+
+  const handleSignUp = () => {
+    const user = app.auth().currentUser;
+    if (user) {
+      navigate(`/events/${props.id}/signup`);
+    } else {
+      navigate("/login");
+    }
   };
 
   return (
-    <>
-      <div>
-        <h1>
-          {props.title}
-          <span> {props.date}</span>
-        </h1>
-        <img src={props.img} alt="test"></img>
-        <p>{props.desc}</p>
-        <button onClick={() => handleSignUp(props.id)}>Sign Up</button>
-      </div>
-    </>
+    <Card>
+      <CardHeader title={props.title} subheader={props.date} />
+      {props.img && (
+        <CardMedia
+          component="img"
+          image={props.img}
+          alt={props.title}
+          style={{
+            height: "200px",
+            width: "200px",
+          }}
+        />
+      )}
+      <CardContent>
+        <Typography variant="body2" color="textSecondary" component="p">
+          {props.desc}
+        </Typography>
+        <Button variant="contained" color="primary" onClick={handleSignUp}>
+          Sign Up
+        </Button>
+      </CardContent>
+    </Card>
   );
 };
 
