5166f416362080ed60cb7766411786aab423abf3
eli384 <88353612+eli384@users.noreply.github.com>
2023-04-27 20:47:54 +1200

    Adding React to SignupForm

--- frontend/src/components/SignUpForm.js
+++ frontend/src/components/SignUpForm.js
@@ -1,36 +1,70 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { auth } from '../firebase';
+import { useNavigate } from 'react-router-dom';
 
-const SignUpForm = () => { 
-    const handleSubmit = (event) => { 
-        event.preventDefault();
-        console.log('Form Submitted');
+const SignUpForm = () => {
+  const [formState, setFormState] = useState({
+    username: '',
+    email: '',
+    password: '',
+    confirmPassword: '',
+  });
+
+  const navigate = useNavigate();
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    const { username, email, password, confirmPassword } = formState;
+    if (password !== confirmPassword) {
+      alert('Passwords do not match');
+      return;
     }
-    return ( 
-        <form onSubmit={handleSubmit}>
-            <div> 
-                <label htmlFor='username'>Username:</label>
-                <input type='text' id='username' name='username' required /> 
-            </div> 
-            <div> 
-                <label htmlFor='email'>Email:</label>
-                <input type='email' id='email' name='email' required /> 
-            </div> 
-            <div> 
-                <label htmlFor='password'>Password:</label>
-                <input type='password' id='password' name='password' required /> 
-            </div> 
-            <div> 
-                <label htmlFor='confirmPassword'>Confirm Password:</label>
-                <input
-                    type='password'
-                    id='confirmPassword'
-                    name='confirmPassword'
-                    required
-                    />
-            </div>
-            <button type='submit'>Submit</button>
-        </form>
-    )
-}
+    try {
+      const { user } = await auth.createUserWithEmailAndPassword(email, password);
+      await user.updateProfile({ displayName: username });
+      console.log('User created successfully');
+      navigate('/success'); // Replace '/success' with the URL of the next page
+    } catch (error) {
+      console.error('Error creating user:', error);
+      alert(error.message);
+    }
+  };
+
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    setFormState((prevState) => ({
+      ...prevState,
+      [name]: value,
+    }));
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <div>
+        <label htmlFor="username">Username:</label>
+        <input type="text" id="username" name="username" required onChange={handleChange} />
+      </div>
+      <div>
+        <label htmlFor="email">Email:</label>
+        <input type="email" id="email" name="email" required onChange={handleChange} />
+      </div>
+      <div>
+        <label htmlFor="password">Password:</label>
+        <input type="password" id="password" name="password" required onChange={handleChange} />
+      </div>
+      <div>
+        <label htmlFor="confirmPassword">Confirm Password:</label>
+        <input
+          type="password"
+          id="confirmPassword"
+          name="confirmPassword"
+          required
+          onChange={handleChange}
+        />
+      </div>
+      <button type="submit">Submit</button>
+    </form>
+  );
+};
 
 export default SignUpForm;
