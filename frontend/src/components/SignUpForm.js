3bce26b9aec0ffa5fd7b140f90221dc5e9723328
eli384 <88353612+eli384@users.noreply.github.com>
2023-04-27 20:33:20 +1200

    Revert "adding a bits of backend in"
    This reverts commit 34ac546a3641faaae0a086eb3b87b477eb877472.

--- frontend/src/components/SignUpForm.js
+++ frontend/src/components/SignUpForm.js
@@ -1,29 +1,10 @@
-//TODO: Ask Bill if it's alg to make a pull request even if I am getting errors with the firebase
-// should I make a request even if I haven't completed everything with the backend yet?
-import React, { useState } from 'react';
-import { auth } from '../firebase';
+import React from 'react';
 
-const SignUpForm = () => {
-    const [formState, setFormState] = useState({username: '', email: '', password: '', confirmPassword: '',});
-
-    const handleSubmit = async (event) => { 
+const SignUpForm = () => { 
+    const handleSubmit = (event) => { 
         event.preventDefault();
-        const {username, email, password, confirmPassword} = formState;
-        if(password != confirmPassword){ 
-            alert("Passwords do not match");
-            return;
-        }
-        try { 
-            const {user} = await auth.createUserWithEmailAndPassword(email, password);
-            await user.updateProfile({displayName: username});
-            console.log("User created successfully");
-            //TODO: go to the successive page but idk what the next page is 
-        } catch (error) {
-            console.error("Error creating user:", error);
-            //TODO: make a pop up error message to the user
-        }
+        console.log('Form Submitted');
     }
-
     return ( 
         <form onSubmit={handleSubmit}>
             <div> 
