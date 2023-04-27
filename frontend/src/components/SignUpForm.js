34ac546a3641faaae0a086eb3b87b477eb877472
eli384 <88353612+eli384@users.noreply.github.com>
2023-04-27 19:19:55 +1200

    adding a bits of backend in

--- frontend/src/components/SignUpForm.js
+++ frontend/src/components/SignUpForm.js
@@ -1,10 +1,29 @@
-import React from 'react';
+//TODO: Ask Bill if it's alg to make a pull request even if I am getting errors with the firebase
+// should I make a request even if I haven't completed everything with the backend yet?
+import React, { useState } from 'react';
+import { auth } from '../firebase';
 
-const SignUpForm = () => { 
-    const handleSubmit = (event) => { 
+const SignUpForm = () => {
+    const [formState, setFormState] = useState({username: '', email: '', password: '', confirmPassword: '',});
+
+    const handleSubmit = async (event) => { 
         event.preventDefault();
-        console.log('Form Submitted');
+        const {username, email, password, confirmPassword} = formState;
+        if(password != confirmPassword){ 
+            alert("Passwords do not match");
+            return;
+        }
+        try { 
+            const {user} = await auth.createUserWithEmailAndPassword(email, password);
+            await user.updateProfile({displayName: username});
+            console.log("User created successfully");
+            //TODO: go to the successive page but idk what the next page is 
+        } catch (error) {
+            console.error("Error creating user:", error);
+            //TODO: make a pop up error message to the user
+        }
     }
+
     return ( 
         <form onSubmit={handleSubmit}>
             <div> 
