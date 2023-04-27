a6d0e3df7558caa9736efb646a3551745358f61f
eli384 <88353612+eli384@users.noreply.github.com>
2023-04-27 20:47:54 +1200

    Adding React to SignupForm

--- frontend/src/firebase.js
+++ frontend/src/firebase.js
@@ -1,5 +1,7 @@
 // setup firebase
 import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
   apiKey: "AIzaSyAVkhnuTBofnvjRXzwB9YmPKzkZm3Pa920",
@@ -11,3 +13,7 @@ const firebaseConfig = {
 };
 
 const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const db = getFirestore(app);
+
+export { auth, db };
