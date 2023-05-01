38017170126a0b3dfe270a7b8f7167cea0fea373
Bill W <86991521+BW82262@users.noreply.github.com>
2023-05-01 15:23:24 +1200

    Installed Material UI Library
    Material UI library along with default theme sheet created.
    The theme for the main styling is yet to be confirmed.

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,8 +1,20 @@
+import logo from "../assets/2023_logo1-768x262-uasc.png";
+import { Typography } from "@mui/material";
+
 const Home = () => {
 	return (
 		<div>
-			<h1> Home Page</h1>
-			<div></div>
+			<img src={logo} alt="uasc-logo-large" />
+			<Typography
+				variant="h1"
+				sx={{
+					textTransform: "uppercase",
+					fontWeight: "bold	",
+					color: "white",
+				}}
+			>
+				University of Auckland Snowsports Club
+			</Typography>
 		</div>
 	);
 };
