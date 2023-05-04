d8008592f1038890e2c7a23170778c9db778785b
eli384 <88353612+edl21@users.noreply.github.com>
2023-05-04 19:25:29 +1200

    Fixing up some errors

--- frontend/src/firebase.js
+++ frontend/src/firebase.js
@@ -1,7 +1,7 @@
 // setup firebase
-import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
-import { getFirestore } from "firebase/firestore";
+import { initializeApp } from '@firebase/app';
+import { getAuth } from '@firebase/auth';
+import { getFirestore } from '@firebase/firestore';
 
 const firebaseConfig = {
   apiKey: "AIzaSyAVkhnuTBofnvjRXzwB9YmPKzkZm3Pa920",
