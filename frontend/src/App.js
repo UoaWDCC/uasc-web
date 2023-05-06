0899d25ca03ab37140bc5b87d0ef506f31da4b31
Mitchell <mitchellwong833@gmail.com>
2023-05-07 01:34:53 +1200

    feat: Implemented booking component which renders DatePicker MUI component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,39 +7,35 @@ import Login from "./pages/Login";
 import Events from "./pages/Events";
 import Contact from "./pages/Contact";
 import Checkout from "./pages/Checkout";
+import Booking from "./pages/Booking";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import { ThemeProvider } from "@mui/material";
 import theme from "./theme";
 
 function App() {
-	return (
-		<div style={{ backgroundColor: "darkgray" }}>
-			<ThemeProvider theme={theme}>
-				<Router>
-					<div className="App">
-						<Navbar />
-						<div className="content">
-							<Routes>
-								<Route path="/" element={<Home />} />
-								<Route path="/about" element={<About />} />
-								<Route path="/events" element={<Events />} />
-								<Route path="/contact" element={<Contact />} />
-								<Route
-									path="/register"
-									element={<Register />}
-								/>
-								<Route path="/login" element={<Login />} />
-								<Route
-									path="/checkout"
-									element={<Checkout />}
-								/>
-							</Routes>
-						</div>
-					</div>
-				</Router>
-			</ThemeProvider>
-		</div>
-	);
+  return (
+    <div style={{ backgroundColor: "darkgray" }}>
+      <ThemeProvider theme={theme}>
+        <Router>
+          <div className="App">
+            <Navbar />
+            <div className="content">
+              <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/about" element={<About />} />
+                <Route path="/events" element={<Events />} />
+                <Route path="/contact" element={<Contact />} />
+                <Route path="/register" element={<Register />} />
+                <Route path="/login" element={<Login />} />
+                <Route path="/checkout" element={<Checkout />} />
+                <Route path="/booking" element={<Booking />} />
+              </Routes>
+            </div>
+          </div>
+        </Router>
+      </ThemeProvider>
+    </div>
+  );
 }
 
 export default App;
