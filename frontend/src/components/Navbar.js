b00eb37424232d5c53d164c32baa9537c35a762a
selin <selinakkaya345@gmail.com>
2023-07-03 16:53:24 +1200

    linked the other web pages together on nav bar

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -12,22 +12,22 @@ const Navbar = () => {
 		<div id="two">
       <ul>
         <li>
-          <a href="#home">Home</a>
+          <a href="/home">Home</a>
         </li>
         <li>
-          <a href="#about">About</a>
+          <a href="/about">About</a>
         </li>
         <li>
-          <a href="#events">Events</a>
+          <a href="/events">Events</a>
         </li>
         <li>
-          <a href="#contact">Contact</a>
+          <a href="/contact">Contact</a>
         </li>
         <li>
-          <a href="#register">Register</a>
+          <a href="/register">Register</a>
         </li>
         <li>
-          <a href="#login">Login</a>
+          <a href="/login">Login</a>
         </li>
       </ul>
 	</div>
