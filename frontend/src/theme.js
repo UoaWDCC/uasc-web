16d8765e5febab31f69d6aa8f933fd479db72c97
selin <selinakkaya345@gmail.com>
2023-07-04 14:32:04 +1200

    added the colours an d font to customise the material U I

--- frontend/src/theme.js
+++ frontend/src/theme.js
@@ -4,15 +4,22 @@ export default createTheme({
 	palette: {
 		common: {
 			black: "#000",
+			grey: "#A8ADB0",
+			darkGrey: "#474747",
 			white: "#fff",
 		},
 		primary: {
-			main: "#2f3336",
-			secondary: "#2f3336cc",
-			disabled: "#2f333680",
-		},
-		secondary: {
-			main: "#ff9b00",
-		},
+			main: "#81C7EB", // Blue color 1
+			secondary: "#54B3E5", // Blue color 2
+			tertiary: "#51A1DD", // Blue color 3
+			quaternary: "#457CC4", // Blue color 4
+		  },
+		  secondary: {
+			main: "#BD8FFF", // Purple color 1
+			secondary: "#8966FF", // Purple color 2
+		  },
 	},
+	typography: {
+		fontFamily: "Rubix Medium",
+	  },
 });
