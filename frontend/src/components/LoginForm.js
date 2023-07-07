59283fbf0b4ce7b311fb19fe986552d9c30e61be
atharvaarankalle <atharvanz@gmail.com>
2023-07-07 13:16:22 +1200

    feat: Added initial Firebase authentication

--- frontend/src/components/LoginForm.js
+++ frontend/src/components/LoginForm.js
@@ -1,18 +1,34 @@
+import React, { useState } from "react";
 import { Button, FormControl, FormLabel, TextField } from "@mui/material";
+import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
 
 const LoginForm = () => {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
 
     const handleSubmit = () => {
-        console.log("Form submitted");
+        const auth = getAuth();
+
+        signInWithEmailAndPassword(auth, email, password)
+            .then((userCredential) => {
+                // The user is successfully signed in
+                const user = userCredential.user;
+                console.log(user);
+            })
+            .catch((error) => {
+                const errorCode = error.code;
+                const errorMessage = error.message;
+                console.log(errorMessage);
+            });
     };
 
     return (
         <form onSubmit={handleSubmit}>
             <FormControl>
-                <FormLabel style={{ "textAlign": "left" }}>Username</FormLabel>
-                <TextField required></TextField>
+                <FormLabel style={{ "textAlign": "left" }}>Email</FormLabel>
+                <TextField id="email" onChange={e => setEmail(e.target.value)} required></TextField>
                 <FormLabel style={{ "textAlign": "left" }}>Password</FormLabel>
-                <TextField required></TextField>
+                <TextField id="password" onChange={e => setPassword(e.target.value)} required></TextField>
                 <Button type="submit" variant="contained">Submit</Button>
             </FormControl>
         </form>
