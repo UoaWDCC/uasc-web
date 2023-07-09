860dabfd4fd99e1af593be7cc6906a02bd469afd
Tony Yin <tony.yin2002@gmail.com>
2023-07-09 17:37:08 +1200

    Added some basic styling

--- frontend/src/pages/Profile.js
+++ frontend/src/pages/Profile.js
@@ -5,14 +5,29 @@ const Profile = () => {
   return (
     <div className="profilePage">
       <h1>Profile page</h1>
-      <div>
-        <h2>Name</h2>
-        <h2>Email</h2>
-        <h2>Membership Type</h2>
-      </div>
-      <div>
-        <h2>Current Bookings</h2>
-        <DateCalendar />
+      <div className="profileSection">
+        <div className="userSection">
+          <div>
+            <h2>NAME</h2>
+            <p>John Doe</p>
+          </div>
+          <div className="userInfo">
+            <h1>USER INFO</h1>
+            <hr></hr>
+            <h2>EMAIL</h2>
+            <p>JohnDoe@gmail.com</p>
+            <h2>MEMBERSHIP</h2>
+            <p>UoA Student</p>
+            <h2>OTHER DETAILS</h2>
+          </div>
+        </div>
+        <div className="bookingSection">
+          <h1>BOOKINGS</h1>
+          <hr></hr>
+          <DateCalendar />
+          <h1>BOOKING HISTORY</h1>
+          <hr></hr>
+        </div>
       </div>
     </div>
   );
