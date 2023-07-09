e0e67e93901d80c079cd879c4a27528330dcf2c3
Tony Yin <tony.yin2002@gmail.com>
2023-07-09 17:39:22 +1200

    Fix conflict

--- frontend/src/pages/Profile.js
+++ frontend/src/pages/Profile.js
@@ -1,24 +1,7 @@
 import { DateCalendar } from "@mui/x-date-pickers";
 import "../styles/Profile.css";
-import { db } from "../firebase";
-import React, { useEffect, useState } from "react";
-import { getDoc, doc } from "firebase/firestore";
 
 const Profile = () => {
-  const [userData, setUserData] = useState(null);
-
-  useEffect(() => {
-    getDoc(doc(db, "users", "lVsOjAp06AfD6atT8bnrVEpcdcg2"))
-      .then((doc) => {
-        if (doc.exists()) {
-          setUserData(doc.data());
-        } else {
-          console.log("Doc does not exist");
-        }
-      })
-      .catch(console.error);
-  }, []);
-
   return (
     <div className="profilePage">
       <h1>Profile page</h1>
@@ -31,7 +14,7 @@ const Profile = () => {
           <div className="userInfo">
             <h1>USER INFO</h1>
             <hr></hr>
-            <h2>EMAIL</h2>
+            <h2 className="">EMAIL</h2>
             <p>JohnDoe@gmail.com</p>
             <h2>MEMBERSHIP</h2>
             <p>UoA Student</p>
@@ -45,18 +28,6 @@ const Profile = () => {
           <h1>BOOKING HISTORY</h1>
           <hr></hr>
         </div>
-      {userData && (
-        <div>
-          <h2>
-            Name: {userData.first_name} {userData.last_name}
-          </h2>
-          <h2>Email: {userData.email}</h2>
-          <h2>Membership Type: {userData.membership}</h2>
-        </div>
-      )}
-      <div>
-        <h2>Current Bookings</h2>
-        <DateCalendar />
       </div>
     </div>
   );
