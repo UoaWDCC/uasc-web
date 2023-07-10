e1376c7eb1eaa0a85731b9e2406523fd0fb87962
atharvaarankalle <atharvanz@gmail.com>
2023-07-10 12:48:12 +1200

    feat: Added success and error messages for login

--- frontend/src/components/LoginForm.js
+++ frontend/src/components/LoginForm.js
@@ -1,11 +1,13 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import { Button, FormControl, FormLabel, TextField } from "@mui/material";
+import { Alert, Button, FormControl, FormLabel, TextField } from "@mui/material";
 import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
 
 const LoginForm = () => {
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
+    const [loginSuccessful, setLoginSuccessful] = useState(false);
+    const [loginErrorMessage, setLoginErrorMessage] = useState("");
 
     const navigate = useNavigate();
 
@@ -14,14 +16,31 @@ const LoginForm = () => {
         const auth = getAuth();
         signInWithEmailAndPassword(auth, email, password)
             .then((userCredential) => {
+                setLoginSuccessful(true);
                 // The user is successfully signed in
                 const user = userCredential.user;
-                navigate("/profile");
+                
+                setTimeout(() => {
+                    setLoginSuccessful(false);
+                    navigate("/profile");
+                }, 2000);
             })
             .catch((error) => {
-                const errorCode = error.code;
-                const errorMessage = error.message;
-                console.log(errorMessage);
+                console.log(error.code)
+                switch (error.code) {
+                    case "auth/wrong-password":
+                        setLoginErrorMessage("Incorrect password");
+                        setTimeout(() => {
+                            setLoginErrorMessage("");
+                        }, 2000);
+                        break;
+                    case "auth/user-not-found":
+                        setLoginErrorMessage("User not found");
+                        setTimeout(() => {
+                            setLoginErrorMessage("");
+                        }, 2000);
+                        break;
+                }
             });
     };
 
@@ -31,8 +50,10 @@ const LoginForm = () => {
                 <FormLabel style={{ "textAlign": "left" }}>Email</FormLabel>
                 <TextField id="email" onChange={e => setEmail(e.target.value)} required></TextField>
                 <FormLabel style={{ "textAlign": "left" }}>Password</FormLabel>
-                <TextField id="password" onChange={e => setPassword(e.target.value)} required></TextField>
-                <Button type="submit" variant="contained">Submit</Button>
+                <TextField type="password" id="password" onChange={e => setPassword(e.target.value)} required></TextField>
+                <Button type="submit" variant="contained" disabled={loginSuccessful} sx={{marginTop: "1vh"}}>Submit</Button>
+                {loginSuccessful ? <Alert severity="success" sx={{marginTop: "1vh"}}>Login Successful!</Alert> : null}
+                {loginErrorMessage !== "" ? <Alert severity="error" sx={{marginTop: "1vh"}}>{loginErrorMessage}</Alert> : null}
             </FormControl>
         </form>
     );
