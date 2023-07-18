a49a99a2f6a225fe60bab22f7e7941e0e91e4174
eli384 <88353612+edl21@users.noreply.github.com>
2023-07-18 17:15:07 +1200

    removing handleClick

--- frontend/src/components/SignUpForm.js
+++ frontend/src/components/SignUpForm.js
@@ -55,10 +55,6 @@ const SignUpForm = () => {
 
   const [open, setOpen] = useState(false);
 
-  const handleClick = () => {
-    setOpen(true);
-  };
-
   const handleClose = (event, reason) => {
     if (reason === "clickaway") {
       return;
