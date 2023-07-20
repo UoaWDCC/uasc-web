19ddf792a08b0da688cde972412ab80abf90bcd2
Bill W <86991521+BW82262@users.noreply.github.com>
2023-07-20 17:22:53 +1200

    Added new rules and fixed up pages to adhere to new ESLint rules

--- frontend/src/components/ContactForm.js
+++ frontend/src/components/ContactForm.js
@@ -1,3 +1,4 @@
+import React from 'react';
 import { Button, FormControl, FormLabel, TextField } from '@mui/material';
 
 const ContactForm = () => {
--- frontend/src/pages/Contact.js
+++ frontend/src/pages/Contact.js
@@ -1,3 +1,4 @@
+import React from 'react';
 import ContactForm from "../components/ContactForm";
 
 const Contact = () => {
