c3257aeb28550190186c4cab384fda11f4db0437
Mitchell <mitchellwong833@gmail.com>
2023-08-02 11:49:14 +1200

    Merge main pt 2

--- frontend/src/components/BookingForm.js
+++ frontend/src/components/BookingForm.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React, { useEffect, useState } from "react";
 import { FormControl, FormLabel, Button } from "@mui/material";
 import { DatePicker } from "@mui/x-date-pickers";
@@ -55,44 +54,6 @@ const BookingForm = () => {
         availabilities.set(dateKey, spotsTaken);
 
         currDate.setDate(currDate.getDate() + 1);
-=======
-import React, { useState } from "react"
-import { FormControl, FormLabel, Button } from "@mui/material"
-import { DatePicker } from "@mui/x-date-pickers"
-import dayjs from "dayjs"
-
-const BookingForm = () => {
-  const [selectedStartDate, setSelectedStartDate] = useState(dayjs(Date.now()))
-  const [selectedEndDate, setSelectedEndDate] = useState(null)
-
-  // dummy data to test, kinda just assuming that the booking documents
-  // in firestore will contain a start and end property
-  const existingBookedDateRanges = [
-    { start: "2023-07-21", end: "2023-07-23" },
-    { start: "2023-07-26", end: "2023-07-27" },
-    { start: "2023-07-29", end: "2023-07-31" },
-  ]
-
-  const isDateWithinExistingBookedDates = (dateToCheck) => {
-    for (const existingBookedRange of existingBookedDateRanges) {
-      const existingStart = new Date(existingBookedRange.start)
-      const existingEnd = new Date(existingBookedRange.end)
-
-      if (dateToCheck >= existingStart && dateToCheck <= existingEnd) {
-        return true
-      }
-    }
-    return false
-  }
-
-  const doesEndDateIncludeExistingBookedDates = (newEndDate) => {
-    for (const existingBookedRange of existingBookedDateRanges) {
-      const existingStart = new Date(existingBookedRange.start)
-      const existingEnd = new Date(existingBookedRange.end)
-
-      if (selectedStartDate <= existingStart && newEndDate >= existingEnd) {
-        return true
->>>>>>> main
       }
     });
 
@@ -106,7 +67,6 @@ const BookingForm = () => {
     if (spotsLeft <= 0) {
       return true;
     }
-<<<<<<< HEAD
 
     return false;
   };
@@ -155,57 +115,10 @@ const BookingForm = () => {
     if (!selectedCheckOutDate) {
       alert("Please select an end date before submitting"); // will replace with mui modal or something later
       return;
-=======
-    return false
-  }
-
-  const isStartDateInvalid = (date) => {
-    const startDateString = dayjs(date).format("YYYY-MM-DD") // needed to convert using dayjs otherwise disabled dates would be 1 day ahead
-    const startDate = new Date(startDateString)
-    return isDateWithinExistingBookedDates(startDate)
-  }
-
-  const isEndDateInvalid = (date) => {
-    const endDateString = dayjs(date).format("YYYY-MM-DD")
-    const endDate = new Date(endDateString)
-
-    if (endDate < selectedStartDate) {
-      return true
-    }
-
-    return (
-      isDateWithinExistingBookedDates(endDate) ||
-      doesEndDateIncludeExistingBookedDates(endDate)
-    )
-  }
-
-  const handleChangeStartDate = (startDate) => {
-    if (startDate > selectedEndDate) {
-      setSelectedEndDate(null)
-    }
-    setSelectedStartDate(startDate)
-  }
-
-  const handleChangeEndDate = (endDate) => {
-    setSelectedEndDate(endDate)
-  }
-
-  const handleSubmit = (e) => {
-    e.preventDefault()
-
-    if (!selectedEndDate) {
-      alert("Please select an end date before submitting") // will replace with mui modal or something later
-      return
->>>>>>> main
     }
     console.log(
-<<<<<<< HEAD
       `Selected Date Range:\n${selectedCheckInDate} - \n${selectedCheckOutDate}`
     );
-=======
-      `Selected Date Range:\n${selectedStartDate.$d} - \n${selectedEndDate.$d}`
-    )
->>>>>>> main
     // need to add booking to firestore
   }
 
