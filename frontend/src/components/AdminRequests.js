67b3cdb7313a358d0098600ea090d6f078d37baa
svimi <zmus754@aucklanduni.ac.nz>
2023-08-02 21:12:56 +1200

    added RequestBlock component
    ive created a request block component to render any data we want to fetch from firebase. at the moment it only works with dummy data ive used in the the AdminRequests file for example

--- frontend/src/components/AdminRequests.js
+++ frontend/src/components/AdminRequests.js
@@ -1,45 +1,68 @@
-import { Grid, Typography, Box, Divider, Paper } from "@mui/material"
+import { Grid, Typography, Divider, Paper } from "@mui/material"
+import RequestBlock from "./RequestBlock"
+import { useState, useEffect } from "react"
 
 const AdminDashboardRequest = () => {
   const gridItemStyle = {
-    gridItem1: {
+    requestBackground: {
       borderRadius: 15, // Change the value as needed
       backgroundColor: "gray", // Change the background color as needed
       padding: "20px", // Add padding as needed
       boxShadow: "none",
     },
-
-    gridItem2: {
-      borderRadius: 15, // Change the value as needed
-      backgroundColor: "pink", // Change the background color as needed
-      padding: "5px", // Add padding as needed
-      boxShadow: "none",
-      marginBottom: "5px",
-    },
-    gridItem3: {
-      borderRadius: 15, // Change the value as needed
-      backgroundColor: "lightblue", // Change the background color as needed
-      padding: "5px", // Add padding as needed
-      boxShadow: "none",
-      marginBottom: "5px",
-    },
   }
 
-  const textBreakStyle = {
-    display: "block",
-    marginBottom: -6,
-  }
+  const [requests, setRequests] = useState([])
+
+  useEffect(() => {
+    // eventually, we make this actually ask for the requests from firebase
+    // for now, simulate waiting 1 second and then return some dummy data
+    setTimeout(() => {
+      setRequests([
+        {
+          user_id: "whatsgood",
+          booking_id: "8mYj7rWOMH6hGy4FzMed",
+          query: "I'd like to cancel my booking.",
+          query_type: "cancellation",
+          status: "unresolved",
+        },
+        {
+          user_id: "deez",
+          booking_id: "8mYj7rWOMH6hGy4FzMed",
+          query: "I'd like to cancel my booking.",
+          query_type: "cancellation",
+          status: "unresolved",
+        },
+        {
+          user_id: "hello",
+          booking_id: "8mYj7rWOMH6hGy4FzMed",
+          query: "I'd like to cancel my booking.",
+          query_type: "cancellation",
+          status: "unresolved",
+        },
+        {
+          user_id: "bowling",
+          booking_id: "8mYj7rWOMH6hGy4FzMed",
+          query: "I'd like to cancel my booking.",
+          query_type: "cancellation",
+          status: "unresolved",
+        },
+      ])
+    }, 1000)
+  }, [])
+
+  // now use requests to display the data
 
   return (
     <div>
-      <Grid container rowSpacing={2} paddingTop="20px">
-        <Grid item xs={6}>
+      <Grid container rowSpacing={2} paddingTop="20px" sx={{ width: "100%" }}>
+        <Grid item xs={12} md={6}>
           <Typography variant="h4" align="left">
             REQUESTS
           </Typography>
           <Divider />
         </Grid>
-        <Grid item xs={6}>
+        <Grid item xs={12} md={6}>
           <Typography variant="h4" align="right">
             MANAGE
           </Typography>
@@ -47,174 +70,21 @@ const AdminDashboardRequest = () => {
         </Grid>
 
         <Grid item xs={12}>
-          <Paper elevation={0} style={gridItemStyle.gridItem1}>
+          <Paper elevation={0} style={gridItemStyle.requestBackground}>
             <Grid
               sx={{
                 overflowY: "scroll",
+                height: "600px",
                 maxHeight: "600px",
               }}
             >
-              <Grid item xs={12}>
-                <Paper elevation={0} style={gridItemStyle.gridItem2}>
-                  <Box paddingLeft="10px">
-                    <Typography
-                      variant="h5"
-                      align="left"
-                      sx={{ fontWeight: "bold" }}
-                    >
-                      USER 1
-                    </Typography>
-                    <Divider width="200px" />
-                    <Typography
-                      variant="h6"
-                      align="left"
-                      sx={{ fontWeight: "bold" }}
-                    >
-                      DATE CHANGE
-                      <span style={textBreakStyle}></span>
-                      5/7 - 7/7 -&gt; 7/7 - 9/7
-                    </Typography>
-                  </Box>
-                </Paper>
-              </Grid>
-              <Grid item xs={12}>
-                <Paper elevation={0} style={gridItemStyle.gridItem3}>
-                  <Box paddingLeft="10px">
-                    <Typography
-                      variant="h5"
-                      align="left"
-                      sx={{ fontWeight: "bold" }}
-                    >
-                      USER 1
-                    </Typography>
-                    <Divider width="200px" />
-                    <Typography
-                      variant="h6"
-                      align="left"
-                      sx={{ fontWeight: "bold" }}
-                    >
-                      DATE CHANGE
-                      <span style={textBreakStyle}></span>
-                      5/7 - 7/7 -&gt; 7/7 - 9/7
-                    </Typography>
-                  </Box>
-                </Paper>
-              </Grid>
-              <Grid item xs={12}>
-                <Paper elevation={0} style={gridItemStyle.gridItem2}>
-                  <Box paddingLeft="10px">
-                    <Typography
-                      variant="h5"
-                      align="left"
-                      sx={{ fontWeight: "bold" }}
-                    >
-                      USER 1
-                    </Typography>
-                    <Divider width="200px" />
-                    <Typography
-                      variant="h6"
-                      align="left"
-                      sx={{ fontWeight: "bold" }}
-                    >
-                      DATE CHANGE
-                      <span style={textBreakStyle}></span>
-                      5/7 - 7/7 -&gt; 7/7 - 9/7
-                    </Typography>
-                  </Box>
-                </Paper>
-              </Grid>
-              <Grid item xs={12}>
-                <Paper elevation={0} style={gridItemStyle.gridItem3}>
-                  <Box paddingLeft="10px">
-                    <Typography
-                      variant="h5"
-                      align="left"
-                      sx={{ fontWeight: "bold" }}
-                    >
-                      USER 1
-                    </Typography>
-                    <Divider width="200px" />
-                    <Typography
-                      variant="h6"
-                      align="left"
-                      sx={{ fontWeight: "bold" }}
-                    >
-                      DATE CHANGE
-                      <span style={textBreakStyle}></span>
-                      5/7 - 7/7 -&gt; 7/7 - 9/7
-                    </Typography>
-                  </Box>
-                </Paper>
-              </Grid>
-              <Grid item xs={12}>
-                <Paper elevation={0} style={gridItemStyle.gridItem2}>
-                  <Box paddingLeft="10px">
-                    <Typography
-                      variant="h5"
-                      align="left"
-                      sx={{ fontWeight: "bold" }}
-                    >
-                      USER 1
-                    </Typography>
-                    <Divider width="200px" />
-                    <Typography
-                      variant="h6"
-                      align="left"
-                      sx={{ fontWeight: "bold" }}
-                    >
-                      DATE CHANGE
-                      <span style={textBreakStyle}></span>
-                      5/7 - 7/7 -&gt; 7/7 - 9/7
-                    </Typography>
-                  </Box>
-                </Paper>
-              </Grid>
-              <Grid item xs={12}>
-                <Paper elevation={0} style={gridItemStyle.gridItem3}>
-                  <Box paddingLeft="10px">
-                    <Typography
-                      variant="h5"
-                      align="left"
-                      sx={{ fontWeight: "bold" }}
-                    >
-                      USER 1
-                    </Typography>
-                    <Divider width="200px" />
-                    <Typography
-                      variant="h6"
-                      align="left"
-                      sx={{ fontWeight: "bold" }}
-                    >
-                      DATE CHANGE
-                      <span style={textBreakStyle}></span>
-                      5/7 - 7/7 -&gt; 7/7 - 9/7
-                    </Typography>
-                  </Box>
-                </Paper>
-              </Grid>
-              <Grid item xs={12}>
-                <Paper elevation={0} style={gridItemStyle.gridItem2}>
-                  <Box paddingLeft="10px">
-                    <Typography
-                      variant="h5"
-                      align="left"
-                      sx={{ fontWeight: "bold" }}
-                    >
-                      USER 1
-                    </Typography>
-                    <Divider width="200px" />
-                    <Typography
-                      variant="h6"
-                      align="left"
-                      sx={{ fontWeight: "bold" }}
-                    >
-                      DATE CHANGE
-                      <span style={textBreakStyle}></span>
-                      5/7 - 7/7 -&gt; 7/7 - 9/7
-                    </Typography>
-                  </Box>
-                </Paper>
-              </Grid>
+              {requests.map((request, index) => (
+                <RequestBlock
+                  key={request.user_id}
+                  requestData={request}
+                  counter={index}
+                />
+              ))}
             </Grid>
           </Paper>
         </Grid>
