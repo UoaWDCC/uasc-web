e523309084b375e2fa85dd6c969cc5585754d22f
svimi <zmus754@aucklanduni.ac.nz>
2023-08-02 13:01:29 +1200

    Added AdminRequests component
    potential styling changes can be added, as well as changes to the scrollbar to look nicer

--- frontend/src/pages/Admin.js
+++ frontend/src/pages/Admin.js
@@ -3,19 +3,30 @@ import Requests from "../components/AdminRequests";
 import Bookings from "../components/AdminBookings";
 
 const Admin = () => {
-	return (
-		<div>
-      <Paper elevation={2} sx={{ margin: "32px", marginTop:"64px", padding:" 32px", backgroundColor: "#A8ADB0", borderRadius: "32px 0px 32px 0px" }}>
-        <Typography variant="h3" align="left" > ADMIN DASHBOARD </Typography>
-        <Divider/>
+  return (
+    <div>
+      <Paper
+        elevation={2}
+        sx={{
+          margin: "32px",
+          marginTop: "64px",
+          padding: " 32px",
+          backgroundColor: "#A8ADB0",
+          borderRadius: "32px 0px 32px 0px",
+        }}
+      >
+        <Typography variant="h3" align="left">
+          {" "}
+          ADMIN DASHBOARD{" "}
+        </Typography>
+        <Divider />
         <Stack direction="row" justifyContent="space-between">
-          <Requests/>
-          <Bookings/>
+          <Requests />
+          <Bookings />
         </Stack>
-
       </Paper>
-		</div>
-	);
+    </div>
+  );
 };
 
 export default Admin;
