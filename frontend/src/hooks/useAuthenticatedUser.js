7d6de54dfa73b240c5d92d56bf7801cbd58e6867
OverHash <46231745+OverHash@users.noreply.github.com>
2023-08-09 12:14:57 +1200

    Add persistent user data
    Uses a React hook to cache the user data once they have logged in. In
    the future, changes to a User object will need to be tracked and update
    the user metadata object.

---
+++ frontend/src/hooks/useAuthenticatedUser.js
@@ -0,0 +1,29 @@
+import { useAuthState } from "react-firebase-hooks/auth"
+import { auth, db } from "../firebase"
+import { doc, getDoc } from "firebase/firestore"
+import { useEffect, useState } from "react"
+
+/**
+ * Retrieves the currently authenticated user to the Firebase app, if it exists.
+ * @returns User The currently authenticated user object.
+ */
+export function useAuthenticatedUser() {
+  const [user] = useAuthState(auth)
+  const [userMetadata, setUserData] = useState(undefined)
+
+  useEffect(() => {
+    const getUserData = async () => {
+      const userMetadata = await getDoc(doc(db, "users", user.uid))
+      if (userMetadata.exists()) {
+        console.log(userMetadata.data())
+        setUserData(userMetadata.data())
+      }
+    }
+
+    if (user) {
+      getUserData()
+    }
+  }, [user])
+
+  return [user, userMetadata]
+}
