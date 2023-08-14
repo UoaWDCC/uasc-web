adf86764ea4c7c44cd24efada94fa2f2ab9620cc
bwon783 <86991521+BW82262@users.noreply.github.com>
2023-08-14 22:57:20 +1200

    Added Current Bookings User Profile Card

---
+++ frontend/src/components/ProfileCurrentBookings.js
@@ -0,0 +1,51 @@
+import { CardContent, Stack, Card, Typography, Button } from "@mui/material"
+import React from "react"
+
+function ProfileCurrentBookings() {
+  const mockBookingData = [0, 1, 2]
+
+  return (
+    <div>
+      <Card
+        sx={{
+          boxShadow: "0px 8px 44px 0px rgba(0, 0, 0, 0.14)",
+          backgroundColor: "white",
+          borderRadius: "15px",
+        }}
+      >
+        <CardContent sx={{ padding: "36px" }}>
+          <Stack spacing={4}>
+            <Typography variant="h6" align="left">
+              Current Bookings{" "}
+            </Typography>
+            <Stack spacing={2}>
+              {mockBookingData.map((booking) => (
+                <Stack
+                  key={booking}
+                  direction="row"
+                  justifyContent="space-between"
+                >
+                  <Typography variant="body1" align="left">
+                    {" "}
+                    Booking {booking}{" "}
+                  </Typography>
+                  <Button
+                    variant="contained"
+                    color="buttonPrimary"
+                    size="small"
+                    sx={{ borderRadius: "100px", paddingX: "24px" }}
+                  >
+                    {" "}
+                    Request Change{" "}
+                  </Button>
+                </Stack>
+              ))}
+            </Stack>
+          </Stack>
+        </CardContent>
+      </Card>
+    </div>
+  )
+}
+
+export default ProfileCurrentBookings
