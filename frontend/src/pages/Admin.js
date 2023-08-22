bdfdf47d2ef0e4aa21771337f0dbe91ee83787eb
Bill W <86991521+BW82262@users.noreply.github.com>
2023-08-22 14:48:48 +1200

    Refactored & Restyled Admin Bookings Overview

--- frontend/src/pages/Admin.js
+++ frontend/src/pages/Admin.js
@@ -1,41 +1,11 @@
 import React, { useState } from "react"
-import {
-  Paper,
-  Typography,
-  Stack,
-  Button,
-  Grid,
-  IconButton,
-} from "@mui/material"
-import { ArrowForwardIos, ArrowBackIos } from "@mui/icons-material"
+import { Typography, Stack } from "@mui/material"
 import Requests from "../components/AdminRequests"
 import RequestDetails from "../components/RequestDetails"
+import AdminBookings from "../components/AdminBookings"
 import "./Admin.css"
 
 const Admin = () => {
-  const daysOfWeek = ["SUN", "MON", "TUE", "WED", "THU", "FRI", "SAT"]
-  const [weekOffset, setWeekOffset] = useState(0)
-
-  const startDate = new Date()
-  startDate.setDate(startDate.getDate() - startDate.getDay() + weekOffset * 7)
-  const endDate = new Date(startDate)
-  endDate.setDate(endDate.getDate() + 6)
-
-  // Fake dataset for now - will be replaced with API call
-  const bookings = {
-    SUN: ["User A"],
-    MON: [],
-    TUE: ["User A", "User B"],
-    WED: ["User A", "User C", "User D"],
-    THU: [],
-    FRI: ["User A", "User B", "User C", "User D"],
-    SAT: ["User C", "User D"],
-  }
-
-  const handleUserClick = (user) => {
-    alert(`Clicked on ${user}`)
-  }
-
   const [selectedUser, setSelectedUser] = useState(null)
 
   return (
@@ -70,78 +40,7 @@ const Admin = () => {
               setSelectedUser={setSelectedUser}
             />
           ) : (
-            <div className="bookings-section">
-              <div className="bookings-header">
-                <Typography variant="h4" align="left">
-                  BOOKINGS
-                </Typography>
-                <Button
-                  variant="contained"
-                  color="primary"
-                  className="manage-bookings-button"
-                >
-                  Manage Bookings
-                </Button>
-              </div>
-              <Paper
-                elevation={2}
-                sx={{
-                  padding: "32px",
-                  borderRadius: "16px",
-                  background: "#D9D9D9",
-                }}
-                className="calendar-container"
-              >
-                <div className="container-header">
-                  <IconButton onClick={() => setWeekOffset(weekOffset - 1)}>
-                    <ArrowBackIos />
-                  </IconButton>
-                  <Typography
-                    variant="h6"
-                    paddingLeft="1rem"
-                    className="date-range"
-                  >
-                    {startDate.toLocaleDateString()} -{" "}
-                    {endDate.toLocaleDateString()}
-                  </Typography>
-                  <IconButton onClick={() => setWeekOffset(weekOffset + 1)}>
-                    <ArrowForwardIos />
-                  </IconButton>
-                </div>
-                <Grid container spacing={1}>
-                  {daysOfWeek.map((day, index) => (
-                    <Grid
-                      key={index}
-                      item
-                      xs={12}
-                      md={12 / daysOfWeek.length}
-                      className="day-container"
-                    >
-                      <Typography align="center" variant="h6">
-                        {day}
-                      </Typography>
-                      <div className="user-buttons-container">
-                        {bookings[day]?.length > 0 ? (
-                          bookings[day].map((user, userIndex) => (
-                            <Button
-                              key={userIndex}
-                              onClick={() => handleUserClick(user)}
-                              className="user-button"
-                            >
-                              {user}
-                            </Button>
-                          ))
-                        ) : (
-                          <Typography variant="no-booking">
-                            No bookings
-                          </Typography>
-                        )}
-                      </div>
-                    </Grid>
-                  ))}
-                </Grid>
-              </Paper>
-            </div>
+            <AdminBookings />
           )}
         </Stack>
       </Stack>
