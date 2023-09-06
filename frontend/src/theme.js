f2d8babcf32c624b92a33ba7b7f0fb6bca408469
OverHash <46231745+OverHash@users.noreply.github.com>
2023-09-06 16:26:05 +1200

    Add prettier as step to eslintrc

--- frontend/src/theme.js
+++ frontend/src/theme.js
@@ -1,30 +1,29 @@
 import { createTheme } from "@mui/material"
 
 export default createTheme({
-	palette: {
-		common: {
-			black: "#000",
-			grey: "#A8ADB0",
-			darkGrey: "#474747",
-			white: "#fff",
-		},
-		primary: {
-			main: "#81C7EB", // Blue color 1
-			secondary: "#54B3E5", // Blue color 2
-			tertiary: "#51A1DD", // Blue color 3
-			quaternary: "#457CC4", // Blue color 4
-		  },
-		  secondary: {
-			main: "#BD8FFF", // Purple color 1
-			secondary: "#8966FF", // Purple color 2
-		  },
-      buttonPrimary: {
+  palette: {
+    common: {
+      black: "#000",
+      grey: "#A8ADB0",
+      darkGrey: "#474747",
+      white: "#fff",
+    },
+    primary: {
+      main: "#81C7EB", // Blue color 1
+      secondary: "#54B3E5", // Blue color 2
+      tertiary: "#51A1DD", // Blue color 3
+      quaternary: "#457CC4", // Blue color 4
+    },
+    secondary: {
+      main: "#BD8FFF", // Purple color 1
+      secondary: "#8966FF", // Purple color 2
+    },
+    buttonPrimary: {
       main: "#54B3E5",
       contrastText: "#fff",
     },
-	},
-	typography: {
-		fontFamily: "Lato",
-	  },
-});
-
+  },
+  typography: {
+    fontFamily: "Lato",
+  },
+})
