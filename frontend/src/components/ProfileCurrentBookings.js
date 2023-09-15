734dcba08907ed6dbb2d3f53cc963d0925be1cee
Bill W <86991521+BW82262@users.noreply.github.com>
2023-09-15 15:04:40 +1200

    Implemented Check in and Checkout dates with logic checks

--- frontend/src/components/ProfileCurrentBookings.js
+++ frontend/src/components/ProfileCurrentBookings.js
@@ -9,8 +9,10 @@ import {
   Dialog,
   TextField,
 } from "@mui/material"
-import React from "react"
+import React, { useEffect } from "react"
 import { useState } from "react"
+import { DatePicker } from "@mui/x-date-pickers/DatePicker"
+import dayjs from "dayjs"
 
 function SingularBookingDetails({ booking, onRequestChange }) {
   return (
@@ -33,6 +35,249 @@ function SingularBookingDetails({ booking, onRequestChange }) {
         Request Change
       </Button>
     </Stack>
+  )
+}
+
+function BookingRequestModal({
+  booking,
+  open,
+  handleClose,
+  handleRequestSubmit,
+}) {
+  console.log(
+    Math.ceil(
+      (new Date(booking.data().check_out.seconds) -
+        new Date(booking.data().check_in.seconds)) /
+        (3600 * 24)
+    )
+  )
+
+  const [checkInDate, setCheckInDate] = useState(null)
+  const [checkOutDate, setCheckOutDate] = useState(null)
+  const [newTotalDays, setNewTotalDays] = useState(0)
+
+  //TODO: REVIEW THIS SECTION OF CODE FROM MITCHELL"S BOOKING FORM
+  const handleChangeStartDate = (startDate) => {
+    if (startDate >= checkOutDate) {
+      setCheckOutDate(null)
+    }
+    const newCheckInDate = new Date(getFormattedDateString(startDate))
+    setCheckInDate(dayjs(newCheckInDate)) // need to use dayjs because thats the MUI datepicker value type
+  }
+
+  const handleChangeEndDate = (endDate) => {
+    const newCheckOutDate = new Date(getFormattedDateString(endDate))
+    setCheckOutDate(dayjs(newCheckOutDate))
+  }
+
+  const isEndDateInvalid = (date) => {
+    const endDate = new Date(getFormattedDateString(date))
+    if (endDate <= checkInDate) {
+      return true
+    }
+    return false
+  }
+
+  const isStartDateInvalid = (date) => {
+    const startDate = new Date(getFormattedDateString(date))
+    const currDate = new Date()
+
+    if (startDate < currDate) {
+      return true
+    }
+
+    return false
+  }
+
+  const getFormattedDateString = (date) => {
+    return dayjs(date).format("YYYY-MM-DD")
+  }
+
+  useEffect(() => {
+    if (checkInDate && checkOutDate) {
+      console.log("Both checkin and checkout dates are valid")
+      console.log(checkOutDate.diff(checkInDate, "day"))
+      setNewTotalDays(checkOutDate.diff(checkInDate, "day"))
+    } else {
+      console.log("One of the dates is invalid")
+    }
+  }, [checkInDate, checkOutDate])
+
+  return (
+    <Dialog
+      open={open}
+      onClose={handleClose}
+      maxWidth="md"
+      fullWidth={false}
+      PaperProps={{ sx: { borderRadius: "15px" } }}
+    >
+      <Card sx={{ padding: "32px", width: "100%" }}>
+        <CardContent sx={{ width: "100%" }}>
+          <Stack spacing={4}>
+            <Typography
+              variant="h4"
+              align="left"
+              color="#474747"
+              sx={{ fontWeight: "900" }}
+            >
+              Request Change
+            </Typography>
+
+            <Stack direction="row" justifyContent="space-between" spacing={12}>
+              <Stack align="left" spacing={2}>
+                <Typography variant="h6" color="#457CC3">
+                  Current Booking details
+                </Typography>
+                <Stack direction="row">
+                  <Typography
+                    variant="body1"
+                    color="#787B7D"
+                    sx={{
+                      minWidth: "200px",
+                      maxWidth: "25%",
+                    }}
+                  >
+                    Check-in Date:
+                  </Typography>
+                  <Typography variant="body1" color="#787B7D" sx={{}}>
+                    {new Date(
+                      booking.data().check_in.seconds * 1000
+                    ).toLocaleDateString("en-GB")}
+                  </Typography>
+                </Stack>
+                <Stack direction="row">
+                  <Typography
+                    variant="body1"
+                    color="#787B7D"
+                    sx={{
+                      minWidth: "200px",
+                      maxWidth: "25%",
+                    }}
+                  >
+                    Check-out Date:
+                  </Typography>
+                  <Typography variant="body1" color="#787B7D" sx={{}}>
+                    {new Date(
+                      booking.data().check_out.seconds * 1000
+                    ).toLocaleDateString("en-GB")}
+                  </Typography>
+                </Stack>
+                <Stack direction="row">
+                  <Typography
+                    variant="body1"
+                    color="#787B7D"
+                    sx={{
+                      minWidth: "200px",
+                      maxWidth: "25%",
+                    }}
+                  >
+                    Total Days:
+                  </Typography>
+                  <Typography variant="body1" color="#787B7D" sx={{}}>
+                    {Math.ceil(
+                      (new Date(booking.data().check_out.seconds) -
+                        new Date(booking.data().check_in.seconds)) /
+                        (3600 * 24)
+                    )}
+                  </Typography>
+                </Stack>
+              </Stack>
+              <Stack align="left" spacing={2}>
+                <Typography variant="h6" color="#457CC3">
+                  Request Details
+                </Typography>
+                <Stack direction="row">
+                  <Typography
+                    variant="body1"
+                    sx={{
+                      minWidth: "200px",
+                      maxWidth: "25%",
+                    }}
+                  >
+                    Check-in Date:
+                  </Typography>
+                  {/* <Typography variant="body1">05/07/2023</Typography> */}
+                  <DatePicker
+                    value={checkInDate}
+                    onChange={handleChangeStartDate}
+                    shouldDisableDate={isStartDateInvalid}
+                    label="New Check-in Date"
+                    slotProps={{ textField: { size: "small" } }}
+                  />
+                </Stack>
+                <Stack direction="row">
+                  <Typography
+                    variant="body1"
+                    sx={{
+                      minWidth: "200px",
+                      maxWidth: "25%",
+                    }}
+                  >
+                    Check-out Date:
+                  </Typography>
+                  {/* <Typography variant="body1">07/07/2023</Typography> */}
+                  <DatePicker
+                    value={checkOutDate}
+                    onChange={handleChangeEndDate}
+                    shouldDisableDate={isEndDateInvalid}
+                    label="New Check-out Date"
+                    slotProps={{ textField: { size: "small" } }}
+                  />
+                </Stack>
+                <Stack direction="row">
+                  <Typography
+                    variant="body1"
+                    sx={{
+                      minWidth: "200px",
+                      maxWidth: "25%",
+                    }}
+                  >
+                    Total Days:
+                  </Typography>
+                  <Typography variant="body1">{newTotalDays}</Typography>
+                </Stack>
+              </Stack>
+            </Stack>
+            <Typography variant="h6" color="#457CC3">
+              Request Message:
+            </Typography>
+            <TextField
+              variant="standard"
+              multiline
+              rows={6}
+              InputProps={{
+                disableUnderline: true,
+              }}
+              style={{
+                width: "100%",
+                background: "#EDF8FF",
+                "&:hover": {
+                  borderColor: "transparent",
+                },
+                borderRadius: "15px",
+              }}
+            />
+            <Stack justifyContent="center" alignItems="center">
+              <Button
+                variant="contained"
+                color="buttonPrimary"
+                size="small"
+                sx={{
+                  borderRadius: "100px",
+                  paddingX: "24px",
+                  textTransform: "none",
+                  width: "fit-content",
+                }}
+                onClick={handleRequestSubmit}
+              >
+                {" "}
+                Request Change{" "}
+              </Button>
+            </Stack>
+          </Stack>
+        </CardContent>
+      </Card>
+    </Dialog>
   )
 }
 
@@ -77,174 +322,26 @@ function ProfileCurrentBookings({ bookings }) {
             ) : (
               <Stack spacing={2}>
                 {bookings.map((booking) => (
-                  <SingularBookingDetails
-                    key={booking.id}
-                    booking={booking}
-                    onRequestChange={handleOpen}
-                  />
+                  <>
+                    <SingularBookingDetails
+                      key={booking.id}
+                      booking={booking}
+                      onRequestChange={handleOpen}
+                    />
+                    <BookingRequestModal
+                      key={booking.id}
+                      booking={booking}
+                      open={open}
+                      handleClose={handleClose}
+                      handleRequestSubmit={handleRequestSubmit}
+                    />
+                  </>
                 ))}
               </Stack>
             )}
           </Stack>
         </CardContent>
       </Card>
-
-      {/* edit details dialog */}
-      <Dialog
-        open={open}
-        onClose={handleClose}
-        maxWidth="md"
-        fullWidth={false}
-        PaperProps={{ sx: { borderRadius: "15px" } }}
-      >
-        <Card sx={{ padding: "32px", width: "100%" }}>
-          <CardContent sx={{ width: "100%" }}>
-            <Stack spacing={4}>
-              <Typography
-                variant="h4"
-                align="left"
-                color="#474747"
-                sx={{ fontWeight: "900" }}
-              >
-                Request Change
-              </Typography>
-              <Stack
-                direction="row"
-                justifyContent="space-between"
-                spacing={12}
-              >
-                <Stack align="left" spacing={2}>
-                  <Typography variant="h6" color="#457CC3">
-                    Current Booking details
-                  </Typography>
-                  <Stack direction="row">
-                    <Typography
-                      variant="body1"
-                      color="#787B7D"
-                      sx={{
-                        minWidth: "200px",
-                        maxWidth: "25%",
-                      }}
-                    >
-                      Check-in Date:
-                    </Typography>
-                    <Typography variant="body1" color="#787B7D" sx={{}}>
-                      05/07/2023
-                    </Typography>
-                  </Stack>
-                  <Stack direction="row">
-                    <Typography
-                      variant="body1"
-                      color="#787B7D"
-                      sx={{
-                        minWidth: "200px",
-                        maxWidth: "25%",
-                      }}
-                    >
-                      Check-out Date:
-                    </Typography>
-                    <Typography variant="body1" color="#787B7D" sx={{}}>
-                      07/07/2023
-                    </Typography>
-                  </Stack>
-                  <Stack direction="row">
-                    <Typography
-                      variant="body1"
-                      color="#787B7D"
-                      sx={{
-                        minWidth: "200px",
-                        maxWidth: "25%",
-                      }}
-                    >
-                      Total Days:
-                    </Typography>
-                    <Typography variant="body1" color="#787B7D" sx={{}}>
-                      3
-                    </Typography>
-                  </Stack>
-                </Stack>
-                <Stack align="left" spacing={2}>
-                  <Typography variant="h6" color="#457CC3">
-                    Request Details
-                  </Typography>
-                  <Stack direction="row">
-                    <Typography
-                      variant="body1"
-                      sx={{
-                        minWidth: "200px",
-                        maxWidth: "25%",
-                      }}
-                    >
-                      Check-in Date:
-                    </Typography>
-                    <Typography variant="body1">05/07/2023</Typography>
-                  </Stack>
-                  <Stack direction="row">
-                    <Typography
-                      variant="body1"
-                      sx={{
-                        minWidth: "200px",
-                        maxWidth: "25%",
-                      }}
-                    >
-                      Check-out Date:
-                    </Typography>
-                    <Typography variant="body1">07/07/2023</Typography>
-                  </Stack>
-                  <Stack direction="row">
-                    <Typography
-                      variant="body1"
-                      sx={{
-                        minWidth: "200px",
-                        maxWidth: "25%",
-                      }}
-                    >
-                      Total Days:
-                    </Typography>
-                    <Typography variant="body1">3</Typography>
-                  </Stack>
-                </Stack>
-              </Stack>
-              <Typography variant="h6" color="#457CC3">
-                Request Message:
-              </Typography>
-              <TextField
-                variant="standard"
-                multiline
-                rows={6}
-                InputProps={{
-                  disableUnderline: true,
-                }}
-                style={{
-                  width: "100%",
-                  background: "#EDF8FF",
-                  "&:hover": {
-                    borderColor: "transparent",
-                  },
-                  borderRadius: "15px",
-                }}
-              />
-              <Stack justifyContent="center" alignItems="center">
-                <Button
-                  variant="contained"
-                  color="buttonPrimary"
-                  size="small"
-                  sx={{
-                    borderRadius: "100px",
-                    paddingX: "24px",
-                    textTransform: "none",
-                    width: "fit-content",
-                  }}
-                  onClick={handleRequestSubmit}
-                >
-                  {" "}
-                  Request Change{" "}
-                </Button>
-              </Stack>
-            </Stack>
-          </CardContent>
-        </Card>
-      </Dialog>
     </div>
   )
 }
