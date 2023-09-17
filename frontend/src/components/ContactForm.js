a343ff7ecec54c6fd44c58196f322f8286fc7cb2
Bill W <86991521+BW82262@users.noreply.github.com>
2023-09-17 23:44:05 +1200

    Updated Contact Page

--- frontend/src/components/ContactForm.js
+++ frontend/src/components/ContactForm.js
@@ -1,5 +1,5 @@
 import React from "react"
-import { Button, FormControl, FormLabel, TextField } from "@mui/material"
+import { Button, TextField, Typography, Stack } from "@mui/material"
 
 const ContactForm = () => {
   const handleSubmit = () => {
@@ -7,19 +7,100 @@ const ContactForm = () => {
   }
 
   return (
-    <form onSubmit={handleSubmit}>
-      <FormControl>
-        <FormLabel style={{ textAlign: "left" }}>Name</FormLabel>
-        <TextField required></TextField>
-        <FormLabel style={{ textAlign: "left" }}>Email Address</FormLabel>
-        <TextField required></TextField>
-        <FormLabel style={{ textAlign: "left" }}>Message</FormLabel>
-        <TextField multiline required></TextField>
-        <Button type="submit" variant="contained">
-          Submit
-        </Button>
-      </FormControl>
-    </form>
+    <div
+      style={{
+        backgroundColor: "white",
+        padding: "20px",
+        borderRadius: "15px",
+        boxShadow: "0px 8px 44px 0px rgba(0, 0, 0, 0.14)",
+      }}
+    >
+      <form onSubmit={handleSubmit}>
+        <Stack spacing={2}>
+          <Stack>
+            <Typography variant="h6" align="left" color="#457CC3">
+              Name
+            </Typography>
+            <TextField
+              variant="standard"
+              required
+              sx={{ backgroundColor: "#EDF8FF" }}
+              InputProps={{
+                disableUnderline: true,
+              }}
+              style={{
+                width: "100%",
+                background: "#EDF8FF",
+                "&:hover": {
+                  borderColor: "transparent",
+                },
+                borderRadius: "5px",
+              }}
+            />
+          </Stack>
+          <Stack>
+            <Typography variant="h6" align="left" color="#457CC3">
+              Email Address
+            </Typography>
+            <TextField
+              variant="standard"
+              required
+              sx={{ backgroundColor: "#EDF8FF" }}
+              InputProps={{
+                disableUnderline: true,
+              }}
+              style={{
+                width: "100%",
+                background: "#EDF8FF",
+                "&:hover": {
+                  borderColor: "transparent",
+                },
+                borderRadius: "5px",
+              }}
+            />
+          </Stack>
+          <Stack>
+            <Typography variant="h6" align="left" color="#457CC3">
+              Message
+            </Typography>
+            <TextField
+              variant="standard"
+              multiline
+              rows={5}
+              placeholder="Write your message here..."
+              // onChange={(e) => setRequestMessage(e.target.value)}
+              InputProps={{
+                disableUnderline: true,
+              }}
+              style={{
+                width: "100%",
+                background: "#EDF8FF",
+                "&:hover": {
+                  borderColor: "transparent",
+                },
+                borderRadius: "5px",
+              }}
+              required
+            />
+          </Stack>
+          <Stack justifyContent="center" alignItems="center">
+            <Button
+              type="submit"
+              variant="contained"
+              color="buttonPrimary"
+              sx={{
+                borderRadius: "100px",
+                paddingX: "24px",
+                textTransform: "none",
+                width: "fit-content",
+              }}
+            >
+              SUBMIT
+            </Button>
+          </Stack>
+        </Stack>
+      </form>
+    </div>
   )
 }
 
