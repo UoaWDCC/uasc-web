0ceb7970119cc551871ef4bd3534dccb4c771f16
Bill W <86991521+BW82262@users.noreply.github.com>
2023-09-17 23:44:05 +1200

    Updated Contact Page

--- frontend/src/pages/Contact.js
+++ frontend/src/pages/Contact.js
@@ -1,11 +1,39 @@
 import React from "react"
 import ContactForm from "../components/ContactForm"
+import { Typography, Stack, Container } from "@mui/material"
 
 const Contact = () => {
   return (
-    <div>
-      <h1> Contact page</h1>
-      <ContactForm />
+    <div
+      style={{
+        backgroundColor: "#f4f4f4",
+        height: "150%",
+        width: "100%",
+        backgroundImage:
+          "radial-gradient(ellipse 50% 50% at 30% 30%, #81c7ebaa, #ffffff)",
+      }}
+    >
+      <Container maxWidth="sm">
+        <Stack spacing={3} sx={{ paddingTop: "124px" }}>
+          <Typography
+            variant="h1"
+            align="left"
+            color="#474747"
+            sx={{ fontWeight: "bold" }}
+          >
+            Contact Us
+          </Typography>
+          <Typography
+            variant="h4"
+            align="left"
+            color="#457CC3"
+            sx={{ fontWeight: "900" }}
+          >
+            Have a question? Send us a message!
+          </Typography>
+          <ContactForm />
+        </Stack>
+      </Container>
     </div>
   )
 }
