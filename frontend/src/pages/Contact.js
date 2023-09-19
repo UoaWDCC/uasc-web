6182ffaea97c8ee7328bd0363b94a975740892ad
Bill W <86991521+BW82262@users.noreply.github.com>
2023-09-19 15:47:12 +1200

    Fixed the height of the pages

--- frontend/src/pages/Contact.js
+++ frontend/src/pages/Contact.js
@@ -7,7 +7,7 @@ const Contact = () => {
     <div
       style={{
         backgroundColor: "#f4f4f4",
-        height: "150%",
+        height: "100%",
         width: "100%",
         backgroundImage:
           "radial-gradient(ellipse 50% 50% at 30% 30%, #81c7ebaa, #ffffff)",
