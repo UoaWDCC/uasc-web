4e1c68786306434aabad8ce63a1b1477a6076414
eli384 <88353612+edl21@users.noreply.github.com>
2023-09-30 12:36:07 +1300

    functionality added

--- frontend/src/components/ProfileBookingHistory.js
+++ frontend/src/components/ProfileBookingHistory.js
@@ -1,64 +1,120 @@
-import React from "react"
-import { CardContent, Stack, Card, Typography } from "@mui/material"
+import React, { useState, useEffect } from "react"
+import { db } from "../firebase"
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+  Paper,
+  Card,
+  CardContent,
+  Typography,
+  Stack,
+} from "@mui/material"
+import {
+  doc,
+  collection,
+  where,
+  query,
+  orderBy,
+  getDocs,
+} from "@firebase/firestore"
 
-function ProfileBookingHistory() {
-  const mockBookingHistoryData = [
-    {
-      check_in: "Jan 1st 2021",
-      check_out: "Jan 4th 2021",
-      total_days: 4,
-      booking_id: 0,
-    },
-    {
-      check_in: "Aug 21st 2021",
-      check_out: "Aug 22nd 2021",
-      total_days: 2,
-      booking_id: 1,
-    },
-    {
-      check_in: "Sep 25th 2022",
-      check_out: "Sep 26th 2022",
-      total_days: 2,
-      booking_id: 2,
-    },
-  ]
+const ProfileBookingHistory = ({ userId }) => {
+  const [bookings, setBookings] = useState([])
+  const [loading, setLoading] = useState(true)
+
+  useEffect(() => {
+    if (!userId) return
+    setLoading(true)
+
+    const fetchData = async () => {
+      try {
+        const userDocRef = doc(db, "users", userId)
+        const q = query(
+          collection(db, "bookings"),
+          where("user_id", "==", userDocRef),
+          orderBy("check_in", "desc")
+        )
+
+        const querySnapshot = await getDocs(q)
+
+        const fetchedBookings = querySnapshot.docs.map((doc) => ({
+          id: doc.id,
+          ...doc.data(),
+        }))
+        setBookings(fetchedBookings)
+      } catch (error) {
+        console.error("Error fetching user bookings: ", error)
+      } finally {
+        setLoading(false)
+      }
+    }
+
+    fetchData()
+  }, [userId])
+
   return (
-    <div>
-      <Card
-        sx={{
-          boxShadow: "0px 8px 44px 0px rgba(0, 0, 0, 0.14)",
-          backgroundColor: "white",
-          borderRadius: "15px",
-        }}
-      >
-        <CardContent sx={{ padding: "36px" }}>
-          <Stack spacing={4}>
-            <Typography
-              variant="h5"
-              align="left"
-              color="#457CC3"
-              sx={{ fontWeight: "900" }}
-            >
-              Booking History{" "}
+    <Card
+      sx={{
+        boxShadow: "0px 8px 44px 0px rgba(0, 0, 0, 0.14)",
+        backgroundColor: "white",
+        borderRadius: "15px",
+      }}
+    >
+      <CardContent sx={{ padding: "36px" }}>
+        <Stack spacing={4}>
+          <Typography
+            variant="h5"
+            align="left"
+            color="#457CC3"
+            sx={{ fontWeight: "900" }}
+          >
+            Booking History
+          </Typography>
+          {loading ? (
+            <Typography variant="body1" align="left">
+              {" "}
+              Loading...{" "}
             </Typography>
-            <Stack spacing={2}>
-              {mockBookingHistoryData.map((booking) => (
-                <Stack
-                  key={booking.booking_id}
-                  direction="row"
-                  justifyContent="space-between"
-                >
-                  <Typography variant="body1" align="left">
-                    {" "}
-                    {booking.check_in} - {booking.check_out}{" "}
-                  </Typography>
-                </Stack>
-              ))}
-            </Stack>
-          </Stack>
-        </CardContent>
-      </Card>
-    </div>
+          ) : bookings.length === 0 ? (
+            <Typography variant="body1" align="left">
+              {" "}
+              You have no past bookings.{" "}
+            </Typography>
+          ) : (
+            <TableContainer component={Paper}>
+              <Table>
+                <TableHead>
+                  <TableRow>
+                    <TableCell align="right">Check In</TableCell>
+                    <TableCell align="right">Check Out</TableCell>
+                  </TableRow>
+                </TableHead>
+                <TableBody>
+                  {bookings.map((booking) => (
+                    <TableRow key={booking.id}>
+                      <TableCell align="right">
+                        {new Date(
+                          booking.check_in.seconds * 1000
+                        ).toLocaleDateString("en-GB") || "N/A"}
+                      </TableCell>
+                      <TableCell align="right">
+                        {new Date(
+                          booking.check_out.seconds * 1000
+                        ).toLocaleDateString("en-GB") || "N/A"}
+                      </TableCell>
+                    </TableRow>
+                  ))}
+                </TableBody>
+              </Table>
+            </TableContainer>
+          )}
+        </Stack>
+      </CardContent>
+    </Card>
   )
 }
 
