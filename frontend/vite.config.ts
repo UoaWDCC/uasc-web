32d0ef91c6bb22b07a58dae6969cebefbd52374b
clear <alexwillmcleod@gmail.com>
2024-01-27 13:46:54 +1300

    fixed vite config path

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,7 +3,9 @@ import react from "@vitejs/plugin-react"
 import viteTsconfigPaths from "vite-tsconfig-paths"
 
 export default defineConfig({
-  // depending on your application, base can also be "/"
+  build: {
+    outDir: "build",
+  },
   base: "/",
   plugins: [react(), viteTsconfigPaths()],
   server: {},
