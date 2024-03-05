fe347395f95d63e1c427a81311afc37e979d775c
bcho892 <AKLbc70@gmail.com>
2024-03-05 16:51:03 +1300

    add sweet state to front end

--- client/src/services/FetchClient.ts
+++ client/src/services/FetchClient.ts
@@ -2,7 +2,7 @@ import createClient from "openapi-fetch"
 import type { paths } from "models/__generated__/schema"
 
 const fetchClient = createClient<paths>({
-  baseUrl: process.env.BACKEND_BASE_URL
+  baseUrl: import.meta.env.VITE_BACKEND_BASE_URL
 })
 
 export default fetchClient
