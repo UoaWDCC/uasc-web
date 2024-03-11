7774fa8a3d9cae46fe209c74059fb6480da5557a
bcho892 <AKLbc70@gmail.com>
2024-03-11 18:17:07 +1300

    fix typo

--- server/src/business-layer/security/Firebase.ts
+++ server/src/business-layer/security/Firebase.ts
@@ -7,11 +7,11 @@ import * as _admin from "firebase-admin"
 const keysEnvVar = process.env.GOOGLE_SERVICE_ACCOUNT_JSON
 let keys
 
-if (!keysEnvVar && process.env.JEST_WORKER_ID !== undefined) {
-  throw new Error("The $CREDS environment variable was not found!")
+if (!keysEnvVar && process.env.JEST_WORKER_ID === undefined) {
+  throw new Error("The service account environment variable was not found!")
 }
 
-if (process.env.JEST_WORKER_ID !== undefined) {
+if (process.env.JEST_WORKER_ID === undefined) {
   keys = JSON.parse(keysEnvVar)
 }
 
