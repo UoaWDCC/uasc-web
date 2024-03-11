ad8e3b92c4bccf72761d62277658bc894ed8eda1
bcho892 <AKLbc70@gmail.com>
2024-03-11 18:17:58 +1300

    remove magic variable

--- server/src/business-layer/security/Firebase.ts
+++ server/src/business-layer/security/Firebase.ts
@@ -4,18 +4,20 @@ import {
 } from "data-layer/adapters/EmulatorConfig"
 import * as _admin from "firebase-admin"
 
+const IS_JEST = process.env.JEST_WORKER_ID === undefined
+
 const keysEnvVar = process.env.GOOGLE_SERVICE_ACCOUNT_JSON
 let keys
 
-if (!keysEnvVar && process.env.JEST_WORKER_ID === undefined) {
+if (!keysEnvVar && IS_JEST) {
   throw new Error("The service account environment variable was not found!")
 }
 
-if (process.env.JEST_WORKER_ID === undefined) {
+if (!IS_JEST) {
   keys = JSON.parse(keysEnvVar)
 }
 
-if (process.env.DEV || process.env.JEST_WORKER_ID !== undefined) {
+if (process.env.DEV || IS_JEST) {
   process.env.FIRESTORE_EMULATOR_HOST = `${EMULATOR_HOST}:${EMULATOR_FIRESTORE_PORT}`
 }
 
