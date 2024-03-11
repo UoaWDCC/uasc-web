f3e5f39a343504e32377f60f4618f89388582d86
bcho892 <AKLbc70@gmail.com>
2024-03-11 18:25:53 +1300

    fix off by one

--- server/src/business-layer/security/Firebase.ts
+++ server/src/business-layer/security/Firebase.ts
@@ -4,26 +4,27 @@ import {
 } from "data-layer/adapters/EmulatorConfig"
 import * as _admin from "firebase-admin"
 
-const IS_JEST = process.env.JEST_WORKER_ID === undefined
+const IS_JEST = process.env.JEST_WORKER_ID !== undefined
 
 const keysEnvVar = process.env.GOOGLE_SERVICE_ACCOUNT_JSON
-let keys
 
-if (!keysEnvVar && IS_JEST) {
+if (!keysEnvVar && !IS_JEST) {
   throw new Error("The service account environment variable was not found!")
 }
 
-if (!IS_JEST) {
-  keys = JSON.parse(keysEnvVar)
-}
+const keys = JSON.parse(keysEnvVar ?? "{}")
 
 if (process.env.DEV || IS_JEST) {
   process.env.FIRESTORE_EMULATOR_HOST = `${EMULATOR_HOST}:${EMULATOR_FIRESTORE_PORT}`
 }
 
-const firebase = _admin.initializeApp({
-  credential: _admin.credential.cert(keys)
-})
+const firebase = _admin.initializeApp(
+  Object.keys(keys).length > 0
+    ? {
+        credential: _admin.credential.cert(keys)
+      }
+    : {}
+)
 
 export const admin = _admin
 
