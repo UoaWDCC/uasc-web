07d779a1e5f1f3c7c1270abbda8a7830b1cf8e17
bcho892 <AKLbc70@gmail.com>
2024-03-12 17:53:06 +1300

    allow custom options

--- client/src/services/User/UserMutations.ts
+++ client/src/services/User/UserMutations.ts
@@ -1,8 +1,12 @@
-import { useMutation } from "@tanstack/react-query"
+import { UseMutationOptions, useMutation } from "@tanstack/react-query"
 import UserService, { EditUsersBody } from "./UserService"
 
-export function useEditUserMutation(users: EditUsersBody) {
+export function useEditUserMutation(
+  users: EditUsersBody,
+  options: UseMutationOptions
+) {
   return useMutation({
+    ...options,
     mutationFn: () => UserService.editUsers(users)
   })
 }
