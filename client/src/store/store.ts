df15e49af3ac9be24812400d0d399ae6a6bdeed7
bcho892 <AKLbc70@gmail.com>
2024-03-23 15:33:53 +1300

    add current user to data store

--- client/src/store/store.ts
+++ client/src/store/store.ts
@@ -1,20 +1,28 @@
+import { User } from "firebase/auth"
 import {
+  defaultRegistry,
   createStore,
   Action,
   createContainer,
   createHook
 } from "react-sweet-state"
 
-type State = { current: number }
+type State = { current: number; currentUser: User | null }
 
 const initialState: State = {
-  current: 1000
+  current: 1000,
+  currentUser: null
 }
 
 const actions = {
   loadInfo:
     (): Action<State> =>
-    async ({ setState }) => {}
+    async ({ setState }) => {},
+  setCurrentUser:
+    (user: User | null): Action<State> =>
+    ({ setState }) => {
+      setState({ currentUser: user })
+    }
 }
 
 type Actions = typeof actions
@@ -26,5 +34,7 @@ const Store = createStore<State, Actions>({
   actions,
   containedBy: AppDataContainer
 })
+
+export const StoreInstance = defaultRegistry.getStore(Store)
 
 export const useAppData = createHook(Store)
