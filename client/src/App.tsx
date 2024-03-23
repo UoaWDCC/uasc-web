878a2d202c49add9584847b91e3dd99b95c176ef
bcho892 <AKLbc70@gmail.com>
2024-03-23 20:17:45 +1300

    test

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -22,7 +22,7 @@ import { QueryClientProvider } from "@tanstack/react-query"
 
 function App() {
   return (
-    <QueryClientProvider client={queryClient}>
+    <QueryClientProvider client={}>
       <LocalizationProvider dateAdapter={AdapterDayjs}>
         <div className="h-screen">
           <ThemeProvider theme={theme}>
