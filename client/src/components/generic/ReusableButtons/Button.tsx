3a4acd94fe2886951f5b54b252e3a0c22d0fe3a2
ZL <ray040406@gmail.com>
2024-03-24 22:43:37 +1300

    added hover animation for button v1

--- client/src/components/generic/ReusableButtons/Button.tsx
+++ client/src/components/generic/ReusableButtons/Button.tsx
@@ -3,12 +3,16 @@ type Props = object
 const Button = (props: Props) => {
   return (
     <>
-      <button className="bg-amber-400 hover:bg-cyan-600 p-2 m-8">
-        Button Version 1
+      <button
+        className="bg-transparent hover:bg-blue-200 p-3 m-8
+      font-semibold rounded-full
+      before:ease relative h-12 w-50 overflow-hidden border border-black before:absolute before:left-0 before:-ml-2 before:h-48 before:w-48 before:origin-top-right before:-translate-x-full before:translate-y-12 before:-rotate-90 before:bg-blue-700 before:transition-all before:duration-300 hover:text-white hover:blue hover:before:-rotate-180"
+      >
+        UASC web button v1
       </button>
 
-      <button className="bg-amber-400 hover:bg-cyan-600">
-        Button Version 2
+      <button className="bg-orange-500 hover:bg-orange-200 p-1.5">
+        UASC web button v1
       </button>
     </>
   )
