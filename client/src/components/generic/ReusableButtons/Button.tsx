906c153bb8166b6e01fbe41a31b55a4a40beae69
ZL <ray040406@gmail.com>
2024-03-24 22:18:17 +1300

    set colors, margin and padding to buttons 1 and 2

--- client/src/components/generic/ReusableButtons/Button.tsx
+++ client/src/components/generic/ReusableButtons/Button.tsx
@@ -1,7 +1,17 @@
 type Props = object
 
 const Button = (props: Props) => {
-  return <div>Button Version 1</div>
+  return (
+    <>
+      <button className="bg-amber-400 hover:bg-cyan-600 p-2 m-8">
+        Button Version 1
+      </button>
+
+      <button className="bg-amber-400 hover:bg-cyan-600">
+        Button Version 2
+      </button>
+    </>
+  )
 }
 
 export default Button
