d9a4dddab0d15417a94f78ea6457c59a1c9efe1a
ZL <ray040406@gmail.com>
2024-03-25 21:56:04 +1300

    PS: take out the margin part when using buttons

--- client/src/components/generic/ReusableButtons/Button.tsx
+++ client/src/components/generic/ReusableButtons/Button.tsx
@@ -90,18 +90,18 @@ const Button = (props: Props) => {
       <button
         type="button"
         className="
-      position:relative
-      text-white 
-      bg-gradient-to-r 
-      from-blue-500 via-blue-600 to-blue-700 
-      hover:bg-gradient-to-br 
-      focus:ring-4 
-      focus:outline-none 
-      focus:ring-blue-300 
-      dark:focus:
-      ring-blue-800 
-      shadow-lg shadow-blue-500/50 dark:shadow-lg dark:shadow-blue-800/80 
-      font-medium rounded-lg text-sm px-5 py-2.5 text-center ml-8"
+        position:relative
+        text-white 
+        bg-gradient-to-r 
+        from-blue-500 via-blue-600 to-blue-700 
+        hover:bg-gradient-to-br 
+        focus:ring-4 
+        focus:outline-none 
+        focus:ring-blue-300 
+        dark:focus:
+        ring-blue-800 
+        shadow-lg shadow-blue-500/50 dark:shadow-lg dark:shadow-blue-800/80 
+        font-medium rounded-lg text-sm px-5 py-2.5 text-center ml-8"
       >
         UASC
       </button>
