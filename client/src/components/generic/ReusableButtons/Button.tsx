bfe977261d945bdb44050f28ba8776689cb23f9f
ZL <ray040406@gmail.com>
2024-03-25 21:53:59 +1300

    expand button - used for links or paragraph drop downs

--- client/src/components/generic/ReusableButtons/Button.tsx
+++ client/src/components/generic/ReusableButtons/Button.tsx
@@ -86,6 +86,25 @@ const Button = (props: Props) => {
       >
         <span className="relative z-10">general button v2</span>
       </button>
+
+      <button
+        type="button"
+        className="
+      position:relative
+      text-white 
+      bg-gradient-to-r 
+      from-blue-500 via-blue-600 to-blue-700 
+      hover:bg-gradient-to-br 
+      focus:ring-4 
+      focus:outline-none 
+      focus:ring-blue-300 
+      dark:focus:
+      ring-blue-800 
+      shadow-lg shadow-blue-500/50 dark:shadow-lg dark:shadow-blue-800/80 
+      font-medium rounded-lg text-sm px-5 py-2.5 text-center ml-8"
+      >
+        UASC
+      </button>
     </>
   )
 }
