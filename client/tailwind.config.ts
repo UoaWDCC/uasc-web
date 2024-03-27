c44faf846336826292e7d11ce3569ff608271756
zlrkw11 <ray040406@gmail.com>
2024-03-27 15:55:59 +1300

    adding default 3 colors by design

--- client/tailwind.config.ts
+++ client/tailwind.config.ts
@@ -3,7 +3,15 @@ import type { Config } from "tailwindcss"
 export default {
   content: ["./src/**/*.tsx"],
   theme: {
-    extend: {}
+    extend: {},
+    colors:{
+      'blue': '#283D87',
+      'light blue': '#4088C3',
+      'orange': '#FF6D04',
+    },
+    fontFamily: {
+
+    }
   },
   plugins: []
 } satisfies Config
