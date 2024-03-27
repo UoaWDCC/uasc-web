0292e6158f4c59111c5a42db0a567dbdb62c38eb
zlrkw11 <ray040406@gmail.com>
2024-03-27 16:33:42 +1300

    setting up h1 font

--- client/tailwind.config.ts
+++ client/tailwind.config.ts
@@ -5,30 +5,38 @@ export default {
   theme: {
     extend: {},
     colors: {
-      'blue': "#283D87",
+      blue: "#283D87",
       "light-blue": "#4088C3",
-      'orange': "#FF6D04"
+      orange: "#FF6D04"
     },
     fontFamily: {
-      'sans': 'Inter',
+      sans: "Inter"
     },
     fontSize: {
-      'h1': ['3.125rem', {
-          lineHeight: '3.125rem',
-          letterSpacing: '-0.21875rem',
-          fontWeight: '900',
-          }],
-      'h2': ['', {
-            lineHeight: '',
-            letterSpacing: '',
-            fontWeight: '',
-          }],
-      'h3': ['', {
-          lineHeight: '',
-          letterSpacing: '',
-          fontWeight: '',
-          }],
-
+      h1: [
+        "3.125rem",
+        {
+          lineHeight: "3.125rem",
+          letterSpacing: "-0.21875rem",
+          fontWeight: "900"
+        }
+      ],
+      h2: [
+        "",
+        {
+          lineHeight: "",
+          letterSpacing: "",
+          fontWeight: ""
+        }
+      ],
+      h3: [
+        "",
+        {
+          lineHeight: "",
+          letterSpacing: "",
+          fontWeight: ""
+        }
+      ]
     }
   },
   plugins: []
