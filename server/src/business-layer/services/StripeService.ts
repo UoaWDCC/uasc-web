5fdec3867761628181bf81cb76c370a14913db07
bcho892 <AKLbc70@gmail.com>
2024-03-28 16:55:07 +1300

    Co-authored-by: Albert Sun <asun555@users.noreply.github.com>

--- server/src/business-layer/services/StripeService.ts
+++ server/src/business-layer/services/StripeService.ts
@@ -1,3 +1,31 @@
+import Stripe from "stripe"
+
+const stripe = new Stripe(process.env.STRIPE_API_KEY)
+
 export default class StripeService {
-  // TODO: implement methods
+  public async getProductsWithLookupKey(lookupKey: string) {
+    const result = await stripe.products.search({
+      query: `lookup_key:'${lookupKey}'`
+    })
+    return result.data
+  }
+
+  public async retrieveProducts(limit?: number, startingAfter?: string) {
+    const products = await stripe.products.list({
+      limit,
+      starting_after: startingAfter
+    })
+    return products.data
+  }
+
+  public async getProductById(id: string) {
+    return await stripe.products.retrieve(id)
+  }
+
+  public async getProductByMetadata(key: string, value: string) {
+    const result = await stripe.products.search({
+      query: `metadata['${key}']:'${value}'`
+    })
+    return result.data
+  }
 }
