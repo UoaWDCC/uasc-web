ccf956c36cbb3f6703f5c9212bf36b250abf7700
bcho892 <AKLbc70@gmail.com>
2024-03-28 17:21:34 +1300

    clean up pr

--- client/src/components/composite/SignUpForm/SignUpForm.story.tsx
+++ client/src/components/composite/SignUpForm/SignUpForm.story.tsx
@@ -2,7 +2,6 @@
 import type { Meta, StoryObj } from "@storybook/react"
 
 import SignUpForm from "./SignUpForm"
-import { BrowserRouter } from "react-router-dom"
 
 // 👇 This default export determines where your story goes in the story list
 const meta: Meta<typeof SignUpForm> = {
@@ -13,13 +12,6 @@ export default meta
 type Story = StoryObj<typeof SignUpForm>
 
 export const FirstStory: Story = {
-  decorators: [
-    (Story) => (
-      <BrowserRouter>
-        <Story />
-      </BrowserRouter>
-    )
-  ],
   args: {
     // 👇 The args you need here will depend on your component
   }
