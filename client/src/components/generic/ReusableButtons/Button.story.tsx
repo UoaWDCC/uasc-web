25fe570990f17a7c3d5eb06b9799ab510a7e8708
ZL <ray040406@gmail.com>
2024-03-29 02:26:57 +1300

    added autodocs and descriptions for the buttons

--- client/src/components/generic/ReusableButtons/Button.story.tsx
+++ client/src/components/generic/ReusableButtons/Button.story.tsx
@@ -11,12 +11,15 @@ export default meta
 
 type Story = StoryObj<typeof meta>
 
+/** Button version 1, can be used under most situations */
 export const v1: Story = {
+  tags: ['autodocs'],
   args: {
     variant: "v1",
     children: "test v1 button"
   }
 }
+
 export const comparison: Story = {
   decorators: [
     () => {
@@ -33,21 +36,21 @@ export const comparison: Story = {
     }
   ]
 }
-
+/** Button version 2, same as button version 1 */
 export const v2: Story = {
   args: {
     variant: "v2",
     children: "test v2 button"
   }
 }
-
+/** Button for text expansion / yes or no selection */
 export const small: Story = {
   args: {
     variant: "small",
     children: "test small button"
   }
 }
-
+/** Button used for signing up only */
 export const signup: Story = {
   args: {
     variant: "signup",
