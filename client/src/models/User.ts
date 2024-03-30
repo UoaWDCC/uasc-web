ac5312ddfe23ae2a98f023f68e4b93a88c36c368
bcho892 <bcho892@aucklanduni.ac.nz>
2024-03-30 19:01:52 +1300

    update userclaims type

--- client/src/models/User.ts
+++ client/src/models/User.ts
@@ -3,5 +3,6 @@ import { components } from "./__generated__/schema"
 export type UserAdditionalInfo = components["schemas"]["UserAdditionalInfo"]
 
 export type UserClaims = {
-  role?: "admin" | "member"
+  admin?: boolean
+  member?: boolean
 }
