8f2192cabf81bff7c2233ee584ed32f7d31e4acc
ganznz <loganbellinghamlb@gmail.com>
2024-04-02 22:52:45 +1300

    Create test for .setCustomUserClaim() method

--- server/src/business-layer/services/AuthService.test.ts
+++ server/src/business-layer/services/AuthService.test.ts
@@ -1,5 +1,6 @@
 import { auth } from "business-layer/security/Firebase"
 import AuthService from "./AuthService"
+import { UserRecord } from "firebase-admin/auth"
 
 describe("AuthService Integration Tests", () => {
   it("deletes a user", async () => {
@@ -11,15 +12,28 @@ describe("AuthService Integration Tests", () => {
     } catch {}
     expect(user).toBe(undefined)
   })
-})
 
-describe("AuthService Unit Tests", () => {
   it("creates a user", async () => {
     const createdUser = await new AuthService().createUser("test@gmail.com")
     let user
     try {
-      user = await auth.getUserByEmail("test@gmail.com")
+      user = await auth.getUser(createdUser.uid)
     } catch {}
     expect(createdUser).toEqual(user)
+    expect(user.email).toEqual("test@gmail.com")
+  })
+
+  it("sets custom claim on a user", async () => {
+    const authService: AuthService = new AuthService()
+    let createdUser: UserRecord = await authService.createUser("test2@mail.com")
+
+    try {
+      await authService.setCustomUserClaim(createdUser.uid, "member")
+    } catch {}
+
+    // refresh user record to get access to newly added custom claim
+    createdUser = await auth.getUser(createdUser.uid)
+
+    expect(createdUser.customClaims["member"]).not.toBe(undefined)
   })
 })
