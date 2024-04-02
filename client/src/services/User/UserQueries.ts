202e9cf08cd432dfabd4a205b96dca49bf2cd423
AzizP786 <m.azizp23@gmail.com>
2024-04-03 11:59:51 +1300

    Code cleanup & documentation.

--- client/src/services/User/UserQueries.ts
+++ client/src/services/User/UserQueries.ts
@@ -1,7 +1,5 @@
 import { useQuery } from "@tanstack/react-query"
 import UserService from "./UserService"
-// import { UserAdditionalInfo } from "server/src/data-layer/models/firebase"
-// import FirestoreCollections from "server/src/data-layer/adapters/FirestoreCollections"
 
 export function useUsersQuery() {
   return useQuery({
@@ -9,15 +7,3 @@ export function useUsersQuery() {
     queryFn: () => UserService.getUsers()
   })
 }
-
-// export async function getFilteredUsers(filters: Partial<UserAdditionalInfo>) {
-//   let users = FirestoreCollections.users.get();
-//   if (filters.membership){
-//     users = users.where('membership', '==', filters.membership)
-//   } else if (filters.first_name){
-//     users = users.where('first_name', '==', filters.first_name)
-//   } else if (filters.last_name){
-//     users = users.where('last_name', '==', filters.last_name)
-//   }
-//   return users;
-// }
--- server/src/data-layer/services/UserDataService.ts
+++ server/src/data-layer/services/UserDataService.ts
@@ -34,11 +34,19 @@ export default class UserDataService {
     return snapshot.exists
   }
 
+  /**
+   * Retrieves a list of filtered users based on the provided filters.
+   *
+   * @param filters - An object containing the filters to apply on the users.
+   * @returns filteredUsers - A promise that resolves to an array of filtered users.
+   */
   public async getFilteredUsers(filters: Partial<UserAdditionalInfo>) {
     let users: FirebaseFirestore.Query<
       UserAdditionalInfo,
       FirebaseFirestore.DocumentData
     > = FirestoreCollections.users
+
+    /** Apply filters to the query based on the provided filter properties */
     if (filters.membership) {
       users = users.where("membership", "==", filters.membership)
     } else if (filters.first_name) {
@@ -46,11 +54,16 @@ export default class UserDataService {
     } else if (filters.last_name) {
       users = users.where("last_name", "==", filters.last_name)
     }
+
+    /** Execute the query and retrieve the snapshot */
     const snapshot = await users.get()
-    const fileteredUsers = snapshot.docs.map((user) => {
+
+    /** Map the snapshot documents to an array of filtered users */
+    const filteredUsers = snapshot.docs.map((user) => {
       return { ...user.data(), uid: user.id }
     })
-    return fileteredUsers
+
+    return filteredUsers
   }
 
   // Update
