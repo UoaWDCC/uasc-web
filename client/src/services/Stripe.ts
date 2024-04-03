3707eb269eff7f885df477cafd03ff95817ff6bc
ZL <ray040406@gmail.com>
2024-04-03 23:48:10 +1300

    changed key name to publishable in stripe.ts

--- client/src/services/Stripe.ts
+++ client/src/services/Stripe.ts
@@ -1,3 +1,3 @@
 import { loadStripe } from "@stripe/stripe-js"
 
-export const stripePromise = loadStripe(import.meta.env.VITE_ENV_STRIPE_API_KEY)
+export const stripePromise = loadStripe(import.meta.env.VITE_ENV_STRIPE_PUBLISHABLE_KEY)
