843cc9981d5e0acbaf2708b15765d35c24b7c062
bcho892 <AKLbc70@gmail.com>
2024-04-06 19:24:31 +1300

    check for subroutes

--- client/src/components/composite/Navbar/Navbar.tsx
+++ client/src/components/composite/Navbar/Navbar.tsx
@@ -1,168 +1,38 @@
-import React, { useEffect, useState } from "react"
-import { useLocation } from "react-router-dom"
-import { Typography, Stack, Link, Box, AppBar } from "@mui/material"
-import { auth } from "../../../firebase"
+import Tab from "components/generic/Tab/Tab"
+import { ReactNode } from "react"
+import { NavLink, useLocation } from "react-router-dom"
 
-const Navbar = () => {
-  const homeLocation = "/"
-  const pageLocation = useLocation().pathname
-  const onHomePage = pageLocation === homeLocation
-  const [isVisible, setIsVisible] = useState(!onHomePage)
-  const [isLoggedIn, setIsLoggedIn] = useState(true) // State for login status
-
-  useEffect(() => {
-    if (!onHomePage) {
-      setIsVisible(true)
-      return
-    }
-
-    setIsVisible(false)
-
-    const handleScroll = () => {
-      const currentScrollPos = window.scrollY
-      const proportionOfWindowHeight = window.innerHeight * 0.3
-      currentScrollPos > proportionOfWindowHeight
-        ? setIsVisible(true)
-        : setIsVisible(false)
-    }
-
-    window.addEventListener("scroll", handleScroll)
-
-    return () => window.removeEventListener("scroll", handleScroll)
-  }, [onHomePage])
-
-  // const handleLogin = () => {
-  //   setIsLoggedIn(true)
-  // }
-
-  const handleLogout = async () => {
-    setIsLoggedIn(false)
-    try {
-      await auth.signOut()
-    } catch (error) {
-      console.error("Failed to sign out: ", error)
-    }
-  }
+const WrappedTab = ({
+  children,
+  to,
+  subroutes
+}: {
+  children: ReactNode
+  to: string
+  subroutes?: string[]
+}) => {
+  const { pathname } = useLocation()
 
   return (
-    <Box sx={{ flexGrow: 1 }}>
-      <AppBar
-        position="fixed"
-        sx={{
-          backgroundColor: "#ffffff50",
-          opacity: isVisible ? "1" : "0",
-          pointerEvents: isVisible ? "auto" : "none",
-          transition: "opacity 0.5s ease-in-out"
-        }}
-      >
-        <Stack
-          direction="row"
-          justifyContent="space-between"
-          sx={{
-            paddingX: "32px",
-            paddingY: "8px"
-          }}
-        >
-          <Stack direction="row" alignItems="center">
-            {/* <img
-            src="https://uasc.co.nz/wp-content/uploads/2021/05/UASC-LOGO-White.png"
-            alt="logo"
-            style={{ width: "48px", height: "48px" }}
-          /> */}
-            <Typography
-              variant="h3"
-              align="left"
-              color="primary.quaternary"
-              sx={{ fontWeight: "bold" }}
-            >
-              UASC
-            </Typography>
-          </Stack>
-          <Stack direction="row" spacing={4} alignItems="center">
-            <Link
-              href="/"
-              variant="h6"
-              underline="none"
-              color="common.darkGrey"
-            >
-              Home
-            </Link>
-            <Link
-              href="/about"
-              variant="h6"
-              underline="none"
-              color="common.darkGrey"
-            >
-              About
-            </Link>
-            <Link
-              href="/events"
-              variant="h6"
-              underline="none"
-              color="common.darkGrey"
-            >
-              Events
-            </Link>
-            <Link
-              href="/contact"
-              variant="h6"
-              underline="none"
-              color="common.darkGrey"
-            >
-              Contact
-            </Link>
-            {isLoggedIn ? (
-              <>
-                <Link
-                  href="/booking"
-                  variant="h6"
-                  underline="none"
-                  color="common.darkGrey"
-                >
-                  Bookings
-                </Link>
-                <Link
-                  href="/profile"
-                  variant="h6"
-                  underline="none"
-                  color="common.darkGrey"
-                >
-                  Profile
-                </Link>
-                {/* @ts-ignore */}
-                <Typography
-                  variant="h6"
-                  underline="none"
-                  color="common.darkGrey"
-                  onClick={handleLogout}
-                >
-                  Logout
-                </Typography>
-              </>
-            ) : (
-              <>
-                <Link
-                  href="/register"
-                  variant="h6"
-                  underline="none"
-                  color="common.darkGrey"
-                >
-                  Register
-                </Link>
-                <Link
-                  href="/login"
-                  variant="h6"
-                  underline="none"
-                  color="common.darkGrey"
-                >
-                  Login
-                </Link>
-              </>
-            )}
-          </Stack>
-        </Stack>
-      </AppBar>
-    </Box>
+    <NavLink to={to} state={{ to }}>
+      <Tab disabled={pathname === to || subroutes?.includes(pathname)}>
+        {children}
+      </Tab>
+    </NavLink>
+  )
+}
+
+const Navbar = () => {
+  return (
+    <div className="bg-gray-1 flex p-3">
+      <div className="ml-auto flex gap-6">
+        <WrappedTab to="/">Home</WrappedTab>
+        <WrappedTab to="/bookings">Bookings</WrappedTab>
+        <WrappedTab to="/signups">Signups</WrappedTab>
+        <WrappedTab to="/events">Events</WrappedTab>
+        <WrappedTab to="/about">About</WrappedTab>
+      </div>
+    </div>
   )
 }
 
