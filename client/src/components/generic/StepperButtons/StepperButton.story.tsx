28ebc3adec67a785f3a60391371bc403813ab459
ZL <ray040406@gmail.com>
2024-04-06 15:47:15 +1300

    created base structure for stepper buttons

---
+++ client/src/components/generic/StepperButtons/StepperButton.story.tsx
@@ -0,0 +1,28 @@
+import type { Meta, StoryObj } from "@storybook/react"
+// importing button.tsx as the object
+import StepperButton from "./StepperButton"
+const meta: Meta<typeof StepperButton> = {
+  component: StepperButton,
+  title: "Button",
+  argTypes: {
+    variant: {
+      control: { type: "radio" },
+      options: ["normal"]
+    },
+    children: {
+      name: "content"
+    }
+  }
+}
+
+export default meta
+
+type Story = StoryObj<typeof meta>
+
+export const normal: Story = {
+    tags: ["autodocs"],
+    args: {
+      variant: "normal",
+      children: "test"
+    }
+  }
---
+++ client/src/components/generic/StepperButtons/StepperButton.tsx
@@ -0,0 +1,28 @@
+type buttonVariants = "normal" | "disabled";
+interface props {
+  children?: React.ReactNode
+  variant?: buttonVariants
+  props?: React.ButtonHTMLAttributes<HTMLButtonElement>
+}
+
+const Default = ({ children, props }: props) => {
+  return (
+    <button
+      {...props}
+      className=""
+          
+    >
+      <span className="relative z-10 hover:text-white">{children}</span>
+    </button>
+  )
+}
+
+const StepperButton = ({ children, variant, props }: props) => {
+    switch (variant) {
+      case "normal":
+        return <Default props={props}>{children}</Default>
+    }
+    return <Default props={props}>{children}</Default>
+  }
+
+export default StepperButton
