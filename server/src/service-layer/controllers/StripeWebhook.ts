0b33d7ac9207af0534b097a67fb53b33c3b535e3
Jefffplays2005 <robloxxxx123@gmail.com>
2024-04-07 16:00:41 +1200

    Tidy code Updated some parts of the code, adding route integration tests next.

--- server/src/service-layer/controllers/StripeWebhook.ts
+++ server/src/service-layer/controllers/StripeWebhook.ts
@@ -2,6 +2,10 @@ import { Controller, SuccessResponse, Route, Post, Request } from "tsoa"
 import Stripe from "stripe"
 import UserDataService from "data-layer/services/UserDataService"
 import AuthService from "business-layer/services/AuthService"
+import {
+  CHECKOUT_TYPE_KEY,
+  CHECKOUT_TYPE_VALUES
+} from "business-layer/utils/StripeService"
 
 @Route("webhook")
 export class StripeWebhook extends Controller {
@@ -15,20 +19,29 @@ export class StripeWebhook extends Controller {
       event = stripe.webhooks.constructEvent(
         request.rawBody,
         request.headers["stripe-signature"],
-        // process.env.STRIPE_LOCAL
-        process.env.STRIPE_API_SECRET
+        process.env.STRIPE_LOCAL
+        // process.env.STRIPE_API_SECRET
       )
     } catch (err) {
       console.error(err)
       return this.setStatus(401) // unauthorized request
     }
     // handle successful checkout
+    // console.log(event.type)
     if (event.type === "checkout.session.completed") {
+      console.log("checkout.session.completed")
       const userService = new UserDataService()
       const authService = new AuthService()
       const uid = event.data.object.client_reference_id
-      if (!uid || !(await userService.userDataExists(uid))) this.setStatus(400) // bad request, non existent user
-      if (event.data.object.metadata?.type === "membership") {
+      console.debug(event.data.object.metadata)
+      console.debug(uid)
+
+      if (!uid || !(await userService.getUserData(uid)))
+        return this.setStatus(400) // bad request, non existent user
+      if (
+        event.data.object.metadata[CHECKOUT_TYPE_KEY] ===
+        CHECKOUT_TYPE_VALUES.MEMBERSHIP
+      ) {
         try {
           // need to update firestore
           await userService.editUserData(uid, {
@@ -36,7 +49,11 @@ export class StripeWebhook extends Controller {
           })
           // need to add member claim to user
           await authService.setCustomUserClaim(uid, "member")
+          console.log(
+            "successfully received checkout.session.completed and added membership"
+          )
         } catch (e) {
+          console.error(e)
           return this.setStatus(500) // unknown server error
         }
       }
