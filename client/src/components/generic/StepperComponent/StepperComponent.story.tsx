b0b91619c41333df1330100312745562df50b858
ZL <ray040406@gmail.com>
2024-04-09 23:23:06 +1200

    created stepper component Co-authored-by: Benson Cho <bcho892@users.noreply.github.com>

---
+++ client/src/components/generic/StepperComponent/StepperComponent.story.tsx
@@ -0,0 +1,46 @@
+import { useState } from "react"
+import type { Meta } from "@storybook/react"
+// importing button.tsx as the object
+import Stepper, { StepProps } from "./StepperComponent"
+
+const meta: Meta<typeof Stepper> = {
+  component: Stepper,
+  title: "StepperComponent"
+}
+
+export default meta
+
+// type Story = StoryObj<typeof meta>
+
+export const DefaultStepper = () => {
+  const [currentStep, setCurrentStep] = useState<number>(0)
+
+  const steps: StepProps[] = [
+    {
+      name: "first",
+      index: 0,
+      onClick: () => setCurrentStep(0)
+    },
+    {
+      name: "second",
+      index: 1,
+      onClick: () => setCurrentStep(1)
+    },
+    {
+      name: "third",
+      index: 2,
+      onClick: () => setCurrentStep(2)
+    }
+  ]
+
+  return (
+    <>
+      <Stepper currentStep={currentStep} steps={steps} />
+      <h1>Current Step: {currentStep}</h1>
+      <span className="flex gap-3">
+        <button onClick={() => setCurrentStep(currentStep - 1)}>back</button>
+        <button onClick={() => setCurrentStep(currentStep + 1)}>next</button>
+      </span>
+    </>
+  )
+}
---
+++ client/src/components/generic/StepperComponent/StepperComponent.tsx
@@ -0,0 +1,38 @@
+import StepperButton from "../StepperButtons/StepperButton"
+
+export type StepProps = {
+  name: string
+  index: number
+  onClick: () => void
+}
+
+interface IStepperProps {
+  currentStep: number
+  steps: StepProps[]
+}
+
+/**
+ *  steps={["Details", "Confirmation"]}
+ */
+const Stepper = ({ steps, currentStep }: IStepperProps) => {
+  return (
+    <span >
+      {steps.map((step) => {
+        const { name, index, onClick } = step
+        return (
+          <span key={index} className=" -ml-[16px]">
+            <StepperButton
+              variant={index === 0 ? "first" : "normal"}
+              disabled={currentStep < index}
+              onClick={onClick}
+            >
+              {name}
+            </StepperButton>
+          </span>
+        )
+      })}
+    </span>
+  )
+}
+
+export default Stepper
