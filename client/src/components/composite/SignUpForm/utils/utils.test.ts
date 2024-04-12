8579b066dda0f93d073e4cd3da39a9038c1d1eed
bcho892 <AKLbc70@gmail.com>
2024-04-12 23:20:44 +1200

    add tests

--- client/src/components/composite/SignUpForm/utils/utils.test.ts
+++
@@ -1,7 +0,0 @@
-import { oneLevelUp } from "./Utils"
-
-describe("utils", () => {
-  it("should return the correct path", () => {
-    expect(oneLevelUp("my_route")).toEqual("../my_route")
-  })
-})
