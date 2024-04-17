30dcfdcabead4bd314bded91fdfd13376b123483
ZL <ray040406@gmail.com>
2024-04-18 01:43:24 +1200

    prettier

--- client/src/components/generic/ConfirmationDialog/ConfirmationDialog.tsx
+++ client/src/components/generic/ConfirmationDialog/ConfirmationDialog.tsx
@@ -11,15 +11,18 @@ interface IDialogProps {
 
 type props = IDialogProps
 
-const ConfirmationDialog = ({ title, text, buttonText1, buttonText2 }: props) => {
+const ConfirmationDialog = ({
+  title,
+  text,
+  buttonText1,
+  buttonText2
+}: props) => {
   return (
-    <div className="max-h-[215px] w-full border border px-6 py-7 rounded-md pb-10">
-      <h1 className="sm:text-h2 font-style: w-full text-lg font-bold italic pb-5">
+    <div className="max-h-[215px] w-full rounded-md border border px-6 py-7 pb-10">
+      <h1 className="sm:text-h2 font-style: w-full pb-5 text-lg font-bold italic">
         {title}
       </h1>
-      <p className="pb-5 text-p">
-      {text}
-      </p>
+      <p className="text-p pb-5">{text}</p>
       <span className="flex space-x-4">
         <Button className="">{buttonText1}</Button>
 
@@ -29,12 +32,26 @@ const ConfirmationDialog = ({ title, text, buttonText1, buttonText2 }: props) =>
   )
 }
 
-const Dialog = ({ title, text, variant , buttonText1, buttonText2 }: props) => {
+const Dialog = ({ title, text, variant, buttonText1, buttonText2 }: props) => {
   switch (variant) {
     case "default":
-      return <ConfirmationDialog title={title} text={text} buttonText1={buttonText1} buttonText2={buttonText2}/>
+      return (
+        <ConfirmationDialog
+          title={title}
+          text={text}
+          buttonText1={buttonText1}
+          buttonText2={buttonText2}
+        />
+      )
   }
-  return <ConfirmationDialog title={title} text={text} buttonText1={buttonText1} buttonText2={buttonText2}/>
+  return (
+    <ConfirmationDialog
+      title={title}
+      text={text}
+      buttonText1={buttonText1}
+      buttonText2={buttonText2}
+    />
+  )
 }
 
 export default Dialog
