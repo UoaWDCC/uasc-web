f970d9d71f1473feafd8e47c3728b8a71d5b1d17
ZL <ray040406@gmail.com>
2024-04-18 01:43:06 +1200

    added 2 buttons, roughly adjusted positioning and other details

--- client/src/components/generic/ConfirmationDialog/ConfirmationDialog.tsx
+++ client/src/components/generic/ConfirmationDialog/ConfirmationDialog.tsx
@@ -1,30 +1,40 @@
+import Button from "../FigmaButtons/FigmaButton"
 type dialogVariants = "default"
 
 interface IDialogProps {
   title: string
   text: string
   variant?: dialogVariants
+  buttonText1: string
+  buttonText2: string
 }
 
 type props = IDialogProps
 
-const ConfirmationDialog = ({ title, text }: props) => {
+const ConfirmationDialog = ({ title, text, buttonText1, buttonText2 }: props) => {
   return (
-    <div className="h-215 w-full border border">
-      <h1 className="sm:text-h2 font-style: w-full text-lg font-bold italic">
+    <div className="max-h-[215px] w-full border border px-6 py-7 rounded-md pb-10">
+      <h1 className="sm:text-h2 font-style: w-full text-lg font-bold italic pb-5">
         {title}
       </h1>
+      <p className="pb-5 text-p">
       {text}
+      </p>
+      <span className="flex space-x-4">
+        <Button className="">{buttonText1}</Button>
+
+        <Button className="">{buttonText2}</Button>
+      </span>
     </div>
   )
 }
 
-const Dialog = ({ title, text, variant }: props) => {
+const Dialog = ({ title, text, variant , buttonText1, buttonText2 }: props) => {
   switch (variant) {
     case "default":
-      return <ConfirmationDialog title={title} text={text} />
+      return <ConfirmationDialog title={title} text={text} buttonText1={buttonText1} buttonText2={buttonText2}/>
   }
-  return <ConfirmationDialog title={title} text={text} />
+  return <ConfirmationDialog title={title} text={text} buttonText1={buttonText1} buttonText2={buttonText2}/>
 }
 
 export default Dialog
