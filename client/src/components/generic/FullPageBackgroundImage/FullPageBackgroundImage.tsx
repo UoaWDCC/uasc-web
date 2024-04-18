75c5edf54801d02a317db70bafa0dcc184bcd664
bcho892 <AKLbc70@gmail.com>
2024-04-18 15:29:48 +1200

    disable overscroll

--- client/src/components/generic/FullPageBackgroundImage/FullPageBackgroundImage.tsx
+++ client/src/components/generic/FullPageBackgroundImage/FullPageBackgroundImage.tsx
@@ -8,7 +8,7 @@ interface IFullPageBackgroundImage {
 const FullPageBackgroundImage = ({ children }: IFullPageBackgroundImage) => (
   <div
     className="bg-mountain-background-image 
-         relative z-0 flex
+         relative z-0 -mt-14 flex
           h-screen min-h-screen flex-col
          items-center justify-center
          overflow-hidden bg-cover"
