3531d38789633a2b035ad5cb0e8f43fab17cba07
bcho892 <AKLbc70@gmail.com>
2024-04-18 17:16:05 +1200

    add more tests for login form

--- client/src/components/composite/LoginForm/LoginForm.test.tsx
+++ client/src/components/composite/LoginForm/LoginForm.test.tsx
@@ -2,76 +2,137 @@ import { render, fireEvent, waitFor } from "@testing-library/react"
 import LoginForm, { LoginHandlerArgs, HandlerResponse } from "./LoginForm"
 
 describe("LoginForm", () => {
-  it("submits the form with email and password", async () => {
-    const loginHandler = jest.fn(
-      ({ email, password }: LoginHandlerArgs): Promise<HandlerResponse> => {
-        return Promise.resolve({
-          success: email === "test@example.com" && password === "password"
+  describe("Login Functionality", () => {
+    it("submits the form with email and password", async () => {
+      const loginHandler = jest.fn(
+        ({ email, password }: LoginHandlerArgs): Promise<HandlerResponse> => {
+          return Promise.resolve({
+            success: email === "test@example.com" && password === "password"
+          })
+        }
+      )
+
+      const { getByTestId } = render(<LoginForm loginHandler={loginHandler} />)
+
+      fireEvent.change(getByTestId("email-input"), {
+        target: { value: "test@example.com" }
+      })
+      fireEvent.change(getByTestId("password-input"), {
+        target: { value: "password" }
+      })
+
+      fireEvent.click(getByTestId("login-button"))
+
+      await waitFor(() => {
+        expect(loginHandler).toHaveBeenCalledWith({
+          email: "test@example.com",
+          password: "password"
         })
-      }
-    )
-
-    const { getByTestId } = render(<LoginForm loginHandler={loginHandler} />)
-
-    fireEvent.change(getByTestId("email-input"), {
-      target: { value: "test@example.com" }
-    })
-    fireEvent.change(getByTestId("password-input"), {
-      target: { value: "password" }
-    })
-
-    fireEvent.click(getByTestId("login-button"))
-
-    await waitFor(() => {
-      expect(loginHandler).toHaveBeenCalledWith({
-        email: "test@example.com",
-        password: "password"
       })
     })
+
+    it("displays success message on successful login", async () => {
+      const loginHandler = jest.fn().mockResolvedValue({ success: true })
+
+      const { getByTestId, findByText } = render(
+        <LoginForm loginHandler={loginHandler} />
+      )
+
+      fireEvent.change(getByTestId("email-input"), {
+        target: { value: "test@example.com" }
+      })
+      fireEvent.change(getByTestId("password-input"), {
+        target: { value: "password" }
+      })
+
+      fireEvent.click(getByTestId("login-button"))
+
+      const successMessage = await findByText("Logged In")
+
+      expect(successMessage).toBeInTheDocument()
+    })
+
+    it("displays error message on failed login", async () => {
+      const loginHandler = jest.fn().mockResolvedValue({
+        success: false,
+        error: { message: "Invalid credentials" }
+      })
+
+      const { getByTestId, findByText } = render(
+        <LoginForm loginHandler={loginHandler} />
+      )
+
+      fireEvent.change(getByTestId("email-input"), {
+        target: { value: "test@example.com" }
+      })
+      fireEvent.change(getByTestId("password-input"), {
+        target: { value: "password" }
+      })
+
+      fireEvent.click(getByTestId("login-button"))
+
+      const errorMessage = await findByText("Invalid credentials")
+
+      expect(errorMessage).toBeInTheDocument()
+    })
   })
+  describe("Password reset functionality", () => {
+    let mockLoginHandler: any,
+      mockPasswordResetHandler: any,
+      mockSuccessHandler: any
 
-  it("displays success message on successful login", async () => {
-    const loginHandler = jest.fn().mockResolvedValue({ success: true })
-
-    const { getByTestId, findByText } = render(
-      <LoginForm loginHandler={loginHandler} />
-    )
-
-    fireEvent.change(getByTestId("email-input"), {
-      target: { value: "test@example.com" }
-    })
-    fireEvent.change(getByTestId("password-input"), {
-      target: { value: "password" }
+    beforeEach(() => {
+      mockLoginHandler = jest.fn()
+      mockPasswordResetHandler = jest.fn()
+      mockSuccessHandler = jest.fn()
     })
 
-    fireEvent.click(getByTestId("login-button"))
+    it("calls passwordResetHandler when reset password is clicked", async () => {
+      const { getByTestId } = render(
+        <LoginForm
+          loginHandler={mockLoginHandler}
+          passwordResetHandler={mockPasswordResetHandler}
+          successHandler={mockSuccessHandler}
+        />
+      )
 
-    const successMessage = await findByText("Logged In")
+      fireEvent.click(getByTestId("reset-password-button"))
 
-    expect(successMessage).toBeInTheDocument()
-  })
-
-  it("displays error message on failed login", async () => {
-    const loginHandler = jest.fn().mockResolvedValue({
-      success: false,
-      error: { message: "Invalid credentials" }
+      await waitFor(() => {
+        expect(mockPasswordResetHandler).toHaveBeenCalled()
+      })
     })
 
-    const { getByTestId, findByText } = render(
-      <LoginForm loginHandler={loginHandler} />
-    )
+    it("does not call loginHandler when reset password is clicked", async () => {
+      const { getByTestId } = render(
+        <LoginForm
+          loginHandler={mockLoginHandler}
+          passwordResetHandler={mockPasswordResetHandler}
+          successHandler={mockSuccessHandler}
+        />
+      )
 
-    fireEvent.change(getByTestId("email-input"), {
-      target: { value: "test@example.com" }
+      fireEvent.click(getByTestId("reset-password-button"))
+
+      await waitFor(() => {
+        expect(mockLoginHandler).not.toHaveBeenCalled()
+      })
     })
-    fireEvent.change(getByTestId("password-input"), {
-      target: { value: "password" }
+
+    it("does not call successHandler when reset password is clicked", async () => {
+      const { getByTestId } = render(
+        <LoginForm
+          loginHandler={mockLoginHandler}
+          passwordResetHandler={mockPasswordResetHandler}
+          successHandler={mockSuccessHandler}
+        />
+      )
+
+      fireEvent.click(getByTestId("reset-password-button"))
+
+      await waitFor(() => {
+        expect(mockSuccessHandler).not.toHaveBeenCalled()
+      })
     })
-
-    fireEvent.click(getByTestId("login-button"))
-
-    const errorMessage = await findByText("Invalid credentials")
-
-    expect(errorMessage).toBeInTheDocument()
   })
 })
