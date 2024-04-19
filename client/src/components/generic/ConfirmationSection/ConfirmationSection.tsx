ad65b7245784e94e74f0ea66f4ba0cd9a48c90ee
ZL <ray040406@gmail.com>
2024-04-20 03:00:17 +1200

    added contents, spacing font-styling, and sizing readjustments

--- client/src/components/generic/ConfirmationSection/ConfirmationSection.tsx
+++ client/src/components/generic/ConfirmationSection/ConfirmationSection.tsx
@@ -20,15 +20,25 @@ const confirmationSection = ({
 }: props) => {
   return (
     <div className="">
-      <h1>{mainHeader}</h1>
-      <h3>{subHeader}</h3>
-      <p>{textTop}</p>
-      <p>{textBottom}</p>
-      <span className="">
-        {SvgIcon && (
-          <SvgIcon className="fill-light-blue-100 max-w-[212.55px]" />
-        )}
-      </span>
+      <div className="flex items-center justify-center">
+        <div className="max-w-[204px]">
+          <h2 className="font-style: text-dark-blue-100 pb-[87px] italic">
+            {mainHeader}
+          </h2>
+          <h3>{subHeader}</h3>
+          <p className="pb-[50px]">{textTop}</p>
+          <p>{textBottom}</p>
+        </div>
+        <div className="pl-[71px]">
+          {SvgIcon && (
+            <SvgIcon
+              width="255px"
+              height="255px"
+              className="fill-light-blue-100"
+            />
+          )}
+        </div>
+      </div>
     </div>
   )
 }
