d757060eb6886c0b38665cc51470105082e89f65
zlrkw11 <ray040406@gmail.com>
2024-04-19 18:28:25 +1200

    making onClick functions

--- client/src/components/generic/ConfirmationDialog/ConfirmationDialog.tsx
+++ client/src/components/generic/ConfirmationDialog/ConfirmationDialog.tsx
@@ -7,11 +7,13 @@ export interface IDialogProps {
   variant?: dialogVariants
   left: string
   right: string
+  onClickLeft?: ()=> void
+  onClickRight?: ()=> void 
 }
 
 type props = IDialogProps
 
-const ConfirmationDialog = ({ title, text, left, right }: props) => {
+const ConfirmationDialog = ({ title, text, left, right, onClickLeft, onClickRight }: props) => {
   return (
     <div className="flex max-h-[215px] max-w-[386px] flex-col items-center rounded-md border-2 px-6 py-7 pb-10">
       <div className="flex flex-col">
@@ -20,11 +22,11 @@ const ConfirmationDialog = ({ title, text, left, right }: props) => {
         </h1>
         <p className="text-p pb-5">{text}</p>
         <span className="flex space-x-4">
-          <Button data-testid="dialog-left" className="">
+          <Button data-testid="dialog-left" onClick={onClickLeft} className="">
             {left}
           </Button>
 
-          <Button data-testid="dialog-right" className="">
+          <Button data-testid="dialog-right" onClick={onClickRight} className="">
             {right}
           </Button>
         </span>
