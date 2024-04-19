aa40954bc653925838a9722ee50ef90a9f6965a9
AzizP786 <m.azizp23@gmail.com>
2024-04-20 07:10:15 +1200

    Created pricingService.ts to determine the membership type based on user

---
+++ server/src/data-layer/services/pricingService.ts
@@ -0,0 +1,16 @@
+import { UserAdditionalInfo } from "data-layer/models/firebase"
+import { MembershipType } from "./membershipType"
+
+export default class pricingService {
+  public async getMembershipType(user: UserAdditionalInfo): Promise<MembershipType> {
+    if (user.university && user.returning) {
+      return MembershipType.UoaReturning;
+    } else if (user.university && !user.returning) {
+      return MembershipType.UoaNew;
+    } else if (!user.university && user.returning) {
+      return MembershipType.OtherReturning;
+    } else {
+      return MembershipType.OtherNew;
+    }
+}
+}
