4b4a19c79f64eeb3bb0c718bc389bd824d570568
AzizP786 <m.azizp23@gmail.com>
2024-04-20 15:55:37 +1200

    removed async for getMembershipType function in PricingService.ts

--- server/src/data-layer/services/PricingService.ts
+++ server/src/data-layer/services/PricingService.ts
@@ -2,9 +2,9 @@ import { UserAdditionalInfo } from "data-layer/models/firebase"
 import { MembershipType } from "./membershipType"
 
 export default class PricingService {
-  public async getMembershipType(
+  public getMembershipType(
     user: UserAdditionalInfo
-  ): Promise<MembershipType> {
+  ): MembershipType {
     if (user.university && user.returning) {
       return MembershipType.UoaReturning
     } else if (user.university && !user.returning) {
