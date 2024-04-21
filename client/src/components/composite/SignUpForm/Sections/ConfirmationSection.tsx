3472d47acc9cdaa9270ad6bad5e7eff89ad0c816
ZL <ray040406@gmail.com>
2024-04-21 19:22:27 +1200

    moved confirmation section under signup form folder and deleted unneeded buttons

---
+++ client/src/components/composite/SignUpForm/Sections/ConfirmationSection.tsx
@@ -0,0 +1,47 @@
+import { SvgImport } from "components/utils/types"
+type confirmationVariants = "default"
+
+export interface ISectionProps {
+  SvgIcon?: SvgImport
+  mainHeader: string
+  subHeader: string
+  textTop: string
+  textBottom: string
+  variant?: confirmationVariants
+}
+
+type props = ISectionProps
+
+export const confirmationSection = ({
+  SvgIcon,
+  mainHeader,
+  subHeader,
+  textTop,
+  textBottom
+}: props) => {
+  return (
+    <div className="">
+      <div className="flex flex-row items-center justify-center">
+        <div className="max-w-[204px]">
+          <h2 className="font-style: text-dark-blue-100 pb-[87px] italic">
+            {mainHeader}
+          </h2>
+          <h3>{subHeader}</h3>
+          <p className="pb-[50px]">{textTop}</p>
+          <p>{textBottom}</p>
+        </div>
+        <div className="pl-[71px]">
+          {SvgIcon && (
+            <SvgIcon
+              width="255px"
+              height="255px"
+              className="fill-light-blue-100"
+            />
+          )}
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default confirmationSection
