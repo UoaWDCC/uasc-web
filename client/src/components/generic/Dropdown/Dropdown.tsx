b5428b02ae5e83449e9c0634c91537a269ab5a3b
Albert <asun555@aucklanduni.ac.nz>
2024-04-23 00:14:11 +1200

    Change Dropdown Arrow

--- client/src/components/generic/Dropdown/Dropdown.tsx
+++ client/src/components/generic/Dropdown/Dropdown.tsx
@@ -10,7 +10,19 @@ const Dropdown = ({ ...props }: DropdownProps) => {
         borderColor: "var(--Greys-100, #242424)",
         borderWidth: "1px",
         borderStyle: "solid",
-        paddingLeft: "18px"
+        paddingLeft: "18px",
+        WebkitAppearance: "none",
+        MozAppearance: "none",
+        background: "transparant",
+        backgroundImage: `url("data:image/svg+xml;utf8,<svg fill='black' height='24' viewBox='0 0 24 24' width='24' xmlns='http://www.w3.org/2000/svg'><path d='M7 10l5 5 5-5z'/><path d='M0 0h24v24H0z' fill='none'/></svg>")`,
+        backgroundRepeat: "no-repeat",
+        backgroundPositionX: "100%",
+        backgroundPositionY: "5px",
+        border: "1px solid #dfdfdf",
+        borderRadius: "2px",
+        marginRight: "2rem",
+        padding: "1rem",
+        paddingRight: "2rem"
       }}
       {...props}
     />
