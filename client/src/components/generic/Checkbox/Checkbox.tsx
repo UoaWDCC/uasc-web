be5f8450c9f58b71d584afbecec819a2b8bfa81f
ZL <ray040406@gmail.com>
2024-04-25 01:32:47 +1200

    empty templates

---
+++ client/src/components/generic/Checkbox/Checkbox.tsx
@@ -0,0 +1,13 @@
+/** import react from 'react'
+
+type checkboxVariants = "unclicked" | "clicked"
+
+interface ICheckboxProps {
+    variant?: checkboxVariants
+}
+
+type props = ICheckboxProps & React.InputHTMLAttributes<HTMLInputElement>
+
+export const CheckBox = ({
+    
+}: props); */
