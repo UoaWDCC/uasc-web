e1455dd11e6981dbfd77a3e9a105b061b17d2882
zlrkw11 <64724157+zlrkw11@users.noreply.github.com>
2024-04-25 17:40:48 +1200

    263 confirmation section (#279)
    * setting up templates and storybook for confirmation section
    
    * adjusting svg positioning
    
    * prettier
    
    * prettier
    
    * added contents, spacing font-styling, and sizing readjustments
    
    * setting up templates and storybook for confirmation section
    
    * adjusting svg positioning
    
    * prettier
    
    * prettier
    
    * added contents, spacing font-styling, and sizing readjustments
    
    * importing button component
    
    * finalizing positioning and div arrangements
    
    * finished the testing file for the section. Added the button testing / guide story
    
    * prettier
    
    * moved confirmation section under signup form folder and deleted unneeded buttons
    
    * deleted button onclick story and removed old folder
    
    * deleted 2 files with repeated names
    
    * deleted unneeded files, added prettier check
    
    * fixed header issue
    
    * responsively made the svg hidden when screen size is small
    
    * prettier
    
    * deleted excess title
    
    * testing for readjusting center for the texts
    
    * x
    
    * x
    
    * vertically centered the texts
    
    * moving image to the right side

---
+++ client/src/components/composite/SignUpForm/Sections/ConfirmationSection.story.tsx
@@ -0,0 +1,28 @@
+import type { Meta, StoryObj } from "@storybook/react"
+import TestIcon from "assets/icons/snowboarder.svg?react"
+
+import confirmationSection from "./ConfirmationSection"
+const meta: Meta<typeof confirmationSection> = {
+  component: confirmationSection,
+  argTypes: {
+    variant: {
+      control: { type: "radio" },
+      options: ["default"]
+    }
+  }
+}
+
+export default meta
+type Story = StoryObj<typeof meta>
+
+export const ConfirmationSection: Story = {
+  tags: ["autodocs"],
+  args: {
+    SvgIcon: TestIcon,
+    variant: "default",
+    subHeader: "Thank you!",
+    textTop:
+      "Your application has been received, and you’ll be sent a confirmation email soon.",
+    textBottom: "In the meantime, please set up your login details."
+  }
+}
