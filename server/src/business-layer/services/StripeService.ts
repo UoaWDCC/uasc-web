6990f9f62e07c6228247facd78458eb0bc001c3b
AzizP786 <m.azizp23@gmail.com>
2024-04-26 07:15:13 +1200

    Documented updateProduct method in StripeService.ts

--- server/src/business-layer/services/StripeService.ts
+++ server/src/business-layer/services/StripeService.ts
@@ -58,6 +58,19 @@ export default class StripeService {
     return session.client_secret
   }
 
+  /**
+   * Updates a product with the specified fields.
+   *
+   * @param productId - The ID of the product to update.
+   * @param updateFields - An object containing the fields to update.
+   * @param updateFields.active - (Optional) Whether the product is active or not.
+   * @param updateFields.description - (Optional) The description of the product.
+   * @param updateFields.metadata - (Optional) Additional metadata for the product.
+   * @param updateFields.price - (Optional) The price of the product.
+   * @param updateFields.name - (Optional) The name of the product.
+   *
+   * @returns {Promise<Stripe.Product>} - A promise that resolves to the updated product.
+   */
   public async updateProduct(
     productId: string,
     updateFields: {
@@ -67,27 +80,42 @@ export default class StripeService {
       price?: string
       name?: string
     }
-  ) {
+  ): Promise<Stripe.Product> {
+    /** Create an empty object to store the product update parameters */
     const productUpdateParams: Stripe.ProductUpdateParams = {}
+
+    /** Check if the 'active' field is provided and assign it to the productUpdateParams object */
     if (updateFields.active !== undefined) {
       productUpdateParams.active = updateFields.active
     }
+
+    /** Check if the 'description' field is provided and assign it to the productUpdateParams object */
     if (updateFields.description !== undefined) {
       productUpdateParams.description = updateFields.description
     }
+
+    /** Check if the 'metadata' field is provided and assign it to the productUpdateParams object */
     if (updateFields.metadata !== undefined) {
       productUpdateParams.metadata = updateFields.metadata
     }
+
+    /** Check if the 'price' field is provided and assign it to the productUpdateParams object */
     if (updateFields.price !== undefined) {
       productUpdateParams.default_price = updateFields.price
     }
+
+    /** Check if the 'name' field is provided and assign it to the productUpdateParams object */
     if (updateFields.name !== undefined) {
       productUpdateParams.name = updateFields.name
     }
+
+    /** Update the product with the specified ID using the productUpdateParams object */
     const updatedProduct = await stripe.products.update(
       productId,
       productUpdateParams
     )
+
+    /** Return the updated product */
     return updatedProduct
   }
 }
