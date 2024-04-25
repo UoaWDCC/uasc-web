786f1bed5b11da86a5d9ac5165323cb4638c3b4f
ZL <ray040406@gmail.com>
2024-04-25 22:51:35 +1200

    applied styling from figma

--- client/src/components/generic/Checkbox/Checkbox.tsx
+++ client/src/components/generic/Checkbox/Checkbox.tsx
@@ -10,9 +10,9 @@ type props = ICheckboxProps & React.InputHTMLAttributes<HTMLInputElement>
 const Checkbox = ({ children, ...props }: props) => {
   return (
     <div {...props}>
-      <label htmlFor="vehicle1">
-        <input type="checkbox" id="1" name="Checkbox" />
-        {children}
+      <label htmlFor="vehicle1" className="flex">
+        <input type="checkbox" id="1" name="Checkbox" disabled={false} />
+        <div className="font-size-16 text-p pl-[12px]">{children}</div>
       </label>
     </div>
   )
