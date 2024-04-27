0305b8a982858b88677c313b39af11cd9e9389cc
Kartik-M24 <161120979+Kartik-M24@users.noreply.github.com>
2024-04-27 22:37:28 +1200

    272 frontend create alerts component (#301)
    * Created generic alert components
    
    * Fixed issues with AlertComponents
    
    * Formatted file appropriately
    
    * Added functionality to exit button (if it is active), ensured alerts work for all screen sizes.
    
    * used defined colours for better code maintainability

--- client/tailwind.config.ts
+++ client/tailwind.config.ts
@@ -87,6 +87,15 @@ export default {
             fontWeight: "400"
           }
         ]
+      },
+      screens: {
+        xxs: [{ min: "320px" }],
+        xs: [{ min: "414px" }],
+        sm: [{ min: "640px" }],
+        md: [{ min: "768px" }],
+        lg: [{ min: "1024px" }],
+        xl: [{ min: "1280px" }],
+        "2xl": [{ min: "1536px" }]
       }
     }
   },
