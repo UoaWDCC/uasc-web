5e83786d5191955e84e1d73a1d9c5c920dc329f2
ganznz <loganbellinghamlb@gmail.com>
2024-04-30 16:56:10 +1200

    Replaced components & styling.
    Replaced TextField MUI component with custom generic TextInput component. Added additional styling to page.

--- client/src/components/composite/SignUpForm/Sections/ContactSection.tsx
+++ client/src/components/composite/SignUpForm/Sections/ContactSection.tsx
@@ -1,32 +1,45 @@
-import { TextField } from "@mui/material"
+import { useSignUpFormData } from "store/signupform"
+import TextInput from "components/generic/TextInputComponent/TextInput"
 
 export const ContactSection = () => {
+  const [
+    { emergency_name, emergency_relation, emergency_phone },
+    { updateFormData }
+  ] = useSignUpFormData()
+
+  const parseEmergencyContactInfo = (
+    e: React.ChangeEvent<HTMLInputElement>
+  ) => {
+    const contactInfo: string[] = e.target.value.split(", ")
+    contactInfo[0] && updateFormData({ emergency_name: contactInfo[0] })
+    contactInfo[1] && updateFormData({ emergency_relation: contactInfo[1] })
+    contactInfo[2] && updateFormData({ emergency_phone: contactInfo[2] })
+  }
+
   return (
-    <div>
-      <TextField
-        id="email"
-        name="email"
-        label="Email Address"
-        variant="outlined"
+    <div className="flex max-w-sm flex-col gap-5">
+      <TextInput
         type="email"
+        label="Email"
+        id="Email"
+        placeholder="email@domain.com"
         required
-        size="small"
       />
-      <TextField
-        id="phoneNumber"
-        name="phoneNumber"
-        label="Phone Number"
-        variant="outlined"
+      <TextInput
+        type="tel"
+        label="Mobile Number"
+        id="MobileNumber"
+        placeholder="000 000 0000"
         required
-        size="small"
       />
-      <TextField
-        id="emergencyContact"
-        name="emergencyContact"
-        label="Emergency contact details"
-        variant="outlined"
+      <TextInput
+        type="text"
+        label="Emergency contact info"
+        description="Name, relationship to you, their mobile number"
+        id="EmergencyContactInfo"
+        value={`${emergency_name}, ${emergency_relation}, ${emergency_phone}`}
+        onChange={parseEmergencyContactInfo}
         required
-        size="small"
       />
     </div>
   )
