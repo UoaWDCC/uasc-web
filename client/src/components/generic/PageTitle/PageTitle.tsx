ad97ee161bd069f0e41ca494067793c8b245603c
ZL <ray040406@gmail.com>
2024-04-30 21:17:33 +1200

    removed border for testing

--- client/src/components/generic/PageTitle/PageTitle.tsx
+++ client/src/components/generic/PageTitle/PageTitle.tsx
@@ -5,7 +5,7 @@ type props = IPageTitleProps
 
 export const PageTitle = ({ title }: props) => {
   return (
-    <div className="bg-light-blue-100 flex h-[70px] w-full items-center justify-center border border-black">
+    <div className="bg-light-blue-100 flex h-[70px] w-full items-center justify-center">
       <div className="text-gray-1 italic">
         <h2>{title}</h2>
       </div>
