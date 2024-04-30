18a9509e5babd8f55d2ae3df7482a6be48829e4c
ganznz <loganbellinghamlb@gmail.com>
2024-04-30 16:12:58 +1200

    Dropdown can now have a label

--- client/src/components/generic/Dropdown/Dropdown.tsx
+++ client/src/components/generic/Dropdown/Dropdown.tsx
@@ -1,13 +1,16 @@
 import React from "react"
 import DownArrow from "assets/icons/downarrow.svg"
 
-type DropdownProps = React.SelectHTMLAttributes<HTMLSelectElement>
+type DropdownProps = React.SelectHTMLAttributes<HTMLSelectElement> & {
+  label?: string
+}
 
-const Dropdown = ({ ...props }: DropdownProps) => {
+const Dropdown = ({ label, ...props }: DropdownProps) => {
   return (
     <div
       style={{ position: "relative", display: "inline-block", width: "100%" }}
     >
+      {label && <label>{label}</label>}
       <select
         className="appearance-none rounded border border-gray-300 bg-transparent bg-contain bg-right bg-no-repeat px-4 py-2"
         style={{
