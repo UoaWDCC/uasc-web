1d2b2b7218ccaacac9136a9c66acf729ad1a62b9
ZL <ray040406@gmail.com>
2024-04-30 21:17:07 +1200

    applied all text and background figma designs

--- client/src/components/generic/PageTitle/PageTitle.story.tsx
+++ client/src/components/generic/PageTitle/PageTitle.story.tsx
@@ -11,6 +11,6 @@ type Story = StoryObj<typeof meta>
 export const DefaultPageTitle: Story = {
   tags: ["autodocs"],
   args: {
-    title: "testing page title"
+    title: "Page Title"
   }
 }
--- client/src/components/generic/PageTitle/PageTitle.tsx
+++ client/src/components/generic/PageTitle/PageTitle.tsx
@@ -5,8 +5,10 @@ type props = IPageTitleProps
 
 export const PageTitle = ({ title }: props) => {
   return (
-    <div className="border border-black w-full h-[70px] bg-light-blue-100 flex items-center justify-center">
-      <div className="">{title}</div>
+    <div className="bg-light-blue-100 flex h-[70px] w-full items-center justify-center border border-black">
+      <div className="text-gray-1 italic">
+        <h2>{title}</h2>
+      </div>
     </div>
   )
 }
