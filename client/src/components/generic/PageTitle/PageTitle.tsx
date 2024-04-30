1e06fd081a9fcced99812079d3fe0fb44a29fc66
ZL <ray040406@gmail.com>
2024-04-30 21:09:20 +1200

    applied page title width and height from figma

--- client/src/components/generic/PageTitle/PageTitle.tsx
+++ client/src/components/generic/PageTitle/PageTitle.tsx
@@ -5,7 +5,7 @@ type props = IPageTitleProps
 
 export const PageTitle = ({ title }: props) => {
   return (
-    <div className="border border-black">
+    <div className="border border-black w-full h-[70px]">
       <div className="">{title}</div>
     </div>
   )
