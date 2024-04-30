6e3636499b1c8fb84258af6500fb163c5ac08a55
ZL <ray040406@gmail.com>
2024-04-30 21:10:13 +1200

    text position justified to center

--- client/src/components/generic/PageTitle/PageTitle.tsx
+++ client/src/components/generic/PageTitle/PageTitle.tsx
@@ -5,7 +5,7 @@ type props = IPageTitleProps
 
 export const PageTitle = ({ title }: props) => {
   return (
-    <div className="border border-black w-full h-[70px]">
+    <div className="border border-black w-full h-[70px] bg-light-blue-100 flex items-center justify-center">
       <div className="">{title}</div>
     </div>
   )
