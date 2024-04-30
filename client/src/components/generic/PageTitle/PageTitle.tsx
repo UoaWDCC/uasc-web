b1d0cf8177ca5bbecf94e5320a152e20fb99e29b
ZL <ray040406@gmail.com>
2024-05-01 02:04:12 +1200

    resolving benson's comment

--- client/src/components/generic/PageTitle/PageTitle.tsx
+++ client/src/components/generic/PageTitle/PageTitle.tsx
@@ -6,8 +6,8 @@ type props = IPageTitleProps
 export const PageTitle = ({ title }: props) => {
   return (
     <div className="bg-light-blue-100 flex h-[70px] w-full items-center justify-center">
-      <div className="text-gray-1 italic">
-        <h2>{title}</h2>
+      <div>
+        <h2 className="text-gray-1 italic">{title}</h2>
       </div>
     </div>
   )
