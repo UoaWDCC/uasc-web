64435f932863521387cd9b05ff1a5374d9fda91d
Jefffplays2005 <robloxxxx123@gmail.com>
2024-05-01 16:54:40 +1200

    Change session type.
    Updated to include the correct checkout session type. Also updated to fetch the session type based on the Stripe api checkout sessions fetch.

--- server/src/service-layer/controllers/StripeWebhook.ts
+++ server/src/service-layer/controllers/StripeWebhook.ts
@@ -1,38 +1,66 @@
 import { Controller, SuccessResponse, Route, Post, Request } from "tsoa"
 import Stripe from "stripe"
+import UserDataService from "data-layer/services/UserDataService"
+import AuthService from "business-layer/services/AuthService"
+import {
+  CHECKOUT_TYPE_KEY,
+  CheckoutTypeValues
+} from "business-layer/utils/StripeProductMetadata"
 
 @Route("webhook")
 export class StripeWebhook extends Controller {
   @Post()
   @SuccessResponse(200, "Webhook post received")
   public async receiveWebhook(@Request() request: any): Promise<void> {
-    console.log("webhook received")
-
     const stripe = new Stripe(process.env.STRIPE_API_KEY)
-
+    // ensure security of the endpoint by constructing an event
+    let event: Stripe.Event
     try {
-      const event: Stripe.Event = stripe.webhooks.constructEvent(
+      event = stripe.webhooks.constructEvent(
         request.rawBody,
         request.headers["stripe-signature"],
-        // process.env.STRIPE_LOCAL // test local api secret
-        process.env.STRIPE_API_SECRET
+        process.env.STRIPE_LOCAL
+        // process.env.STRIPE_API_SECRET
       )
-      switch (event.type) {
-        case "payment_intent.succeeded":
-          console.log("payment_intent.succeeded")
-          break
-        case "payment_method.attached":
-          console.log("payment_method.attached")
-          break
-        default:
-          console.log(`Unhandled event type ${event.type}.`)
-      }
-
-      return this.setStatus(200) // set status to 200 as success
     } catch (err) {
       console.error(err)
-      // set status to 400 due to bad request
-      return this.setStatus(400)
+      return this.setStatus(401) // unauthorized request
     }
+    // console.log(event.type)
+    if (event.type === "payment_intent.succeeded") {
+      // console.log("[WEBHOOK] received: payment_intent.succeeded")
+      console.log(request.body)
+      const userService = new UserDataService()
+      const authService = new AuthService()
+      // Stripe PaymentIntent
+      const { id } = event.data.object
+      // Fetch the checkout session from the PaymentIntent ID
+      const stripeSession: Stripe.Checkout.Session =
+        await stripe.checkout.sessions.retrieve(id)
+      const uid = stripeSession.client_reference_id
+
+      if (!uid || !(await userService.getUserData(uid)))
+        return this.setStatus(400) // bad request, non existent user
+      if (
+        event.data.object.metadata[CHECKOUT_TYPE_KEY] ===
+        CheckoutTypeValues.MEMBERSHIP
+      ) {
+        try {
+          // need to update firestore
+          await userService.editUserData(uid, {
+            membership: "member"
+          })
+          // need to add member claim to user
+          await authService.setCustomUserClaim(uid, "member")
+          console.log(
+            "successfully received checkout.session.completed and added membership"
+          )
+        } catch (e) {
+          console.error(e)
+          return this.setStatus(500) // unknown server error
+        }
+      }
+    }
+    return this.setStatus(200) // set status to 200 as success
   }
 }
