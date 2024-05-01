033ce2069b09eb673b31c9b7f83aaf148babbfe7
ZL <ray040406@gmail.com>
2024-05-02 02:29:36 +1200

    added split lines in between

--- client/src/components/generic/Footer/Footer.tsx
+++ client/src/components/generic/Footer/Footer.tsx
@@ -6,7 +6,7 @@ const HorizontalDivider = () => <span className="bg-gray-3 h-[12px] w-[1px]" />
 
 export const Footer = () => {
   return (
-    <div className="text-gray-3 flex h-[56px] w-full items-center justify-center gap-2 border border-black bg-black">
+    <div className="text-gray-3 flex h-[56px] w-full items-center justify-center gap-3 border border-black bg-black">
       <h5 className="uppercase">
         COPYRIGHT {new Date().getFullYear()} University of Auckland Snowsports
         Club
@@ -14,14 +14,17 @@ export const Footer = () => {
       <Link to="/about">
         <h5 className="uppercase">About</h5>
       </Link>
+      <HorizontalDivider />
       <Link to="/faq">
-        <h5 className="uppercase">About</h5>
+        <h5 className="uppercase">faq</h5>
       </Link>
+      <HorizontalDivider />
       <Link to="/contact us">
-        <h5 className="uppercase">About</h5>
+        <h5 className="uppercase">contact us</h5>
       </Link>
+      <HorizontalDivider />
       <Link to="/policy">
-        <h5 className="uppercase">About</h5>
+        <h5 className="uppercase">policy</h5>
       </Link>
       <HorizontalDivider />
       <span className="h-[24px] w-[24px]">
