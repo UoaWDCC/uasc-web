582864bf067428db1fc27fd48a8232f850f46fc4
ZL <ray040406@gmail.com>
2024-05-03 02:17:40 +1200

    added a button from ticket instruction to redirect to a page

--- client/src/components/generic/SignUpNotif/SignUpNotif.tsx
+++ client/src/components/generic/SignUpNotif/SignUpNotif.tsx
@@ -1,11 +1,18 @@
+import { Navigate } from "react-router-dom"
+import Button from "../FigmaButtons/FigmaButton"
+
 export const SignUpNotif = () => {
   return (
-    <div className="text-dark-blue-100 flex flex-col gap-2 items-center justify-center">
+    <div className="text-dark-blue-100 flex flex-col items-center justify-center gap-2">
       <h2 className="text-center italic">Currently unavailable</h2>
       <h4 className="h-[87px] w-[386px] text-center">
         Only paid members can access bookings. Please sign up or wait until your
         membership payment has been proccessed.
       </h4>
+      <Button onClick={() => nav()} />
     </div>
   )
+}
+function nav() {
+  ;<Navigate to=""></Navigate>
 }
