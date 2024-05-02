4b0c24eaea80fcd9d964987f16ded8cb472749b5
zlrkw11 <64724157+zlrkw11@users.noreply.github.com>
2024-05-02 23:00:21 +1200

    271 footer (#318)
    * setting up basic templates for footer. (still lack most props)
    
    * added testing border
    
    * applied width and height from figma designs
    
    * prettier
    
    * testing item positioning
    
    * prettier
    
    * added about as a link
    
    * benson coded this
    
    Co-authored-by: Benson Cho <bcho892@users.noreply.github.com>
    
    * page links
    
    * added split lines in between
    
    * twitter svg icon imported
    
    * added all social media svg icons along with links
    
    * wrapping social media icons as a div to modify as a whole
    
    * wrapping text links as a div, applied same styling
    
    * mobile version justified. text links and social media icons will not show on small screen size
    
    * prettier
    
    * testing for moving elements to the left. need further testing
    
    * multiple adjustments made for: 1. making footer mobile suitable 2. positioning
    
    * adjusted bigger gap for icons
    
    * updated the social media links
    
    Co-authored-by: Benson Cho <bcho892@users.noreply.github.com>
    
    * items centered in md view
    
    * applied footer to homepage
    
    * applied footer to login and home page
    
    * padding 14 from top added for home page's issue with footer
    
    * route name fix
    
    * deleted args regarding to Benson's comment
    
    ---------
    
    Co-authored-by: Benson Cho <bcho892@users.noreply.github.com>

--- client/src/pages/Home/Home.tsx
+++ client/src/pages/Home/Home.tsx
@@ -8,18 +8,24 @@ import {
   pricingBannerContent,
   pricingNote
 } from "./sections/utils/Pricing"
+import { Footer } from "components/generic/Footer/Footer"
 
 const Home = () => {
   return (
     <>
-      <LandingSection />
-      <AboutSection />
-      <BenefitSection benefits={benefits} />
-      <PricingSection
-        pricings={Pricings}
-        note={pricingNote}
-        bannerContent={pricingBannerContent}
-      />
+      <div className="">
+        <LandingSection />
+        <AboutSection />
+        <BenefitSection benefits={benefits} />
+        <PricingSection
+          pricings={Pricings}
+          note={pricingNote}
+          bannerContent={pricingBannerContent}
+        />
+      </div>
+      <div className="pt-14">
+        <Footer />
+      </div>
     </>
   )
 }
