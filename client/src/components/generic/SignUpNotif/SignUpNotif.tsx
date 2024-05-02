7723b39c87494882218eec4f9ea1b955974b4353
ZL <ray040406@gmail.com>
2024-05-03 01:51:40 +1200

    added text content from figma

--- client/src/components/generic/SignUpNotif/SignUpNotif.tsx
+++ client/src/components/generic/SignUpNotif/SignUpNotif.tsx
@@ -2,7 +2,10 @@ export const SignUpNotif = () => {
   return (
     <div>
       <h5>Testing Text Section</h5>
-      <h4></h4>
+      <h4>
+        Only paid members can access bookings. Please sign up or wait until your
+        membership payment has been proccessed.
+      </h4>
     </div>
   )
 }
