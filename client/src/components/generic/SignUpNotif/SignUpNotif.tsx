4d29e21c9d55fd2525e74d2bd4dea9b8d8858b28
ZL <ray040406@gmail.com>
2024-05-03 01:58:44 +1200

    applied italic style to title and changed title name

--- client/src/components/generic/SignUpNotif/SignUpNotif.tsx
+++ client/src/components/generic/SignUpNotif/SignUpNotif.tsx
@@ -1,7 +1,7 @@
 export const SignUpNotif = () => {
   return (
-    <div className="text-dark-blue-100 flex flex-col items-center justify-center">
-      <h2 className="text-center">Testing Text Section</h2>
+    <div className="text-dark-blue-100 flex flex-col gap-2 items-center justify-center">
+      <h2 className="text-center italic">Currently unavailable</h2>
       <h4 className="h-[87px] w-[386px] text-center">
         Only paid members can access bookings. Please sign up or wait until your
         membership payment has been proccessed.
