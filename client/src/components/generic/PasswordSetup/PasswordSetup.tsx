12c715ed5d02d1426f1c894ad5836ed40b53c583
ZL <ray040406@gmail.com>
2024-05-03 23:34:11 +1200

    added all necessary components specified in figma designs

--- client/src/components/generic/PasswordSetup/PasswordSetup.tsx
+++ client/src/components/generic/PasswordSetup/PasswordSetup.tsx
@@ -11,13 +11,16 @@ import TextInput from "../TextInputComponent/TextInput"
 
 export const PasswordSetup = () => {
   return (
-    <div className="flex flex-col">
+    <div className="flex flex-col gap-5 pl-10">
       <div>
         <p>Password</p>
-        <TextInput className="w-[346px]" data-testid="" />
-        <p>Confirm password</p>
-        <TextInput className="w-[346px]" data-testid="" />
+        <TextInput className="max-w-[346px] rounded" />
       </div>
+      <div>
+        <p>Confirm password</p>
+        <TextInput className="max-w-[346px] rounded" />
+      </div>
+      <p>You can reset your password at any time.</p>
     </div>
   )
 }
