0e4437466437d98f215377d15ebf1c1605475327
ZL <ray040406@gmail.com>
2024-05-03 21:45:57 +1200

    fixed w and h according to bensons comment

--- client/src/components/generic/SignUpNotif/SignUpNotif.tsx
+++ client/src/components/generic/SignUpNotif/SignUpNotif.tsx
@@ -5,7 +5,7 @@ export const SignUpNotif = () => {
   return (
     <div className="text-dark-blue-100 flex flex-col items-center justify-center gap-2">
       <h2 className="text-center italic">Currently unavailable</h2>
-      <h4 className="h-[87px] w-[386px] text-center">
+      <h4 className="max-h-[87px] max-w-[386px] text-center">
         Only paid members can access bookings. Please sign up or wait until your
         membership payment has been proccessed.
       </h4>
