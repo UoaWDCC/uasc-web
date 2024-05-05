0017607c593d75d719cd72157ca9c9deaf8536d1
ZL <ray040406@gmail.com>
2024-05-05 17:44:07 +1200

    prettier

--- client/src/components/generic/Calender/CalenderDates.tsx
+++ client/src/components/generic/Calender/CalenderDates.tsx
@@ -9,7 +9,7 @@ type props = IDateProps & React.ButtonHTMLAttributes<HTMLButtonElement>
 
 const CalenderDates = ({ children }: props) => {
   return (
-    <div className="bg-dark-blue-100 h-[38px] max-w-[42px] rounded flex items-center justify-center"> 
+    <div className="bg-dark-blue-100 flex h-[38px] max-w-[42px] items-center justify-center rounded">
       <h4 className="text-white">{children}</h4>
     </div>
   )
