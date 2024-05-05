163cb476966be115a3fde73e60f8e6641925603e
bcho892 <AKLbc70@gmail.com>
2024-05-05 21:39:31 +1200

    run prettier

--- client/src/components/generic/PasswordSetupForm/PasswordSetupForm.test.tsx
+++ client/src/components/generic/PasswordSetupForm/PasswordSetupForm.test.tsx
@@ -3,152 +3,150 @@ import { PasswordSetupForm } from "./PasswordSetupForm"
 import { createRef } from "react"
 
 const MockPasswordSetupFormConsumer = ({
-    mockSetupHandler,
-    mockSuccessHandler
+  mockSetupHandler,
+  mockSuccessHandler
 }: {
-    mockSetupHandler: any
-    mockSuccessHandler: any
+  mockSetupHandler: any
+  mockSuccessHandler: any
 }) => {
-    const formRef = createRef<HTMLFormElement>()
-    const submit = () => {
-        formRef.current?.requestSubmit()
-    }
-    return (
-        <>
-            <PasswordSetupForm
-                passwordSetUpHandler={mockSetupHandler}
-                successHandler={mockSuccessHandler}
-                formRef={formRef}
-            />
-            <button onClick={() => submit()} data-testid="submit-button">
-                submit
-            </button>
-        </>
-    )
+  const formRef = createRef<HTMLFormElement>()
+  const submit = () => {
+    formRef.current?.requestSubmit()
+  }
+  return (
+    <>
+      <PasswordSetupForm
+        passwordSetUpHandler={mockSetupHandler}
+        successHandler={mockSuccessHandler}
+        formRef={formRef}
+      />
+      <button onClick={() => submit()} data-testid="submit-button">
+        submit
+      </button>
+    </>
+  )
 }
 
 describe("PasswordResetForm", () => {
-    it("does not allow submission on weak password", async () => {
-        const passwordSetupHandler = jest
-            .fn()
-            .mockResolvedValue({ success: true, successMessage: "Success" })
-        const successHandler = jest.fn()
-        const { getByTestId } = render(
-            <MockPasswordSetupFormConsumer
-                mockSetupHandler={passwordSetupHandler}
-                mockSuccessHandler={successHandler}
-            />
-        )
+  it("does not allow submission on weak password", async () => {
+    const passwordSetupHandler = jest
+      .fn()
+      .mockResolvedValue({ success: true, successMessage: "Success" })
+    const successHandler = jest.fn()
+    const { getByTestId } = render(
+      <MockPasswordSetupFormConsumer
+        mockSetupHandler={passwordSetupHandler}
+        mockSuccessHandler={successHandler}
+      />
+    )
 
-        fireEvent.change(getByTestId("new-password-input"), {
-            target: { value: "1234" }
-        })
-        fireEvent.click(getByTestId("submit-button"))
+    fireEvent.change(getByTestId("new-password-input"), {
+      target: { value: "1234" }
+    })
+    fireEvent.click(getByTestId("submit-button"))
 
-        await waitFor(() => {
-            expect(passwordSetupHandler).not.toHaveBeenCalled()
-            expect(successHandler).not.toHaveBeenCalled()
-        })
+    await waitFor(() => {
+      expect(passwordSetupHandler).not.toHaveBeenCalled()
+      expect(successHandler).not.toHaveBeenCalled()
+    })
+  })
+
+  it("allows submission only when passwords match and is strong", async () => {
+    const passwordSetupHandler = jest
+      .fn()
+      .mockResolvedValue({ success: true, successMessage: "Success" })
+    const successHandler = jest.fn()
+    const { getByTestId } = render(
+      <MockPasswordSetupFormConsumer
+        mockSetupHandler={passwordSetupHandler}
+        mockSuccessHandler={successHandler}
+      />
+    )
+
+    /**
+     * Valid case
+     */
+    fireEvent.change(getByTestId("new-password-input"), {
+      target: { value: "abcdef12@A" }
     })
 
-    it("allows submission only when passwords match and is strong", async () => {
-        const passwordSetupHandler = jest
-            .fn()
-            .mockResolvedValue({ success: true, successMessage: "Success" })
-        const successHandler = jest.fn()
-        const { getByTestId } = render(
-            <MockPasswordSetupFormConsumer
-                mockSetupHandler={passwordSetupHandler}
-                mockSuccessHandler={successHandler}
-            />
-        )
-
-        /**
-         * Valid case
-         */
-        fireEvent.change(getByTestId("new-password-input"), {
-            target: { value: "abcdef12@A" }
-        })
-
-        fireEvent.change(getByTestId("confirm-password-input"), {
-            target: { value: "abcdef12@A" }
-        })
-
-        expect(getByTestId("submit-button")).toBeInTheDocument()
-
-        fireEvent.click(getByTestId("submit-button"))
-
-        await waitFor(() => {
-            expect(passwordSetupHandler).toHaveBeenCalled()
-            expect(successHandler).toHaveBeenCalled()
-        })
-
-        jest.clearAllMocks()
-
-        /**
-         * Invalid case
-         */
-        fireEvent.change(getByTestId("new-password-input"), {
-            target: { value: "abcdef12@A" }
-        })
-
-        fireEvent.change(getByTestId("confirm-password-input"), {
-            target: { value: "abcdef" }
-        })
-
-        await waitFor(() => {
-            expect(passwordSetupHandler).not.toHaveBeenCalled()
-            expect(successHandler).not.toHaveBeenCalled()
-        })
-
-
-        jest.clearAllMocks()
-
-        /**
-         * Passwords match but is weak
-         */
-        fireEvent.change(getByTestId("new-password-input"), {
-            target: { value: "abcdef" }
-        })
-
-        fireEvent.change(getByTestId("confirm-password-input"), {
-            target: { value: "abcdef" }
-        })
-
-        await waitFor(() => {
-            expect(passwordSetupHandler).not.toHaveBeenCalled()
-            expect(successHandler).not.toHaveBeenCalled()
-        })
+    fireEvent.change(getByTestId("confirm-password-input"), {
+      target: { value: "abcdef12@A" }
     })
 
-    it("should not submit an empty form", async () => {
-        const passwordSetupHandler = jest
-            .fn()
-            .mockResolvedValue({ success: true, successMessage: "Success" })
-        const successHandler = jest.fn()
-        const { getByTestId } = render(
-            <MockPasswordSetupFormConsumer
-                mockSetupHandler={passwordSetupHandler}
-                mockSuccessHandler={successHandler}
-            />
-        )
+    expect(getByTestId("submit-button")).toBeInTheDocument()
 
-        fireEvent.change(getByTestId("new-password-input"), {
-            target: { value: "" }
-        })
+    fireEvent.click(getByTestId("submit-button"))
 
-        fireEvent.change(getByTestId("confirm-password-input"), {
-            target: { value: "" }
-        })
-
-        expect(getByTestId("submit-button")).toBeInTheDocument()
-
-        fireEvent.click(getByTestId("submit-button"))
-
-        await waitFor(() => {
-            expect(passwordSetupHandler).not.toHaveBeenCalled()
-            expect(successHandler).not.toHaveBeenCalled()
-        })
+    await waitFor(() => {
+      expect(passwordSetupHandler).toHaveBeenCalled()
+      expect(successHandler).toHaveBeenCalled()
     })
+
+    jest.clearAllMocks()
+
+    /**
+     * Invalid case
+     */
+    fireEvent.change(getByTestId("new-password-input"), {
+      target: { value: "abcdef12@A" }
+    })
+
+    fireEvent.change(getByTestId("confirm-password-input"), {
+      target: { value: "abcdef" }
+    })
+
+    await waitFor(() => {
+      expect(passwordSetupHandler).not.toHaveBeenCalled()
+      expect(successHandler).not.toHaveBeenCalled()
+    })
+
+    jest.clearAllMocks()
+
+    /**
+     * Passwords match but is weak
+     */
+    fireEvent.change(getByTestId("new-password-input"), {
+      target: { value: "abcdef" }
+    })
+
+    fireEvent.change(getByTestId("confirm-password-input"), {
+      target: { value: "abcdef" }
+    })
+
+    await waitFor(() => {
+      expect(passwordSetupHandler).not.toHaveBeenCalled()
+      expect(successHandler).not.toHaveBeenCalled()
+    })
+  })
+
+  it("should not submit an empty form", async () => {
+    const passwordSetupHandler = jest
+      .fn()
+      .mockResolvedValue({ success: true, successMessage: "Success" })
+    const successHandler = jest.fn()
+    const { getByTestId } = render(
+      <MockPasswordSetupFormConsumer
+        mockSetupHandler={passwordSetupHandler}
+        mockSuccessHandler={successHandler}
+      />
+    )
+
+    fireEvent.change(getByTestId("new-password-input"), {
+      target: { value: "" }
+    })
+
+    fireEvent.change(getByTestId("confirm-password-input"), {
+      target: { value: "" }
+    })
+
+    expect(getByTestId("submit-button")).toBeInTheDocument()
+
+    fireEvent.click(getByTestId("submit-button"))
+
+    await waitFor(() => {
+      expect(passwordSetupHandler).not.toHaveBeenCalled()
+      expect(successHandler).not.toHaveBeenCalled()
+    })
+  })
 })
-
