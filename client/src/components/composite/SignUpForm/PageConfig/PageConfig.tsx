16c7e78fec652e7ebd9e75883f5d1085c61376a5
zlrkw11 <64724157+zlrkw11@users.noreply.github.com>
2024-05-05 23:01:28 +1200

    262 password setup form (#329)
    * pseudo code for main logic, need further implementation
    
    * structure template for storybook and tsx files
    
    * setting up 2 input boxes and titles
    
    * added all necessary components specified in figma designs
    
    * account set up section added with password setup component
    
    Co-authored-by: Benson Cho <bcho892@users.noreply.github.com>
    
    * setting up logic for the password setting up section
    
    * prettier
    
    * prettier
    
    * adding success route, go to success page
    
    * prettier
    
    * made a new div wrapper and styled for spacing
    
    * added styling for the reset password button
    
    * adjusted spacing for the text at the bottom
    
    * made small breakpoint's inputs size full
    
    * prettier
    
    * adjusted gap size to make everything 20px apart and balanced
    
    * prettier
    
    * add tests
    
    * run prettier
    
    * fix tests
    
    * delete unused classNames
    
    * run prettier
    
    ---------
    
    Co-authored-by: Benson Cho <bcho892@users.noreply.github.com>
    Co-authored-by: bcho892 <AKLbc70@gmail.com>

--- client/src/components/composite/SignUpForm/PageConfig/PageConfig.tsx
+++ client/src/components/composite/SignUpForm/PageConfig/PageConfig.tsx
@@ -7,6 +7,7 @@ import { ContactSection } from "../Sections/ContactSection"
 import { AdditionalSection } from "../Sections/AdditionalSection"
 import ConfirmationSection from "../Sections/ConfirmationSection"
 import SuccessSection from "../Sections/SuccessSection"
+
 import {
   ACCOUNT_SETUP_ROUTE,
   ADDITIONAL_ROUTE,
@@ -16,6 +17,7 @@ import {
   PAYMENT_ROUTE,
   PERSONAL_ROUTE_1,
   PERSONAL_ROUTE_2,
+  SUCCESS_ROUTE,
   RouteNames
 } from "../utils/RouteNames"
 import {
@@ -23,6 +25,7 @@ import {
   PaymentSection
 } from "../Sections/PaymentSection"
 import TestIcon from "assets/icons/snowboarder.svg?react"
+import AccountSetupSection from "../Sections/AccountSetupSection"
 
 export const PAGINATED_FORM_PAGES = (
   navigateFn: (route: RouteNames | "/profile" | number) => void
@@ -73,12 +76,14 @@ export const PAGINATED_FORM_PAGES = (
     index: PAGES.AccountSetup,
     title: "Account",
     // goes back to one page earlier in history, otherwise does nothing
-    onBack: () => navigateFn(-1)
+    onBack: () => navigateFn(-1),
     // after setting up the account, the next button is enabled and shows the success section
+    onNext: () => navigateFn(SUCCESS_ROUTE)
   },
   {
     index: PAGES.Success,
     title: "Success",
+    onBack: () => navigateFn(ACCOUNT_SETUP_ROUTE),
     onNext: () => navigateFn("/profile")
   }
 ]
@@ -100,8 +105,7 @@ export const PAGE_CONTENT = [
     textBottom="In the meantime, please set up your login details."
     SvgIcon={TestIcon}
   />,
-  // Account setup section here
-  <></>,
+  <AccountSetupSection key="account" />,
   <SuccessSection key="Success" />
 ]
 
