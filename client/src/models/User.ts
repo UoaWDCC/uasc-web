78aeef98bddce57942413f7984e79763580eae3d
bcho892 <AKLbc70@gmail.com>
2024-05-05 22:12:15 +1200

    update reduceduseradditionalinfo

--- client/src/models/User.ts
+++ client/src/models/User.ts
@@ -3,7 +3,7 @@ import { components } from "./__generated__/schema"
 export type UserAdditionalInfo = components["schemas"]["UserAdditionalInfo"]
 
 export type ReducedUserAdditionalInfo =
-  components["schemas"]["Pick_UserAdditionalInfo.Exclude_keyofUserAdditionalInfo.membership-or-stripe_id__"]
+  components["schemas"]["Pick_UserAdditionalInfo.Exclude_keyofUserAdditionalInfo.stripe_id__"]
 
 export type UserClaims = {
   admin?: boolean
