70372b6146a3289d098e2a9a356c67958a9b4af1
bcho892 <AKLbc70@gmail.com>
2024-05-05 22:17:45 +1200

    revert ts checking - will create new ticket

--- client/src/models/User.ts
+++ client/src/models/User.ts
@@ -2,7 +2,8 @@ import { components } from "./__generated__/schema"
 
 export type UserAdditionalInfo = components["schemas"]["UserAdditionalInfo"]
 
-export type ReducedUserAdditionalInfo = components["schemas"][""]
+export type ReducedUserAdditionalInfo =
+  components["schemas"]["Pick_UserAdditionalInfo.Exclude_keyofUserAdditionalInfo.stripe_id__"]
 
 export type UserClaims = {
   admin?: boolean
