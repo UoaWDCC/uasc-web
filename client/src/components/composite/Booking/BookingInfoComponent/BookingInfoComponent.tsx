3e6336d6d72a1151c7181a03ac0292cbaa2de813
ZL <ray040406@gmail.com>
2024-05-06 17:12:48 +1200

    fixed the part on the last section on the top, by splitting in half, so they will fit

--- client/src/components/composite/Booking/BookingInfoComponent/BookingInfoComponent.tsx
+++ client/src/components/composite/Booking/BookingInfoComponent/BookingInfoComponent.tsx
@@ -4,10 +4,10 @@ const Divider = () => (
 
 const BookingInfoComponent = () => {
   return (
-    <div className="flex h-[474px] max-w-[347px] flex-col items-center border border-gray-3 rounded">
+    <div className="border-gray-3 flex h-[474px] max-w-[347px] flex-col items-center rounded border">
       <div
         id="top"
-        className="text-dark-blue-100 flex flex-col gap-2 pl-8 pr-8 pt-16"
+        className="text-dark-blue-100 flex flex-col gap-2 pr-12 pt-16"
       >
         <h3 className="flex gap-2">
           $40<h3 className="font-normal">per night</h3>
@@ -16,13 +16,18 @@ const BookingInfoComponent = () => {
         <h3 className="flex gap-2">
           $60<h3 className="font-normal">a single Saturday</h3>
         </h3>
-
-        <h3 className="flex gap-2">
-          $60<h3 className="font-normal">per non-member guest, per night</h3>
+        <h3>
+          <h3 className="flex gap-2">
+            $60<h3 className="font-normal">per non-member</h3>
+          </h3>
+          <h3 className="font-normal">guest, per night</h3>
         </h3>
       </div>
       <Divider />
-      <div id="bottom" className="pl-8 pr-8 text-dark-blue-100 flex flex-col gap-4">
+      <div
+        id="bottom"
+        className="text-dark-blue-100 flex flex-col gap-4 pl-8 pr-8"
+      >
         <p>
           Once booking has been selected, please complete payment within 20
           minutes to confirm the booking.
