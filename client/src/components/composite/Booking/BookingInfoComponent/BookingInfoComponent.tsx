b6fe87f1e59c4c25ec7b32939d0228e1d1358728
ZL <ray040406@gmail.com>
2024-05-06 17:06:20 +1200

    styled all text size and weights along with adjustment on the divider

--- client/src/components/composite/Booking/BookingInfoComponent/BookingInfoComponent.tsx
+++ client/src/components/composite/Booking/BookingInfoComponent/BookingInfoComponent.tsx
@@ -1,17 +1,22 @@
 const Divider = () => (
-  <span className="bg-dark-blue-100 m-4 h-[1px] w-[283px]" />
+  <span className="bg-dark-blue-100 m-8 h-[1px] w-[283px]" />
 )
 
 const BookingInfoComponent = () => {
   return (
     <div className="flex h-[474px] max-w-[347px] flex-col items-center border border-black">
-      <div id="top" className="text-dark-blue-100 flex flex-col gap-2 pl-8 pt-16">
+      <div id="top" className="text-dark-blue-100 flex flex-col gap-2 pl-8 pt-16 pr-8">
         <h3 className="flex gap-2">
-          $40<h3>per night</h3>
+          $40<h3 className="font-normal">per night</h3>
         </h3>
 
-        <h3>$60 a single Saturday</h3>
-        <h3>$60 per non-member guest, per night</h3>
+        <h3 className="flex gap-2">
+          $60<h3 className="font-normal">a single Saturday</h3>
+        </h3>
+
+        <h3 className="flex gap-2">
+          $60<h3 className="font-normal">per non-member guest, per night</h3>
+        </h3>
       </div>
       <Divider />
       <div id="bottom" className="pl-4">
