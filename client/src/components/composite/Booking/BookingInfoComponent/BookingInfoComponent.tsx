d8e9d7fb6ef997b85223091aec2276463fc29450
ZL <ray040406@gmail.com>
2024-05-06 16:26:15 +1200

    styled padding for top text section and positioning

--- client/src/components/composite/Booking/BookingInfoComponent/BookingInfoComponent.tsx
+++ client/src/components/composite/Booking/BookingInfoComponent/BookingInfoComponent.tsx
@@ -1,5 +1,14 @@
 const BookingInfoComponent = () => {
-  return <div></div>
+  return (
+    <div className="flex flex-col border border-black max-w-[347px] h-[474px]">
+      <div id="top" className="pl-8 pt-16">
+        <h3>$40 per night</h3>
+        <h3>$60 a single Saturday</h3>
+        <h3>$60 per non-member guest, per night</h3>
+      </div>
+      <div id="bottom"></div>
+    </div>
+  )
 }
 
 export default BookingInfoComponent
