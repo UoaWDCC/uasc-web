67ebb1c345855514319b68a9dc9043df8a1b7695
ZL <ray040406@gmail.com>
2024-05-06 16:40:31 +1200

    prettier

--- client/src/components/composite/Booking/BookingInfoComponent/BookingInfoComponent.tsx
+++ client/src/components/composite/Booking/BookingInfoComponent/BookingInfoComponent.tsx
@@ -1,7 +1,7 @@
 const BookingInfoComponent = () => {
   return (
     <div className="flex h-[474px] max-w-[347px] flex-col border border-black">
-      <div id="top" className="pl-8 pt-16 text-dark-blue-100">
+      <div id="top" className="text-dark-blue-100 pl-8 pt-16">
         <h3>$40 per night</h3>
         <h3>$60 a single Saturday</h3>
         <h3>$60 per non-member guest, per night</h3>
