6a756165c7caf6ea3972c0a6f92f07dfc007d998
ZL <ray040406@gmail.com>
2024-05-06 16:54:35 +1200

    balanced and centered the middle divider

--- client/src/components/composite/Booking/BookingInfoComponent/BookingInfoComponent.tsx
+++ client/src/components/composite/Booking/BookingInfoComponent/BookingInfoComponent.tsx
@@ -1,12 +1,26 @@
+const Divider = () => (
+  <span className="bg-dark-blue-100 m-4 h-[1px] w-[283px]" />
+)
+
 const BookingInfoComponent = () => {
   return (
-    <div className="flex h-[474px] max-w-[347px] flex-col border border-black">
-      <div id="top" className="text-dark-blue-100 pl-8 pt-16">
+    <div className="flex h-[474px] max-w-[347px] flex-col items-center border border-black">
+      <div
+        id="top"
+        className="text-dark-blue-100 flex flex-col gap-2 pl-8 pt-16"
+      >
         <h3>$40 per night</h3>
         <h3>$60 a single Saturday</h3>
         <h3>$60 per non-member guest, per night</h3>
       </div>
-      <div id="bottom"></div>
+      <Divider />
+      <div id="bottom" className="pl-4">
+        <p>
+          Once booking has been selected, please complete payment within 20
+          minutes to confirm the booking.
+        </p>
+        <p>Please read UASC policy below before booking.</p>
+      </div>
     </div>
   )
 }
