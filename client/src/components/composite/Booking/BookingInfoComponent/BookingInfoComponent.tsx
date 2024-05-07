e9145e7c5f49f19761e1a28887d5bf1174c80ad7
ZL <ray040406@gmail.com>
2024-05-07 21:36:25 +1200

    changed the price names according to BENSON's comments

--- client/src/components/composite/Booking/BookingInfoComponent/BookingInfoComponent.tsx
+++ client/src/components/composite/Booking/BookingInfoComponent/BookingInfoComponent.tsx
@@ -1,32 +1,36 @@
 interface IBookingInfoProps {
-  price1: string
-  price2: string
-  price3: string
+  pricePerNight: string
+  priceSaturday: string
+  priceNonMember: string
 }
 
 type props = IBookingInfoProps
 
 const Divider = () => <span className="bg-dark-blue-100 m-8 h-[1px] w-3/4" />
 
-const BookingInfoComponent = ({ price1, price2, price3 }: props) => {
+const BookingInfoComponent = ({
+  pricePerNight,
+  priceSaturday,
+  priceNonMember
+}: props) => {
   return (
     <div className="border-gray-3 flex h-fit w-full flex-col rounded border pb-4 lg:items-center lg:justify-center">
       <div
         id="top"
-        className="text-dark-blue-100 flex flex-col gap-2 pl-8 pr-4.5 pt-16"
+        className="text-dark-blue-100 pr-4.5 flex flex-col gap-2 pl-8 pt-16"
       >
         <h3 className="flex gap-2">
-          ${price1}
+          ${pricePerNight}
           <h3 className="font-normal">per night</h3>
         </h3>
 
         <h3 className="flex gap-2">
-          ${price2}
+          ${priceSaturday}
           <h3 className="font-normal">a single Saturday</h3>
         </h3>
         <h3>
           <h3 className="flex gap-2">
-            ${price3}
+            ${priceNonMember}
             <h3 className="font-normal">per non-member</h3>
           </h3>
           <h3 className="font-normal">guest, per night</h3>
