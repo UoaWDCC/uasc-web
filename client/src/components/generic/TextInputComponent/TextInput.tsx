9241880f5c5414bf498e56a0c0f9f3a1a50ad65a
Logan <78389560+ganznz@users.noreply.github.com>
2024-05-09 11:26:48 +1200

    261 style signup sections (#312)
    * Changed disabled prop on Radio component to optional
    
    * Changed the title property of some pages
    
    * TextInput component can now have label & description text
    
    * Replaced components.
    
    MUI TextField, Select & MenuItem now replaced by custom TextInput & Radio
    
    * Replaced components in PersonalSectionSecond.
    
    Replaced MUI TextField, Select & MenuItem components with custom TextInput & Dropdown components
    
    * Dropdown can now have a label
    
    * Replaced components & styling.
    
    Replaced TextField MUI component with custom generic TextInput component. Added additional styling to page.
    
    * Fixed Radio components not tracking changes on onChange event.
    
    * Replaced components in AdditionalSection.
    
    Replaced MenuItem, Select & TextField MUI components with custom generic Dropdown component.
    
    * Dropdowns in PersonalSection now display placeholder values.
    
    * Dropdown component now takes description prop.
    
    To display a 'description' above the input, and below the label.
    
    * Removed code preventing push to remote repo
    
    * merge emergency contact info
    
    * Fixed date_of_birth parsing functionality throwing errors
    
    * Added additional styling
    
    * emergency_contact is now merged into one field
    
    * Dropdown component is now fully controlled
    
    * TextInput component is now fully controlled
    
    * Changed dropdowns default value text
    
    * Fixed cursor resetting when editing input value.
    
    -- Changed value prop to defaultValue when creating components.
    
    * Removed unnecessary prop & dropdown can now show placeholder value
    
    * Removed unnecessary props
    
    ---------
    
    Co-authored-by: bcho892 <AKLbc70@gmail.com>

--- client/src/components/generic/TextInputComponent/TextInput.tsx
+++ client/src/components/generic/TextInputComponent/TextInput.tsx
@@ -2,9 +2,16 @@ import React from "react"
 
 type TextInputProps = React.InputHTMLAttributes<HTMLInputElement> & {
   variant?: "success" | "error"
+  label?: string
+  description?: string
 }
 
-const TextInput = ({ variant, ...props }: TextInputProps) => {
+const TextInput = ({
+  variant,
+  label,
+  description,
+  ...props
+}: TextInputProps) => {
   const borderColor =
     variant === "success"
       ? "var(--Other-Green, #109D27)"
@@ -15,11 +22,21 @@ const TextInput = ({ variant, ...props }: TextInputProps) => {
   const borderWidth = variant ? "2px" : "1px"
 
   return (
-    <input
-      className={`flex-shrink:0 border-radius: 0.25rem h-9 w-full rounded ${variant}`}
-      style={{ borderColor, borderWidth, paddingLeft }}
-      {...props}
-    />
+    <div>
+      {label && (
+        <label className="text-black" htmlFor={{ ...props }.id}>
+          {label}
+        </label>
+      )}
+      {description && (
+        <p className="text-dark-blue-60 mb-1 text-xs">{description}</p>
+      )}
+      <input
+        className={`flex-shrink:0 border-radius: 0.25rem h-9 w-full rounded ${variant}`}
+        style={{ borderColor, borderWidth, paddingLeft }}
+        {...props}
+      />
+    </div>
   )
 }
 
