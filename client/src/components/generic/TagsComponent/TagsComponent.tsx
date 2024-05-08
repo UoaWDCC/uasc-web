e96acc2a42bc879387d89083af407e7cf3e67232
ZL <ray040406@gmail.com>
2024-05-08 21:43:09 +1200

    adding h5 texts

--- client/src/components/generic/TagsComponent/TagsComponent.tsx
+++ client/src/components/generic/TagsComponent/TagsComponent.tsx
@@ -4,8 +4,12 @@ export interface TagProps extends HTMLAttributes<HTMLOrSVGElement> {
   children: string
 }
 
-const TagsComponent = () => {
-  return <div className="h-[32px] w-[80px] rounded border border-black"></div>
+const PrimaryTagsComponent = () => {
+  return (
+    <div className="h-[32px] w-[80px] rounded border border-black">
+      <h5>1 of 12</h5>
+    </div>
+  )
 }
 
-export default TagsComponent
+export default PrimaryTagsComponent
