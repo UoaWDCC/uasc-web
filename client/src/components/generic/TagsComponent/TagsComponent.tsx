a8a902c14d014496f0ffe2ad38d1b60a2e06fc76
ZL <ray040406@gmail.com>
2024-05-08 21:46:16 +1200

    setting font weight to 700

--- client/src/components/generic/TagsComponent/TagsComponent.tsx
+++ client/src/components/generic/TagsComponent/TagsComponent.tsx
@@ -7,7 +7,7 @@ export interface TagProps extends HTMLAttributes<HTMLOrSVGElement> {
 const PrimaryTagsComponent = () => {
   return (
     <div className="bg-light-blue-60 flex h-[32px] w-[80px] items-center justify-center rounded-full border border-black">
-      <h5>1 of 12</h5>
+      <h5 className="font-bold">1 OF 12</h5>
     </div>
   )
 }
