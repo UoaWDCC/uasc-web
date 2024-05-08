5d3225044f878f6550e4e73f5f465d62ca4439c8
rxia357 <100566786+rxia357@users.noreply.github.com>
2024-05-09 02:28:10 +1200

    basic set up
    added images, built skeleton code

--- client/src/pages/Home/sections/LandingSection.tsx
+++ client/src/pages/Home/sections/LandingSection.tsx
@@ -1,15 +1,17 @@
-import UASCHeader from "assets/logos/UASCTextLogo.png"
+import UASCHeader from "assets/logos/2024logo.png"
 import Button from "components/generic/FigmaButtons/FigmaButton"
+import Facebook from "assets/logos/facebookBlue.png"
+import Instagram from "assets/logos/instagramBlue.png"
 import { Link } from "react-router-dom"
 
 const LandingSection = () => (
   <section>
     <div
       className="bg-home-ski-image relative
-         z-10 -mt-14 flex
-          min-h-screen flex-col 
-         items-center
-         justify-center overflow-hidden bg-cover bg-top bg-no-repeat"
+         -mt-14 flex
+         min-h-screen flex-col
+         items-center justify-center
+         overflow-hidden bg-cover bg-top bg-no-repeat"
     >
       <div className="bg-gray-1 pointer-events-none absolute -z-20 h-screen w-full opacity-90" />
       <div className="z-10 flex flex-col items-center gap-8 text-center">
@@ -20,15 +22,35 @@ const LandingSection = () => (
             className=" pointer-events-none"
           />
         </div>
-        <h3 className="text-dark-blue-100 lg:w-1/3 ">
-          We are UASC, The largest sports club on campus
-        </h3>
-        <h5 className="text-dark-blue-100 uppercase">
-          Cheapest Membership on the mountain
-        </h5>
-        <Link to="/register" className="cursor-pointer">
-          <Button>Sign up now!</Button>
-        </Link>
+
+        {/*<div className="ml-auto hidden items-center gap-6 pr-[32px] lg:flex">*/}
+          <h3 className="text-dark-blue-100 font-normal tracking-tighter">
+            The largest sports club on campus, and <br/> the cheapest membership on Mt Ruapehu!
+          </h3>
+
+          {/*<span>*/}
+          {/*  <a href="https://www.instagram.com/uasc_nz/" target="blank">*/}
+          {/*    <Instagram/>*/}
+          {/*  </a>*/}
+          {/*</span>*/}
+
+          {/*<span className="h-[24px] w-[24px]">*/}
+          {/*  <a href="https://www.facebook.com/UoAsnowsports/" target="blank">*/}
+          {/*    <Facebook/>*/}
+          {/*  </a>*/}
+          {/*</span>*/}
+        {/*</div>*/}
+
+
+        <div className="flex flex-row">
+          <Link to="/register" className="cursor-pointer">
+            <Button>Sign up now!</Button>
+          </Link>
+          <Link to="/Login" className="cursor-pointer">
+            <Button>LOG IN</Button>
+          </Link>
+        </div>
+
       </div>
 
       <h5 className="text-dark-blue-100 absolute bottom-3 uppercase sm:bottom-9">
