51bb02ae53114aeb9aadc559b31d2d4e5ce20eca
bcho892 <AKLbc70@gmail.com>
2024-05-09 14:25:18 +1200

    remove unused import

--- client/src/components/generic/TextInputComponent/TextInput.tsx
+++ client/src/components/generic/TextInputComponent/TextInput.tsx
@@ -1,4 +1,4 @@
-import React, { ChangeEventHandler } from "react"
+import React from "react"
 
 type TextInputProps = React.InputHTMLAttributes<HTMLInputElement> & {
   variant?: "success" | "error"
