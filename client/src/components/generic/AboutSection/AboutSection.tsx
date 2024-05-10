4680db16abd59e474be973c2cd8574fded271dfc
ZL <ray040406@gmail.com>
2024-05-11 01:38:19 +1200

    same border for text section

--- client/src/components/generic/AboutSection/AboutSection.tsx
+++ client/src/components/generic/AboutSection/AboutSection.tsx
@@ -13,7 +13,7 @@ const AboutSection = ({ title, children, image }: props) => {
   return (
     <div>
       <div className="h-[500px] w-[905px] border border-black">{image}</div>
-      <div>
+      <div className="h-[278px] w-[530px] border border-black">
         {title}
         <p>{children}</p>
       </div>
