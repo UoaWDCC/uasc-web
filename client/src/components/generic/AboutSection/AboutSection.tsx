4270984bf5c886833158bad24b2e026542da0adc
ZL <ray040406@gmail.com>
2024-05-12 11:37:38 +1200

    modifying width

--- client/src/components/generic/AboutSection/AboutSection.tsx
+++ client/src/components/generic/AboutSection/AboutSection.tsx
@@ -12,8 +12,8 @@ type props = IAboutProps
 const AboutSection = ({ title, children, image }: props) => {
   return (
     <div>
-      <div className="h-[500px] w-[905px] border border-black">{image}</div>
-      <div className="h-[278px] w-[530px] border border-black">
+      <div className="h-[500px] w-3/4 border border-black">{image}</div>
+      <div className="h-[278px] w-[530px] border border-black pl-[650px]">
         {title}
         <p>{children}</p>
       </div>
