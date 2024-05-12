3330160b93a299f771af0069c68b8733fecbc3f0
ZL <ray040406@gmail.com>
2024-05-12 23:20:58 +1200

    resetted all the widths and height relative positionings using percentages, so everything will be responsive.

--- client/src/components/generic/AboutSection/AboutSection.tsx
+++ client/src/components/generic/AboutSection/AboutSection.tsx
@@ -12,12 +12,12 @@ type Props = IAboutProps
 const AboutSection = ({ title, children, imageSrc }: Props) => {
   return (
     <div className="relative w-full border border-black ">
-      <div className="absolute left-0 w-3/4 border border-black">
+      <div className="relative left-0 w-3/4 border border-black">
         <img src={imageSrc} />
-      </div>
-      <div className="absolute right-[150px] top-[350px] flex h-[188px] w-1/3 flex-col gap-4 border border-black bg-white">
-        <h3 className="text-dark-blue-100 ml-8 mr-8 mt-4">{title}</h3>
-        <p className="text-dark-blue-100 mb-8 ml-8 mr-8">{children}</p>
+        <div className="absolute left-[72%] top-[75%] flex w-[52%] h-[56%] flex-col gap-4 border border-black bg-white">
+          <h3 className="text-dark-blue-100 ml-8 mr-8 mt-4">{title}</h3>
+          <p className="text-dark-blue-100 mb-8 ml-8 mr-8">{children}</p>
+        </div>
       </div>
     </div>
   )
