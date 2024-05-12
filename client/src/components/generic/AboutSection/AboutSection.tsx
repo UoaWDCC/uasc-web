c504293baf541ed4bedc0a93c7d706f4bb15e172
ZL <ray040406@gmail.com>
2024-05-12 23:34:14 +1200

    rounded top right corners for both

--- client/src/components/generic/AboutSection/AboutSection.tsx
+++ client/src/components/generic/AboutSection/AboutSection.tsx
@@ -12,8 +12,8 @@ type Props = IAboutProps
 const AboutSection = ({ title, children, imageSrc }: Props) => {
   return (
     <div className="relative left-0 w-3/4 border border-black">
-      <img src={imageSrc} />
-      <div className="absolute left-[72%] top-[70%] flex h-[56%] w-[52%] flex-col gap-4 overflow-hidden border border-black bg-white">
+      <img src={imageSrc} className="rounded-tr-lg"/>
+      <div className="absolute left-[72%] top-[70%] flex h-[56%] w-[52%] flex-col gap-4 overflow-hidden border border-black bg-white rounded-tr-lg">
         <h3 className="text-dark-blue-100 ml-8 mr-8 mt-4">{title}</h3>
         <p className="text-dark-blue-100 mb-8 ml-8 mr-8">{children}</p>
       </div>
