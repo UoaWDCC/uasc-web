d6c8689dc5d1dde32be58ac8d1a1db1fd5ac22ac
ZL <ray040406@gmail.com>
2024-05-12 23:48:29 +1200

    made 3 different viewports to make this responsive in all possible ways. Focusing on mobile views

--- client/src/components/generic/AboutSection/AboutSection.tsx
+++ client/src/components/generic/AboutSection/AboutSection.tsx
@@ -11,11 +11,15 @@ type Props = IAboutProps
 
 const AboutSection = ({ title, children, imageSrc }: Props) => {
   return (
-    <div className="relative left-0 w-3/4 border border-black">
+    <div className="relative left-0 w-[71%] border border-black">
       <img src={imageSrc} className="rounded-tr-lg" />
-      <div className="absolute left-[72%] top-[70%] flex h-[56%] w-[52%] flex-col gap-4 overflow-hidden rounded-tr-lg border border-black bg-white">
-        <h3 className="text-dark-blue-100 ml-8 mr-8 mt-4">{title}</h3>
-        <p className="text-dark-blue-100 mb-8 ml-8 mr-8">{children}</p>
+      <div className=" absolute top-[70%] flex w-full flex-col overflow-hidden rounded-tr-lg border border-black bg-white md:left-[72%] md:h-[56%] md:w-[52%] lg:left-[72%] lg:h-[56%] lg:w-[52%] lg:gap-4">
+        <p className="lg:text-h2 text-dark-blue-100 font-bold md:ml-2 md:mr-2 md:mt-1 lg:ml-8 lg:mr-8 lg:mt-4">
+          {title}
+        </p>
+        <p className=" text-dark-blue-100 sm:text-xs md:ml-2 md:mr-2 md:mt-1 md:text-xs lg:mb-8 lg:ml-8 lg:mr-8">
+          {children}
+        </p>
       </div>
     </div>
   )
