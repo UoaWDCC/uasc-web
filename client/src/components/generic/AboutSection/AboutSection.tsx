f7ebfd29e324b77bf10d42b5aae119fb7f61744b
ZL <ray040406@gmail.com>
2024-05-13 02:26:45 +1200

    adding function for returning different component based on left / right variant

--- client/src/components/generic/AboutSection/AboutSection.tsx
+++ client/src/components/generic/AboutSection/AboutSection.tsx
@@ -9,20 +9,36 @@ interface IAboutProps {
 
 type Props = IAboutProps
 
-const AboutSection = ({ title, children, imageSrc }: Props) => {
-  return (
-    <div className="relative left-0 w-[71%] border border-black">
-      <img src={imageSrc} className="rounded-tr-lg" />
-      <div className=" absolute top-[70%] flex w-full flex-col overflow-hidden rounded-tr-lg border border-black bg-white md:left-[72%] md:h-[56%] md:w-[52%] lg:left-[72%] lg:h-[56%] lg:w-[52%] lg:gap-4">
-        <p className="lg:text-h2 text-dark-blue-100 ml-2 mr-2 mt-1 font-bold md:ml-2 md:mr-2 md:mt-1 lg:ml-8 lg:mr-8 lg:mt-4">
-          {title}
-        </p>
-        <p className=" text-dark-blue-100 lg:text-p ml-2 mr-2 mt-1 text-xs md:ml-2 md:mr-2 md:mt-1 md:text-xs lg:mb-8 lg:ml-8 lg:mr-8">
-          {children}
-        </p>
+const AboutSection = ({ title, children, imageSrc, variant }: Props) => {
+  if (variant === "left") {
+    return (
+      <div className="relative left-0 w-[71%] border border-black">
+        <img src={imageSrc} className="rounded-tr-lg" />
+        <div className=" absolute top-[70%] flex w-full flex-col overflow-hidden rounded-tr-lg border border-black bg-white md:left-[72%] md:h-[56%] md:w-[52%] lg:left-[72%] lg:h-[56%] lg:w-[52%] lg:gap-4">
+          <p className="lg:text-h2 text-dark-blue-100 ml-2 mr-2 mt-1 font-bold md:ml-2 md:mr-2 md:mt-1 lg:ml-8 lg:mr-8 lg:mt-4">
+            {title}
+          </p>
+          <p className=" text-dark-blue-100 lg:text-p ml-2 mr-2 mt-1 text-xs md:ml-2 md:mr-2 md:mt-1 md:text-xs lg:mb-8 lg:ml-8 lg:mr-8">
+            {children}
+          </p>
+        </div>
       </div>
-    </div>
-  )
+    )
+  } else {
+    return (
+      <div className="relative left-0 w-[71%] border border-black">
+        <img src={imageSrc} className="rounded-tr-lg" />
+        <div className=" absolute top-[70%] flex w-full flex-col overflow-hidden rounded-tr-lg border border-black bg-white md:left-[72%] md:h-[56%] md:w-[52%] lg:left-[72%] lg:h-[56%] lg:w-[52%] lg:gap-4">
+          <p className="lg:text-h2 text-dark-blue-100 ml-2 mr-2 mt-1 font-bold md:ml-2 md:mr-2 md:mt-1 lg:ml-8 lg:mr-8 lg:mt-4">
+            {title}
+          </p>
+          <p className=" text-dark-blue-100 lg:text-p ml-2 mr-2 mt-1 text-xs md:ml-2 md:mr-2 md:mt-1 md:text-xs lg:mb-8 lg:ml-8 lg:mr-8">
+            {children}
+          </p>
+        </div>
+      </div>
+    )
+  }
 }
 
 export default AboutSection
