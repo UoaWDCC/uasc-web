8caf4efa52c98b6bc03b5de9bb498c9c3703a6a5
ZL <ray040406@gmail.com>
2024-05-13 02:56:54 +1200

    adjustments for title and text, differs from left variant, fully responsive

--- client/src/components/generic/AboutSection/AboutSection.tsx
+++ client/src/components/generic/AboutSection/AboutSection.tsx
@@ -28,11 +28,11 @@ const AboutSection = ({ title, children, imageSrc, variant }: Props) => {
     return (
       <div className="relative left-[20%] w-[71%] border border-black">
         <img src={imageSrc} className="rounded-tl-lg" />
-        <div className=" absolute top-[100%] flex w-full flex-col overflow-hidden rounded-t-lg border border-black bg-white md:left-[-25%] md:h-[78%] md:w-[74%] lg:right-[72%] lg:top-[60%] lg:h-[50%] lg:w-[50%] lg:gap-4">
-          <p className="lg:text-h2 text-dark-blue-100 ml-2 mr-2 mt-1 font-bold italic md:ml-2 md:mr-2 md:mt-1 lg:ml-8 lg:mr-8 lg:mt-4">
+        <div className=" absolute top-[100%] flex w-full flex-col overflow-hidden rounded-t-lg border border-black bg-white md:left-[-25%] md:h-[45%] md:w-[74%] lg:left-[-19%] md:top-[60%] lg:top-[60%] lg:h-[45%] lg:w-[50%] lg:gap-4">
+          <p className="lg:text-h2 md:text-h2 text-dark-blue-100 ml-2 mr-2 mt-1 font-bold italic md:ml-2 md:mr-2 md:mt-1 lg:ml-8 lg:mr-8 lg:mt-4">
             {title}
           </p>
-          <p className=" text-dark-blue-100 lg:text-p ml-2 mr-2 mt-1 text-xs md:ml-2 md:mr-2 md:mt-1 md:text-xs lg:mb-8 lg:ml-8 lg:mr-8">
+          <p className=" text-dark-blue-100 lg:text-p ml-2 mr-2 mt-1 md:ml-2 md:mr-2 md:mt-1 lg:mb-8 lg:ml-8 lg:mr-8">
             {children}
           </p>
         </div>
