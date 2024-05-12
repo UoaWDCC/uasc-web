244899efd2ba6546984604d29cc32fe5119fbd04
ZL <ray040406@gmail.com>
2024-05-12 23:24:54 +1200

    prettier

--- client/src/components/generic/AboutSection/AboutSection.tsx
+++ client/src/components/generic/AboutSection/AboutSection.tsx
@@ -14,7 +14,7 @@ const AboutSection = ({ title, children, imageSrc }: Props) => {
     <div className="relative w-full border border-black ">
       <div className="relative left-0 w-3/4 border border-black">
         <img src={imageSrc} />
-        <div className="absolute left-[72%] top-[75%] flex h-[56%] w-[52%] flex-col gap-4 border border-black bg-white overflow-hidden">
+        <div className="absolute left-[72%] top-[75%] flex h-[56%] w-[52%] flex-col gap-4 overflow-hidden border border-black bg-white">
           <h3 className="text-dark-blue-100 ml-8 mr-8 mt-4">{title}</h3>
           <p className="text-dark-blue-100 mb-8 ml-8 mr-8">{children}</p>
         </div>
