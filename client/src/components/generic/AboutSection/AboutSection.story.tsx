4b33086497e5523b059ffe5fff890fc02e0f1c6e
ZL <ray040406@gmail.com>
2024-05-12 23:34:57 +1200

    switching position on stories

--- client/src/components/generic/AboutSection/AboutSection.story.tsx
+++ client/src/components/generic/AboutSection/AboutSection.story.tsx
@@ -10,16 +10,6 @@ const meta: Meta<typeof AboutSection> = {
 export default meta
 type Story = StoryObj<typeof meta>
 
-export const KaynAboutSection: Story = {
-  tags: ["autodocs"],
-  args: {
-    variant: "left",
-    title: "Kayn",
-    children:
-      "A peerless practitioner of lethal shadow magic, Shieda Kayn battles to achieve his true destiny — to one day lead the Order of Shadow into a new era of Ionian supremacy.",
-    imageSrc: RandomImage
-  }
-}
 export const DefaultAboutSection: Story = {
   tags: ["autodocs"],
   args: {
@@ -28,5 +18,16 @@ export const DefaultAboutSection: Story = {
     children:
       "Back in 1966 a few of our founding members built the club lodge on Mt. Ruapehu. The lodge is geared up to sleep 32 and host raging parties all winter, and even with all the student antics we’ve been able to keep the place in good knick.",
     imageSrc: RandomImage2
+  }
+}
+
+export const KaynAboutSection: Story = {
+  tags: ["autodocs"],
+  args: {
+    variant: "left",
+    title: "Kayn",
+    children:
+      "A peerless practitioner of lethal shadow magic, Shieda Kayn battles to achieve his true destiny — to one day lead the Order of Shadow into a new era of Ionian supremacy.",
+    imageSrc: RandomImage
   }
 }
