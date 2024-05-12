2d90991d3d2b5cf9b5f66941cde9cf94fce38a86
ZL <ray040406@gmail.com>
2024-05-12 23:49:39 +1200

    large viewport text to p. Other vps to xs

--- client/src/components/generic/AboutSection/AboutSection.tsx
+++ client/src/components/generic/AboutSection/AboutSection.tsx
@@ -17,7 +17,7 @@ const AboutSection = ({ title, children, imageSrc }: Props) => {
         <p className="lg:text-h2 text-dark-blue-100 font-bold md:ml-2 md:mr-2 md:mt-1 lg:ml-8 lg:mr-8 lg:mt-4">
           {title}
         </p>
-        <p className=" text-dark-blue-100 sm:text-xs md:ml-2 md:mr-2 md:mt-1 md:text-xs lg:mb-8 lg:ml-8 lg:mr-8">
+        <p className=" text-dark-blue-100 lg:text-p text-xs md:ml-2 md:mr-2 md:mt-1 md:text-xs lg:mb-8 lg:ml-8 lg:mr-8">
           {children}
         </p>
       </div>
