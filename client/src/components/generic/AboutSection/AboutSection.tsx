f90ec7597478520d714cdfe3bd3181c931025088
ZL <ray040406@gmail.com>
2024-05-13 13:06:13 +1200

    detail: rounded edge at the bottom for sm vp.

--- client/src/components/generic/AboutSection/AboutSection.tsx
+++ client/src/components/generic/AboutSection/AboutSection.tsx
@@ -14,7 +14,7 @@ const AboutSection = ({ title, children, imageSrc, variant }: Props) => {
     return (
       <div className="relative left-0 w-[71%]">
         <img src={imageSrc} className="rounded-tr-lg"></img>
-        <div className=" absolute top-[100%] flex w-full flex-col overflow-hidden rounded-t-lg border border-black bg-white md:left-[72%] md:top-[70%] md:h-[56%] md:w-[40%] lg:left-[35%] lg:top-[70%] lg:h-[56%] lg:w-[35%] lg:gap-4">
+        <div className=" absolute top-[100%] flex w-full flex-col overflow-hidden rounded-t-lg sm:rounded-b-lg border border-black bg-white md:left-[72%] md:top-[70%] md:h-[56%] md:w-[40%] lg:left-[35%] lg:top-[70%] lg:h-[56%] lg:w-[35%] lg:gap-4">
           <p className="lg:text-h2 text-dark-blue-100 ml-2 mr-2 mt-1 font-bold italic md:ml-2 md:mr-2 md:mt-1 lg:ml-8 lg:mr-8 lg:mt-4">
             {title}
           </p>
@@ -28,7 +28,7 @@ const AboutSection = ({ title, children, imageSrc, variant }: Props) => {
     return (
       <div className="relative left-[20%] w-[71%]">
         <img src={imageSrc} className="rounded-tl-lg" />
-        <div className=" absolute top-[100%] flex w-full flex-col overflow-hidden rounded-t-lg border border-black bg-white md:left-[-25%] md:top-[60%] md:h-[45%] md:w-[74%] lg:left-[-19%] lg:top-[60%] lg:h-[45%] lg:w-[50%] lg:gap-4">
+        <div className=" absolute top-[100%] flex w-full flex-col overflow-hidden rounded-t-lg border sm:rounded-b-lg border-black bg-white md:left-[-25%] md:top-[60%] md:h-[45%] md:w-[74%] lg:left-[-19%] lg:top-[60%] lg:h-[45%] lg:w-[50%] lg:gap-4">
           <p className="lg:text-h2 md:text-p text-dark-blue-100 ml-2 mr-2 mt-1 font-bold italic md:ml-2 md:mr-2 md:mt-1 md:font-bold lg:ml-8 lg:mr-8 lg:mt-4">
             {title}
           </p>
