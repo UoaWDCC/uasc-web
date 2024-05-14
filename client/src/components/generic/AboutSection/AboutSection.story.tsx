7f98df8e53b7651bb231b12dd7ee0b7df7303c14
ZL <ray040406@gmail.com>
2024-05-14 23:39:11 +1200

    resolved BENSON's comment about sharp edges for the textboxes

--- client/src/components/generic/AboutSection/AboutSection.story.tsx
+++ client/src/components/generic/AboutSection/AboutSection.story.tsx
@@ -14,7 +14,7 @@ export const LeftAboutSection: Story = {
   args: {
     variant: "left",
     title: "Founded in 1996",
-    children:
+    text:
       "Back in 1966 a few of our founding members built the club lodge on Mt. Ruapehu. The lodge is geared up to sleep 32 and host raging parties all winter, and even with all the student antics we’ve been able to keep the place in good knick.",
     imageSrc: ImageLeft
   }
@@ -23,7 +23,7 @@ export const RightAboutSection: Story = {
   args: {
     variant: "right",
     title: "Our Purpose: Skiing and Snowboarding",
-    children:
+    text:
       "The UASC lodge is a 32-bed haven for students and alike to gather up on the mountain throughout the winter season. It’s ridiculously close to the main chairlift at the base of Whakapapa, just a few minutes walk and you’re in line to get up the mountain. We hold events with organised transport every few weekends to make sure there’s an opportunity for everyone to get down there at some point.",
     imageSrc: ImageRight
   }
