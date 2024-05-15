67e44e06fffcede2a2d4ebc08feb745226e9856c
ZL <ray040406@gmail.com>
2024-05-15 19:19:11 +1200

    importing JEFFEREY's profile information components

--- client/src/pages/Profile/Profile.tsx
+++ client/src/pages/Profile/Profile.tsx
@@ -1,6 +1,7 @@
 import UserInformationSection from "components/composite/Profile/UserInformation/UserInformationSection"
 import { PageTitle } from "components/generic/PageTitle/PageTitle"
 import { useAppData } from "store/Store"
+import ProfileInformationPanel from "components/generic/ProfileInformationPanel/ProfileInformationPanel"
 
 export default function Profile() {
   const [{ currentUserData }] = useAppData()
