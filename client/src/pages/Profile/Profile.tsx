e5ab0f85de3264adbc1e9f3d44b57b383b367413
ZL <ray040406@gmail.com>
2024-05-15 17:31:41 +1200

    fixing typo on BENSON's work

--- client/src/pages/Profile/Profile.tsx
+++ client/src/pages/Profile/Profile.tsx
@@ -1,6 +1,6 @@
 import UserInformationSection from "components/composite/Profile/UserInformation/UserInformationSection"
 import { PageTitle } from "components/generic/PageTitle/PageTitle"
-import { useAppData } from "store/store"
+import { useAppData } from "store/Store"
 
 export default function Profile() {
   const [{ currentUserData }] = useAppData()
