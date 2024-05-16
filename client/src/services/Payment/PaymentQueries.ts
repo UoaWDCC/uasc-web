a3e212ae2355c3d2c2026afbd2a793452dff2dd8
Benson Cho <100653148+bcho892@users.noreply.github.com>
2024-05-16 13:10:08 +1200

    Update membership types, make membership selectable on frontend (#356)
    * fix payment flow to allow user selection of price
    
    * remove !
    
    * fix errors not displaying on payment
    
    * regenerate
    
    * only rerender on data, loading and error changes

--- client/src/services/Payment/PaymentQueries.ts
+++ client/src/services/Payment/PaymentQueries.ts
@@ -3,12 +3,13 @@ import PaymentService from "./PaymentService"
 import { MembershipTypes } from "models/Payment"
 
 export function useMembershipClientSecretQuery(
-  membershipType: MembershipTypes
+  membershipType?: MembershipTypes
 ) {
   return useQuery({
     queryKey: ["membershipClientSecret", membershipType],
     queryFn: () =>
       PaymentService.getMembershipPaymentClientSecret(membershipType),
-    staleTime: 300000 // 5 mins
+    retry: 2,
+    staleTime: 30000 // 30 Sec
   })
 }
