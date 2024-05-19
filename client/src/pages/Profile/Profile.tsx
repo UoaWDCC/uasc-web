8f44b0046d37e237b3e447cf264ef163ecffc23e
ZL <ray040406@gmail.com>
2024-05-19 18:06:19 +1200

    prettier

--- client/src/pages/Profile/Profile.tsx
+++ client/src/pages/Profile/Profile.tsx
@@ -60,7 +60,6 @@ export default function Profile() {
             <ProfileInformationPanel title="Calender" />
           </div>
         </div>
-        
       </div>
     </FullPageBackgroundImage>
   )
