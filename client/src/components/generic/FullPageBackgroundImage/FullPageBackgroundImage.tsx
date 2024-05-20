e1c808c8a4d0540ad8176277b6987f04e61deff9
ZL <ray040406@gmail.com>
2024-05-21 01:20:22 +1200

    using the image instead of the component as bg image

--- client/src/components/generic/FullPageBackgroundImage/FullPageBackgroundImage.tsx
+++ client/src/components/generic/FullPageBackgroundImage/FullPageBackgroundImage.tsx
@@ -11,7 +11,7 @@ const FullPageBackgroundImage = ({ children }: IFullPageBackgroundImage) => (
          relative z-0 -mt-14 flex h-screen
           min-h-screen flex-col items-center
          justify-center overflow-hidden
-         bg-cover bg-center"
+         bg-cover bg-center " 
   >
     <div className="bg-gray-1 pointer-events-none absolute -z-30 h-screen w-full opacity-70" />
     {children}
