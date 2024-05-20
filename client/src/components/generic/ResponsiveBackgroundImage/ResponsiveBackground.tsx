91c9d70358af3c3f95347f4c08a61897be2fa22b
ZL <ray040406@gmail.com>
2024-05-21 01:55:30 +1200

    fixed the opacity applying problem on all children

--- client/src/components/generic/ResponsiveBackgroundImage/ResponsiveBackground.tsx
+++ client/src/components/generic/ResponsiveBackgroundImage/ResponsiveBackground.tsx
@@ -6,9 +6,9 @@ interface IResponsiveBackgroundImage {
  * `backgroundImage` property in `tailwind.config.ts`
  */
 const ResponsiveBackgroundImage = ({ children }: IResponsiveBackgroundImage) => (
-    <div className="relative bg-mountain-background-image min-h-screen bg-cover overflow-hidden">
+    <div className="relative bg-mountain-background-image min-h-screen bg-cover">
       <div className="absolute inset-0 bg-gray-1 opacity-70 pointer-events-none" />
-      <div className="absolute inset-0 overflow-auto flex items-center justify-center">
+      <div className="relative inset-0 flex items-center justify-center">
         {children}
       </div>
     </div>
