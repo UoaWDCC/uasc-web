6f6898430b1d077b8925c46f9c6044fe0d4fb558
Kartik Malik <kmal768@aucklanduni.ac.nz>
2024-05-21 01:35:34 +1200

    Editted background image

--- client/src/components/generic/FullPageBookLodgeImage/FullPageBookLodgeImage.tsx
+++ client/src/components/generic/FullPageBookLodgeImage/FullPageBookLodgeImage.tsx
@@ -3,8 +3,8 @@ interface IFullPageBookLodgeImage {
 }
 
 const FullPageBookLodgeImage = ({ children }: IFullPageBookLodgeImage) => (
-  <div className="bg-book-lodge-image relative z-0 mt-10 flex h-screen flex-col items-center justify-center overflow-hidden bg-cover bg-center bg-no-repeat">
-    <div className="bg-gray-1  absolute -z-30 h-screen w-full opacity-20" />
+  <div className="bg-book-lodge-image relative z-0 -mt-14 flex h-svh flex-col items-center justify-center overflow-hidden bg-cover bg-center bg-no-repeat">
+    <div className="bg-gray-1  absolute -z-30 h-svh w-full overflow-hidden opacity-20" />
     {children}
   </div>
 )
