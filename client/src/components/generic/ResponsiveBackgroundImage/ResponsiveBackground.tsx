43af2d1906d12ba97088b80c549c69b25ec94a89
ZL <ray040406@gmail.com>
2024-05-21 01:58:44 +1200

    prettier

--- client/src/components/generic/ResponsiveBackgroundImage/ResponsiveBackground.tsx
+++ client/src/components/generic/ResponsiveBackgroundImage/ResponsiveBackground.tsx
@@ -5,13 +5,15 @@ interface IResponsiveBackgroundImage {
  * to find/change the bg you need to check the
  * `backgroundImage` property in `tailwind.config.ts`
  */
-const ResponsiveBackgroundImage = ({ children }: IResponsiveBackgroundImage) => (
-    <div className="relative bg-mountain-background-image min-h-screen bg-cover">
-      <div className="absolute inset-0 bg-gray-1 opacity-70 pointer-events-none" />
-      <div className="relative inset-0 flex items-center justify-center">
-        {children}
-      </div>
+const ResponsiveBackgroundImage = ({
+  children
+}: IResponsiveBackgroundImage) => (
+  <div className="bg-mountain-background-image relative min-h-screen bg-cover">
+    <div className="bg-gray-1 pointer-events-none absolute inset-0 opacity-70" />
+    <div className="relative inset-0 flex items-center justify-center">
+      {children}
     </div>
-  );
+  </div>
+)
 
 export default ResponsiveBackgroundImage
