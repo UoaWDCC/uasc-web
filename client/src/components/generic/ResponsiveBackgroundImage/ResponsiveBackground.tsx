db47da6eec0a89721a570de41693502a8e1a308a
ZL <ray040406@gmail.com>
2024-05-21 01:26:06 +1200

    making responsive background image component to serve for profile page

---
+++ client/src/components/generic/ResponsiveBackgroundImage/ResponsiveBackground.tsx
@@ -0,0 +1,21 @@
+interface IResponsiveBackgroundImage {
+    children: React.ReactNode
+  }
+  /**
+   * to find/change the bg you need to check the
+   * `backgroundImage` property in `tailwind.config.ts`
+   */
+  const ResponsiveBackgroundImage = ({ children }: IResponsiveBackgroundImage) => (
+    <div
+      className="bg-mountain-background-image 
+           relative z-0 -mt-14 flex h-screen
+            min-h-screen flex-col items-center
+           justify-center overflow-hidden
+           bg-cover bg-center " 
+    >
+      <div className="bg-gray-1 pointer-events-none absolute -z-30 h-screen w-full opacity-70" />
+      {children}
+    </div>
+  )
+  
+  export default ResponsiveBackgroundImage
