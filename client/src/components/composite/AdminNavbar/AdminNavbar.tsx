b66e97e1ae3cc3bd01785fd64b3466150cc9305a
ZL <ray040406@gmail.com>
2024-05-21 21:20:22 +1200

    added addresses for the links

--- client/src/components/composite/AdminNavbar/AdminNavbar.tsx
+++ client/src/components/composite/AdminNavbar/AdminNavbar.tsx
@@ -4,15 +4,15 @@ const AdminNavbar = () => {
   return (
     <div className="flex h-[46px] w-full overflow-hidden border border-black">
       <div className="flex items-center justify-items-start gap-6 pl-[10%] uppercase">
-        <Link to="/booking">
+        <Link to="/members">
           <h5>Members</h5>
         </Link>
 
-        <Link to="/">
+        <Link to="/booking">
           <h5>bookings</h5>
         </Link>
 
-        <Link to="/">
+        <Link to="/payments">
           <h5>payments</h5>
         </Link>
       </div>
