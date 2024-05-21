c9c3a4ddfbf5e59f026490f4b3e0e8cbb285fe34
ZL <ray040406@gmail.com>
2024-05-21 20:59:14 +1200

    making the template for spacing starting with the overall navbar

--- client/src/components/composite/AdminNavbar/AdminNavbar.tsx
+++ client/src/components/composite/AdminNavbar/AdminNavbar.tsx
@@ -1,7 +1,15 @@
 // import { Link } from "react-router-dom"
 
 const AdminNavbar = () => {
-  return <div className="h-[46px] w-full border border-black"></div>
+  return (
+    <div className="flex h-[46px] w-full border border-black">
+      <div className="grid grid-cols-8 items-center justify-items-start">
+        <h5>Members</h5>
+        <h5>bookings</h5>
+        <h5>paymentss</h5>
+      </div>
+    </div>
+  )
 }
 
 export default AdminNavbar
