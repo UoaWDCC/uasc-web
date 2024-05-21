76216a0d0dc43cf77a4e4289352be1ca9ea9a22a
ZL <ray040406@gmail.com>
2024-05-21 20:52:39 +1200

    setting up tester border and height

--- client/src/components/composite/AdminNavbar/AdminNavbar.tsx
+++ client/src/components/composite/AdminNavbar/AdminNavbar.tsx
@@ -1,7 +1,7 @@
 // import { Link } from "react-router-dom"
 
 const AdminNavbar = () => {
-  return <div></div>
+  return <div className="h-[46px] w-full border border-black"></div>
 }
 
 export default AdminNavbar
