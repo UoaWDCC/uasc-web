98b9f35fce6f24180049253dba7b80768a48c79a
ZL <ray040406@gmail.com>
2024-05-22 20:18:16 +1200

    turned titles into divs to span out the row in 7

--- client/src/components/generic/ReusableTable/Table.tsx
+++ client/src/components/generic/ReusableTable/Table.tsx
@@ -13,8 +13,11 @@ const Table = <T extends Record<string, any>>({ data }: Props<T>) => {
 
   return (
     <div>
-      <div className="border-dark-blue-100 grid h-[32px] grid-cols-7 border uppercase">
-        name email status membership type date joined
+      <div className="border-dark-blue-100 grid h-[32px] w-full grid-cols-8 border uppercase">
+        <p>name</p>
+        <p>email</p> <p>status</p> <p>membership</p>
+        <p>type</p>
+        <p>date joined</p>
       </div>
       <div className="grid h-[500px] w-full grid-cols-3 border border-black">
         <div className="flex pl-4 pr-4">
