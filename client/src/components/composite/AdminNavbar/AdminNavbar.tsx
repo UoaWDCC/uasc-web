6a6076da379e160e15fa1ff307d30c4ca0a0542a
ZL <ray040406@gmail.com>
2024-05-23 22:20:37 +1200

    adding Clicked function to control the state changes on the nav links.

--- client/src/components/composite/AdminNavbar/AdminNavbar.tsx
+++ client/src/components/composite/AdminNavbar/AdminNavbar.tsx
@@ -1,6 +1,8 @@
 import { Link } from "react-router-dom"
-
+import { useState } from "react"
 const AdminNavbar = () => {
+  const [clicked, setClicked] = useState<boolean>(false)
+
   return (
     <div className="flex h-[46px] w-full overflow-hidden border border-black">
       <div className="flex items-center justify-items-start gap-6 pl-[10%] uppercase">
@@ -15,6 +17,12 @@ const AdminNavbar = () => {
         <Link to="/payments">
           <h5>payments</h5>
         </Link>
+
+        <div
+          className={`ml-auto block h-[20px] w-[24px] cursor-pointer md:hidden
+          ${clicked ? "stroke-light-blue-100" : "stroke-black"} " pt-[5px]`}
+          onClick={() => setClicked(!clicked)}
+        ></div>
       </div>
     </div>
   )
