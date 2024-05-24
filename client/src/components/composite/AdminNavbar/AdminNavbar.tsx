fcb6e1279fe0455098b1d86f528cff30745431d5
ZL <ray040406@gmail.com>
2024-05-24 20:17:34 +1200

    positioning adjustments

--- client/src/components/composite/AdminNavbar/AdminNavbar.tsx
+++ client/src/components/composite/AdminNavbar/AdminNavbar.tsx
@@ -2,10 +2,12 @@ import { WrappedTab } from "../Navbar/utils/WrappedTab"
 
 const AdminNavbar = () => {
   return (
-    <div className="flex h-[46px] w-full overflow-hidden border border-black">
-      <WrappedTab to="/members">members</WrappedTab>
-      <WrappedTab to="/bookings">bookings</WrappedTab>
-      <WrappedTab to="/payments">payments</WrappedTab>
+    <div className="flex h-[46px] w-full items-center overflow-hidden border border-black">
+      <div className="flex pl-4">
+        <WrappedTab to="/members">members</WrappedTab>
+        <WrappedTab to="/bookings">bookings</WrappedTab>
+        <WrappedTab to="/payments">payments</WrappedTab>
+      </div>
     </div>
   )
 }
