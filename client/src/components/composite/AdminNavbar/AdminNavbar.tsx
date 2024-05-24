dcbeaf47857a98d510a6005f2ad6f319536c0b5c
ZL <ray040406@gmail.com>
2024-05-24 20:27:12 +1200

    adjusting spacing so the tab is on spot

--- client/src/components/composite/AdminNavbar/AdminNavbar.tsx
+++ client/src/components/composite/AdminNavbar/AdminNavbar.tsx
@@ -2,8 +2,8 @@ import { WrappedTab } from "../Navbar/utils/WrappedTab"
 
 const AdminNavbar = () => {
   return (
-    <div className="flex h-[46px] w-full items-center overflow-hidden border border-black">
-      <div className="flex pl-4">
+    <div className="flex h-[46px] w-full items-center overflow-hidden border">
+      <div className="flex gap-4 pl-4 pt-[13px] ">
         <WrappedTab to="/members">members</WrappedTab>
         <WrappedTab to="/bookings">bookings</WrappedTab>
         <WrappedTab to="/payments">payments</WrappedTab>
