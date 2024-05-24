9007f7d581316955f635e131dd3f258714033368
ZL <ray040406@gmail.com>
2024-05-24 20:36:42 +1200

    added the exit button and adjusted positioning and styling

--- client/src/components/composite/AdminNavbar/AdminNavbar.tsx
+++ client/src/components/composite/AdminNavbar/AdminNavbar.tsx
@@ -1,12 +1,19 @@
 import { WrappedTab } from "../Navbar/utils/WrappedTab"
 
+const Exit = () => {
+  return <h5 className="text-red uppercase">exit</h5>
+}
+
 const AdminNavbar = () => {
   return (
-    <div className="flex h-[46px] w-full items-center overflow-hidden border">
-      <div className="flex gap-4 pl-4 pt-[13px] ">
+    <div className=" relative flex h-[46px] w-full items-center overflow-hidden border">
+      <div className="flex gap-4 pl-12 pt-[13px] ">
         <WrappedTab to="/members">members</WrappedTab>
         <WrappedTab to="/bookings">bookings</WrappedTab>
         <WrappedTab to="/payments">payments</WrappedTab>
+      </div>
+      <div className="absolute right-0 pr-12">
+        <Exit />
       </div>
     </div>
   )
