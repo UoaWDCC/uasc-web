ba598d5a1cff09539a2c10290f1f59754608586f
ZL <ray040406@gmail.com>
2024-05-24 20:09:56 +1200

    imported BENSON's wrapped tab

--- client/src/components/composite/AdminNavbar/AdminNavbar.tsx
+++ client/src/components/composite/AdminNavbar/AdminNavbar.tsx
@@ -1,34 +1,18 @@
 import { Link } from "react-router-dom"
-import { useState } from "react"
+import { WrappedTab } from "../Navbar/utils/WrappedTab"
+
 const AdminNavbar = () => {
-  const [activeLink, setActiveLink] = useState<string>("")
-
-  const handleClick = (link: string) => {
-    if (activeLink !== link) {
-      setActiveLink(link)
-    }
-  }
-
   return (
     <div className="flex h-[46px] w-full overflow-hidden border border-black">
-      <div className="flex items-center justify-items-start gap-6 pl-[10%] uppercase">
-        <Link to="/members">
-          <h5
-            className={activeLink ? "text-light-blue-100" : "text-black"}
-            onClick={() => handleClick("/members")}
-          >
-            Members
-          </h5>
-        </Link>
+      <WrappedTab to="/members">members</WrappedTab>
 
-        <Link to="/bookings">
-          <h5>bookings</h5>
-        </Link>
+      <Link to="/bookings">
+        <h5>bookings</h5>
+      </Link>
 
-        <Link to="/payments">
-          <h5>payments</h5>
-        </Link>
-      </div>
+      <Link to="/payments">
+        <h5>payments</h5>
+      </Link>
     </div>
   )
 }
