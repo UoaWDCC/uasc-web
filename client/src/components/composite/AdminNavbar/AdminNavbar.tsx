e5e781dbd48b10fb11ec454cd3a5ada21b3b2163
ZL <ray040406@gmail.com>
2024-05-24 20:14:15 +1200

    set up all 3 links as wrapped tabs

--- client/src/components/composite/AdminNavbar/AdminNavbar.tsx
+++ client/src/components/composite/AdminNavbar/AdminNavbar.tsx
@@ -1,18 +1,11 @@
-import { Link } from "react-router-dom"
 import { WrappedTab } from "../Navbar/utils/WrappedTab"
 
 const AdminNavbar = () => {
   return (
     <div className="flex h-[46px] w-full overflow-hidden border border-black">
       <WrappedTab to="/members">members</WrappedTab>
-
-      <Link to="/bookings">
-        <h5>bookings</h5>
-      </Link>
-
-      <Link to="/payments">
-        <h5>payments</h5>
-      </Link>
+      <WrappedTab to="/bookings">bookings</WrappedTab>
+      <WrappedTab to="/payments">payments</WrappedTab>
     </div>
   )
 }
