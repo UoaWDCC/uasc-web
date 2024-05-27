df464ecd13f8cca56449b7541d86e75d2df0310c
ZL <ray040406@gmail.com>
2024-05-27 20:13:58 +1200

    imported needed props for the left about section. Created the left about section for testing purposes

--- client/src/pages/About.tsx
+++ client/src/pages/About.tsx
@@ -1,7 +1,15 @@
+import AboutSection from "components/generic/AboutSection/AboutSection"
+import ImageLeft from "assets/images/hut.png"
 const About = () => {
   return (
     <div>
       <h1> About us page</h1>
+      <AboutSection
+        title="Founded in 1966"
+        variant="left"
+        text="Back in 1966 a few of our founding members built the club lodge on Mt. Ruapehu. The lodge is geared up to sleep 32 and host raging parties all winter, and even with all the student antics we’ve been able to keep the place in good knick."
+        imageSrc={ImageLeft}
+      ></AboutSection>
     </div>
   )
 }
