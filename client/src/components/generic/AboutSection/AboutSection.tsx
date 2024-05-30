8836955859528be15e896d6552850b12ae4d1576
ZL <ray040406@gmail.com>
2024-05-30 22:38:17 +1200

    sm viewport widths changes

--- client/src/components/generic/AboutSection/AboutSection.tsx
+++ client/src/components/generic/AboutSection/AboutSection.tsx
@@ -29,7 +29,7 @@ const AboutSection = ({ title, text, imageSrc, variant }: Props) => {
           src={imageSrc}
           className="w-[45%] rounded-tr-lg md:w-[65%] lg:w-[70%]"
         ></img>
-        <div className=" border-dark-blue-100 absolute top-[100%] flex w-[65%] flex-col overflow-hidden rounded-b-lg border bg-white md:left-[42%] md:top-[70%] md:h-[60%] md:w-[50%]  md:rounded-t-lg lg:left-[45%] lg:top-[70%] lg:h-[56%] lg:w-[35%] lg:gap-4  lg:rounded-t-lg">
+        <div className=" border-dark-blue-100 absolute top-[100%] flex w-[45%] flex-col overflow-hidden rounded-b-lg border bg-white md:left-[42%] md:top-[70%] md:h-[60%] md:w-[50%]  md:rounded-t-lg lg:left-[45%] lg:top-[70%] lg:h-[56%] lg:w-[35%] lg:gap-4  lg:rounded-t-lg">
           <TextStyler
             title={title}
             text={text}
@@ -46,7 +46,7 @@ const AboutSection = ({ title, text, imageSrc, variant }: Props) => {
           src={imageSrc}
           className="w-[35%] rounded-tl-lg md:w-[45%] lg:w-[70%] "
         />
-        <div className=" border-dark-blue-100 absolute top-[100%] flex w-[65%] flex-col overflow-hidden rounded-b-lg border bg-white sm:rounded-b-lg md:left-[-15%] md:top-[60%] md:h-[60%] md:w-[40%] md:rounded-t-lg lg:left-[-10%] lg:top-[60%] lg:h-[55%] lg:w-[40%] lg:gap-4 lg:rounded-t-lg">
+        <div className=" border-dark-blue-100 absolute top-[100%] flex w-[35%] flex-col overflow-hidden rounded-b-lg border bg-white sm:rounded-b-lg md:left-[-15%] md:top-[60%] md:h-[60%] md:w-[40%] md:rounded-t-lg lg:left-[-10%] lg:top-[60%] lg:h-[55%] lg:w-[40%] lg:gap-4 lg:rounded-t-lg">
           <TextStyler
             title={title}
             text={text}
