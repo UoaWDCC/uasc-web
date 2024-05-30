a207b77de69613453798945bb2d421b883fd67f6
ZL <ray040406@gmail.com>
2024-05-30 22:21:00 +1200

    adjusted textbox's positioning for md viewport on the left section

--- client/src/components/generic/AboutSection/AboutSection.tsx
+++ client/src/components/generic/AboutSection/AboutSection.tsx
@@ -11,7 +11,7 @@ type Props = IAboutProps
 const TextStyler = ({ title, text }: Props) => {
   return (
     <>
-      <h4 className="lg:text-h2 md:text-h2 text-dark-blue-100 ml-2 mr-2 mt-1 font-bold italic md:ml-2 md:mr-2 md:mt-1 md:font-bold lg:ml-8 lg:mr-8 lg:mt-4">
+      <h4 className="lg:text-h2 md:text-h4 text-dark-blue-100 ml-2 mr-2 mt-1 font-bold italic md:ml-2 md:mr-2 md:mt-1 md:font-bold lg:ml-8 lg:mr-8 lg:mt-4">
         {title}
       </h4>
       <p className=" text-dark-blue-100 lg:text-p mb-2 ml-2 mr-2 mt-1 md:ml-2 md:mr-2 md:mt-1 lg:mb-8 lg:ml-8 lg:mr-8">
@@ -29,7 +29,7 @@ const AboutSection = ({ title, text, imageSrc, variant }: Props) => {
           src={imageSrc}
           className="w-[65%] rounded-tr-lg lg:h-[500px] lg:w-[900px]"
         ></img>
-        <div className=" border-dark-blue-100 absolute top-[100%] flex w-[65%] flex-col overflow-hidden rounded-b-lg border bg-white md:left-[72%] md:top-[70%] md:h-[60%] md:w-[50%]  md:rounded-t-lg lg:left-[35%] lg:top-[70%] lg:h-[56%] lg:w-[35%] lg:gap-4  lg:rounded-t-lg">
+        <div className=" border-dark-blue-100 absolute top-[100%] flex w-[65%] flex-col overflow-hidden rounded-b-lg border bg-white md:left-[42%] md:top-[70%] md:h-[60%] md:w-[50%]  md:rounded-t-lg lg:left-[35%] lg:top-[70%] lg:h-[56%] lg:w-[35%] lg:gap-4  lg:rounded-t-lg">
           <TextStyler
             title={title}
             text={text}
@@ -46,7 +46,7 @@ const AboutSection = ({ title, text, imageSrc, variant }: Props) => {
           src={imageSrc}
           className="w-[65%] rounded-tl-lg lg:h-[500px] lg:w-[900px]"
         />
-        <div className=" border-dark-blue-100 absolute top-[100%] flex w-[65%] flex-col overflow-hidden rounded-b-lg border bg-white sm:rounded-b-lg md:left-[-15%] md:top-[60%] md:h-[60%] md:w-[45%] md:rounded-t-lg lg:left-[-10%] lg:top-[60%] lg:h-[55%] lg:w-[30%] lg:gap-4 lg:rounded-t-lg">
+        <div className=" border-dark-blue-100 absolute top-[100%] flex w-[65%] flex-col overflow-hidden rounded-b-lg border bg-white sm:rounded-b-lg md:left-[-15%] md:top-[60%] md:h-[60%] md:w-[60%] md:rounded-t-lg lg:left-[-10%] lg:top-[60%] lg:h-[55%] lg:w-[40%] lg:gap-4 lg:rounded-t-lg">
           <TextStyler
             title={title}
             text={text}
