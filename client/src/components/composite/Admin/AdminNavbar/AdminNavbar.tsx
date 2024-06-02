30229ee1290ccb3e91d4948cd25164b5b0a7dab3
bcho892 <AKLbc70@gmail.com>
2024-06-02 17:25:20 +1200

    fix breaking issues on mobile navbar

--- client/src/components/composite/Admin/AdminNavbar/AdminNavbar.tsx
+++ client/src/components/composite/Admin/AdminNavbar/AdminNavbar.tsx
@@ -15,12 +15,18 @@ const Exit = () => {
 const AdminNavbar = () => {
   return (
     <div className="top-13 fixed z-[99] flex h-[46px] w-screen items-center overflow-hidden border bg-white">
-      <div className="flex gap-4 pl-28 pt-[13px] ">
-        <WrappedTab to="/admin/members">members</WrappedTab>
-        <WrappedTab to="/admin/bookings">bookings</WrappedTab>
-        <WrappedTab to="/admin/payments">payments</WrappedTab>
+      <div className="flex pt-[13px] md:gap-4 md:pl-28 ">
+        <WrappedTab mobileCompatiability={false} to="/admin/members">
+          members
+        </WrappedTab>
+        <WrappedTab mobileCompatiability={false} to="/admin/bookings">
+          bookings
+        </WrappedTab>
+        <WrappedTab mobileCompatiability={false} to="/admin/payments">
+          payments
+        </WrappedTab>
       </div>
-      <div className="absolute right-0 pr-28">
+      <div className="right-0 md:absolute md:pr-28">
         <Exit />
       </div>
     </div>
