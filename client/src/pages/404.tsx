365a8082af7d39edcfdabb2201ac1486ee5a5ad1
ZL <ray040406@gmail.com>
2024-06-11 23:46:00 +1200

    added figma button

--- client/src/pages/404.tsx
+++ client/src/pages/404.tsx
@@ -1,5 +1,7 @@
 import React from "react"
 import Wind from "assets/logos/Wind.png"
+import Button from "components/generic/FigmaButtons/FigmaButton"
+
 const InvalidPage = () => {
   return (
     <div className="flex flex-col items-center justify-center gap-4">
@@ -7,6 +9,7 @@ const InvalidPage = () => {
 
       <h2>Error 404</h2>
       <h4>Sorry! Page not found</h4>
+      <Button iconSide="left">Back</Button>
     </div>
   )
 }
