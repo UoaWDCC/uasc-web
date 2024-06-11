f919886ec3853d459c74c240459ba480c56b5aa3
ZL <ray040406@gmail.com>
2024-06-11 23:41:10 +1200

    added text contents from figma

--- client/src/pages/404.tsx
+++ client/src/pages/404.tsx
@@ -2,8 +2,11 @@ import React from "react"
 import Wind from "assets/logos/Wind.png"
 const InvalidPage = () => {
   return (
-    <div>
-      <Wind />
+    <div className="flex flex-col items-center justify-center gap-4">
+      <img src={Wind}></img>
+
+      <h2>Error 404</h2>
+      <h4>Sorry! Page not found</h4>
     </div>
   )
 }
