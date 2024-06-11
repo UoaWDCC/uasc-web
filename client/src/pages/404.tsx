9db9c5b61d725600fb89826d89695b89c13ea524
ZL <ray040406@gmail.com>
2024-06-12 00:01:20 +1200

    handling going back 1 page function

--- client/src/pages/404.tsx
+++ client/src/pages/404.tsx
@@ -2,6 +2,10 @@ import React from "react"
 import Wind from "assets/logos/Wind.png"
 import Button from "components/generic/FigmaButtons/FigmaButton"
 
+function handleBack() {
+  // window.history.go(-1)
+}
+
 const InvalidPage = () => {
   return (
     <div className="flex flex-col items-center justify-center gap-4">
@@ -9,7 +13,11 @@ const InvalidPage = () => {
 
       <h2>Error 404</h2>
       <h4>Sorry! Page not found</h4>
-      <Button iconSide="left">Back</Button>
+      <div>
+        <Button iconSide="left" variant="progress-default" onClick={handleBack}>
+          Back
+        </Button>
+      </div>
     </div>
   )
 }
