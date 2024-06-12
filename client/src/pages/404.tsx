7c024872749f7a846826db1c8a10acca61dc417b
ZL <ray040406@gmail.com>
2024-06-12 16:51:16 +1200

    downloaded new high res icon

--- client/src/pages/404.tsx
+++ client/src/pages/404.tsx
@@ -1,5 +1,5 @@
 import { useNavigate } from "react-router-dom"
-import Wind from "assets/icons/Wind.svg"
+import Wind from "assets/icons/Wind.svg?react"
 import Button from "components/generic/FigmaButtons/FigmaButton"
 
 const NoMatch = () => {
