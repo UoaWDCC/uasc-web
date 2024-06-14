95b6e28e7e167a0079ed47cbd7102e757afdc000
ZL <ray040406@gmail.com>
2024-06-15 01:06:49 +1200

    x

--- client/src/components/composite/Profile/EditPopUp/profileEdit.tsx
+++ client/src/components/composite/Profile/EditPopUp/profileEdit.tsx
@@ -1,10 +1,33 @@
 import { useAppData } from "store/Store"
+
+const Field = ({
+  subtitle,
+  description
+}: {
+  subtitle: string
+  description: string
+}) => {
+  return (
+    <>
+      <div>
+        <p className="">{subtitle}</p>
+
+        <p>{description}</p>
+      </div>
+    </>
+  )
+}
+
 const profileEdit = () => {
   const [{ currentUserData }] = useAppData()
-  const [{ currentUser }] = useAppData()
   return (
     <div className="flex w-full items-center justify-center">
-      <div className="w-[45%] border border-black"></div>
+      <div className="w-[45%] border border-black">
+        <Field
+          subtitle="Name"
+          description={`${currentUserData?.first_name}`}
+        ></Field>
+      </div>
     </div>
   )
 }
