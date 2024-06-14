d8f3adde3c97b8b158a20975dfb7496eda533631
bcho892 <AKLbc70@gmail.com>
2024-06-15 10:19:25 +1200

    send email on sign up

--- client/src/services/User/UserMutations.ts
+++ client/src/services/User/UserMutations.ts
@@ -1,11 +1,23 @@
 import { useMutation } from "@tanstack/react-query"
 import UserService, { SignUpUserBody } from "./UserService"
+import { sendPasswordResetEmail, signInWithCustomToken } from "firebase/auth"
+import { auth } from "firebase"
 
 const SIGN_UP_USER_MUTATION_KEY = "signUpUser" as const
 
 export function useSignUpUserMutation(userData: SignUpUserBody) {
   return useMutation({
     mutationKey: [SIGN_UP_USER_MUTATION_KEY],
-    mutationFn: () => UserService.signUpUser(userData)
+    mutationFn: () => UserService.signUpUser(userData),
+    onSuccess: async (data) => {
+      if (data?.jwtToken) {
+        try {
+          await sendPasswordResetEmail(auth, userData.email)
+        } finally {
+          await signInWithCustomToken(auth, data.jwtToken)
+        }
+      }
+    },
+    retry: 0
   })
 }
