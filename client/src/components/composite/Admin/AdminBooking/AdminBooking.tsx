343a7af2013c5207458be4790bf8051a9d3a5988
ZL <ray040406@gmail.com>
2024-06-15 23:06:11 +1200

    imports

--- client/src/components/composite/Admin/AdminBooking/AdminBooking.tsx
+++ client/src/components/composite/Admin/AdminBooking/AdminBooking.tsx
@@ -1,3 +1,7 @@
+import Table from "components/generic/ReusableTable/Table"
+import Button from "components/generic/FigmaButtons/FigmaButton"
+
+
 const AdminBooking = () => {
   return <div></div>
 }
