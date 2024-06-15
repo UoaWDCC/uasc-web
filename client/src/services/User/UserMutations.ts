228d7b3ccff2ab665e8cf8a78f7a3ba920c072e0
Benson Cho <100653148+bcho892@users.noreply.github.com>
2024-06-15 17:47:36 +1200

    Send additional password reset email on sign up (#441)
    * send email on sign up
    
    * add user prompts for verfication

--- client/src/services/User/UserMutations.ts
+++ client/src/services/User/UserMutations.ts
@@ -1,5 +1,7 @@
 import { useMutation } from "@tanstack/react-query"
 import UserService, { SignUpUserBody } from "./UserService"
+import { sendPasswordResetEmail, signInWithCustomToken } from "firebase/auth"
+import { auth } from "firebase"
 import { ReducedUserAdditionalInfo } from "models/User"
 
 const SIGN_UP_USER_MUTATION_KEY = "signUpUser" as const
@@ -7,7 +9,17 @@ const SIGN_UP_USER_MUTATION_KEY = "signUpUser" as const
 export function useSignUpUserMutation(userData: SignUpUserBody) {
   return useMutation({
     mutationKey: [SIGN_UP_USER_MUTATION_KEY],
-    mutationFn: () => UserService.signUpUser(userData)
+    mutationFn: () => UserService.signUpUser(userData),
+    onSuccess: async (data) => {
+      if (data?.jwtToken) {
+        try {
+          await sendPasswordResetEmail(auth, userData.email)
+        } finally {
+          await signInWithCustomToken(auth, data.jwtToken)
+        }
+      }
+    },
+    retry: 0
   })
 }
 
