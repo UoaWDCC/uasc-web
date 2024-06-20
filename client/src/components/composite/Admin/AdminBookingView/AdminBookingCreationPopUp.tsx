a1173d1292ef0da300bf1511a34ec03e263a56e2
ZL <ray040406@gmail.com>
2024-06-20 22:11:43 +1200

    h adjust

--- client/src/components/composite/Admin/AdminBookingView/AdminBookingCreationPopUp.tsx
+++ client/src/components/composite/Admin/AdminBookingView/AdminBookingCreationPopUp.tsx
@@ -21,24 +21,23 @@ enum FlowStages {
 
 const Divider = () => {
   return (
-    <div className="bg-gray-3 ml-auto hidden h-screen w-[1px] md:block"></div>
+    <div className="bg-gray-3 ml-auto hidden h-screen w-[1px] sm:block"></div>
   )
 }
 
 const AdminBookingCreationPopUp = ({
   handleClose
 }: IAdminBookingCreationPopUp) => {
-  swi
   return (
-    <div className="flex max-w-[800px] flex-col gap-7 md:flex-row">
-      <div className="flex w-full flex-col md:basis-1/2">
+    <div className="flex w-full max-w-[900px] flex-col gap-7 sm:flex-row">
+      <div className="flex w-full flex-col sm:basis-1/2">
         <p className="opacity-20">Select user</p>
         <AdminSearchBar />
 
         <p className="mt-8">Creating booking for:</p>
       </div>
       <Divider />
-      <div className="flex md:basis-1/2">
+      <div className="flex sm:basis-1/2">
         <div className="w-full max-w-[380px]">
           <Calendar />
           <DateRangePicker />
