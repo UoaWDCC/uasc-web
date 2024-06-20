55370c13dc20436e7989493c2801c08de39f76e8
ZL <ray040406@gmail.com>
2024-06-20 22:16:11 +1200

    mock data

--- client/src/components/composite/Admin/AdminBookingView/AdminBookingCreationPopUp.story.tsx
+++ client/src/components/composite/Admin/AdminBookingView/AdminBookingCreationPopUp.story.tsx
@@ -1,5 +1,8 @@
 import type { Meta, StoryObj } from "@storybook/react"
 import AdminBookingCreationPopUp from "./AdminBookingCreationPopUp"
+import { CombinedUserData } from "models/User"
+import { Timestamp } from "firebase/firestore"
+
 const meta: Meta<typeof AdminBookingCreationPopUp> = {
   component: AdminBookingCreationPopUp
 }
@@ -7,6 +10,60 @@ const meta: Meta<typeof AdminBookingCreationPopUp> = {
 export default meta
 type Story = StoryObj<typeof AdminBookingCreationPopUp>
 
+const mockData: CombinedUserData[] = [
+  {
+    email: "zlzlzl@gmail.com",
+    first_name: "ZL",
+    membership: "member",
+    phone_number: 11,
+    dietary_requirements: "x",
+    date_of_birth: Timestamp.fromDate(new Date("2004-04-06")),
+    last_name: "ZHAO",
+    uid: "11kaede"
+  },
+  {
+    email: "aasds@gmail.com",
+    first_name: "sdjsds",
+    last_name: "addsds",
+    uid: "asdas",
+    membership: "guest",
+    phone_number: 111,
+    dietary_requirements: "nutz",
+    date_of_birth: Timestamp.fromDate(new Date("2001-01-01"))
+  },
+
+  {
+    email: "aasds@gmail.com",
+    first_name: "sdjsds",
+    last_name: "addsds",
+    uid: "asdas",
+    membership: "guest",
+    phone_number: 111,
+    dietary_requirements: "nutz",
+    date_of_birth: Timestamp.fromDate(new Date("2001-01-01"))
+  },
+  {
+    email: "aasds@gmail.com",
+    first_name: "sdjsds",
+    last_name: "addsds",
+    uid: "asdas",
+    membership: "guest",
+    phone_number: 111,
+    dietary_requirements: "nutz",
+    date_of_birth: Timestamp.fromDate(new Date("2001-01-01"))
+  },
+  {
+    email: "aasds@gmail.com",
+    first_name: "sdjsds",
+    last_name: "addsds",
+    uid: "asdas",
+    membership: "guest",
+    phone_number: 111,
+    dietary_requirements: "nutz",
+    date_of_birth: Timestamp.fromDate(new Date("2001-01-01"))
+  }
+]
+
 export const AdminBookingCreationPopUp1: Story = {
-  args: {}
+  args: { users: mockData }
 }
