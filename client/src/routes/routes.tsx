8adb2fa70ff6835adaf4dceb282e18c3c25719a9
bcho892 <AKLbc70@gmail.com>
2024-06-24 00:09:51 +1200

    revert lazy changes

--- client/src/routes/routes.tsx
+++ client/src/routes/routes.tsx
@@ -1,17 +1,15 @@
 import About from "pages/About"
+import Admin from "pages/Admin/Admin"
+import Booking from "pages/Booking"
 import Contact from "pages/Contact"
 import Events from "pages/Events"
 import Login from "pages/Login/Login"
+import Profile from "pages/Profile/Profile"
 import Register from "pages/Register/Register"
 import { Route, Routes } from "react-router-dom"
 import NoMatch from "pages/404"
 import { BookingContextProvider } from "components/composite/Booking/BookingContext"
 import WrappedHomeComponent from "pages/Home/sections/utils/WrappedHomeComponent"
-import { Suspense, lazy } from "react"
-
-const AsyncAdmin = lazy(() => import("pages/Admin/Admin"))
-const AsyncProfile = lazy(() => import("pages/Profile/Profile"))
-const AsyncBookings = lazy(() => import("pages/Booking"))
 
 export const AllRoutes = () => (
   <Routes>
@@ -25,29 +23,13 @@ export const AllRoutes = () => (
       <Route
         path="bookings/*"
         element={
-          <Suspense>
-            <BookingContextProvider>
-              <AsyncBookings />
-            </BookingContextProvider>
-          </Suspense>
+          <BookingContextProvider>
+            <Booking />
+          </BookingContextProvider>
         }
       />
-      <Route
-        path="profile"
-        element={
-          <Suspense>
-            <AsyncProfile />
-          </Suspense>
-        }
-      />
-      <Route
-        path="admin/*"
-        element={
-          <Suspense>
-            <AsyncAdmin />
-          </Suspense>
-        }
-      />
+      <Route path="profile" element={<Profile />} />
+      <Route path="admin/*" element={<Admin />} />
       <Route path="*" element={<NoMatch />} />
     </Route>
   </Routes>
