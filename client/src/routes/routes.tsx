e22fb636f80a621f18f8e1710b45a31d95603ade
bcho892 <AKLbc70@gmail.com>
2024-06-24 11:13:44 +1200

    async load all

--- client/src/routes/routes.tsx
+++ client/src/routes/routes.tsx
@@ -1,35 +1,63 @@
 import About from "pages/About"
-import Admin from "pages/Admin/Admin"
-import Booking from "pages/Booking"
-import Contact from "pages/Contact"
-import Events from "pages/Events"
-import Login from "pages/Login/Login"
-import Profile from "pages/Profile/Profile"
-import Register from "pages/Register/Register"
 import { Route, Routes } from "react-router-dom"
 import NoMatch from "pages/404"
 import { BookingContextProvider } from "components/composite/Booking/BookingContext"
 import WrappedHomeComponent from "pages/Home/sections/utils/WrappedHomeComponent"
+import { Suspense, lazy } from "react"
+
+const AsyncBooking = lazy(() => import("pages/Booking"))
+const AsyncAdmin = lazy(() => import("pages/Admin/Admin"))
+const AsyncRegister = lazy(() => import("pages/Register/Register"))
+const AsyncLogin = lazy(() => import("pages/Login/Login"))
+const AsyncProfile = lazy(() => import("pages/Profile/Profile"))
 
 export const AllRoutes = () => (
   <Routes>
     <Route path="/">
       <Route index element={<WrappedHomeComponent />} />
       <Route path="about" element={<About />} />
-      <Route path="events" element={<Events />} />
-      <Route path="contact" element={<Contact />} />
-      <Route path="register/*" element={<Register />} />
-      <Route path="login/*" element={<Login />} />
+      <Route
+        path="register/*"
+        element={
+          <Suspense>
+            <AsyncRegister />
+          </Suspense>
+        }
+      />
+      <Route
+        path="login/*"
+        element={
+          <Suspense>
+            <AsyncLogin />
+          </Suspense>
+        }
+      />
       <Route
         path="bookings/*"
         element={
           <BookingContextProvider>
-            <Booking />
+            <Suspense>
+              <AsyncBooking />
+            </Suspense>
           </BookingContextProvider>
         }
       />
-      <Route path="profile" element={<Profile />} />
-      <Route path="admin/*" element={<Admin />} />
+      <Route
+        path="profile"
+        element={
+          <Suspense>
+            <AsyncProfile />
+          </Suspense>
+        }
+      />
+      <Route
+        path="admin/*"
+        element={
+          <Suspense>
+            <AsyncAdmin />
+          </Suspense>
+        }
+      />
       <Route path="*" element={<NoMatch />} />
     </Route>
   </Routes>
