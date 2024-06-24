e4f20e13cf0f76809ce870ae871668b3f3c7f672
ZL <ray040406@gmail.com>
2024-06-24 20:31:17 +1200

    set up icon and size

--- client/src/components/generic/SuspenseComponent/Loader.tsx
+++ client/src/components/generic/SuspenseComponent/Loader.tsx
@@ -1,6 +1,9 @@
+import Gear from "assets/icons/gear-spinner.svg?react"
+
 const Loader = () => {
   return (
     <div className="flex h-screen w-full items-center justify-center">
+      <Gear className="w-[50px]" />
       <h2 className="text-dark-blue-100 font-bold">Loading...</h2>
     </div>
   )
