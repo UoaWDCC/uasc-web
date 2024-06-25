54fad27dea9785db652f3ce85b90bce63241a3f1
ZL <ray040406@gmail.com>
2024-06-25 22:53:40 +1200

    centered button

--- client/src/components/composite/Profile/ProfileEdit/ProfileEdit.tsx
+++ client/src/components/composite/Profile/ProfileEdit/ProfileEdit.tsx
@@ -60,8 +60,8 @@ const ProfileEdit = <T extends Partial<ReducedUserAdditionalInfo>>({
   const [closed, setClosed] = useState(false)
   if (!closed) {
     return (
-      <div className="flex w-[480px] flex-col items-center justify-center ">
-        <div className="border-gray-3 mt-4 flex w-full  flex-col gap-4 rounded-md border p-4">
+      <div className="border-gray-3 flex w-[480px] flex-col items-center justify-center rounded-md border">
+        <div className=" mt-4 flex w-full flex-col gap-4  p-4">
           <div className="flex w-full">
             <h3 className="text-dark-blue-100">{title}</h3>{" "}
             <CloseButton
@@ -80,10 +80,9 @@ const ProfileEdit = <T extends Partial<ReducedUserAdditionalInfo>>({
               />
             )
           })}
-
-          <div className="w-[200px]">
-            <Button onClick={() => {}}>Update details</Button>
-          </div>
+        </div>
+        <div className="mt-4 w-[200px]">
+          <Button onClick={() => {}}>Update details</Button>
         </div>
       </div>
     )
