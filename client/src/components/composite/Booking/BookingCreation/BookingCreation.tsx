362c542a49ffe1d3d37a390d769e931c271037a8
ZL <ray040406@gmail.com>
2024-06-26 22:49:32 +1200

    changed to placeholder

--- client/src/components/composite/Booking/BookingCreation/BookingCreation.tsx
+++ client/src/components/composite/Booking/BookingCreation/BookingCreation.tsx
@@ -265,7 +265,7 @@ export const CreateBookingSection = ({
           <TextInput
             onChange={(e) => handleAllergyChange?.(e.target.value)}
             label="Please describe your dietary requirements"
-            description="Enter dietary requirements here"
+            placeholder="Enter dietary requirements here"
           />
 
           {hasExistingSession ? (
