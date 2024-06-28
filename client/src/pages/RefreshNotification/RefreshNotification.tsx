2a174a5d59af44475bac535d2ecc46a3d09e4dae
Benson Cho <100653148+bcho892@users.noreply.github.com>
2024-06-29 00:56:28 +1200

    add error boundary for components (#545)
    * add error handling
    
    * change message
    
    * update docs

---
+++ client/src/pages/RefreshNotification/RefreshNotification.tsx
@@ -0,0 +1,16 @@
+/**
+ * Used for if there is a lazy load error due to a chunk not being found
+ *
+ * Also catches any unhandled errors
+ */
+const RefreshNotification = () => {
+  return (
+    <div className="flex h-screen w-full items-center justify-center">
+      <h2 className="text-dark-blue-100">
+        Something went wrong. Please refresh the page
+      </h2>
+    </div>
+  )
+}
+
+export default RefreshNotification
