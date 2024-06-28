32a3b5e5fd3261fbbe4e6d9eae9412172e600f53
bcho892 <AKLbc70@gmail.com>
2024-06-29 11:14:23 +1200

    add password reset script

---
+++ server/tooling/reset-all-passwords.ts
@@ -0,0 +1,30 @@
+import admin from "firebase-admin"
+import dotenv from "dotenv"
+import { initializeApp } from "firebase/app"
+import { getAuth, sendPasswordResetEmail } from "firebase/auth"
+dotenv.config()
+
+admin.initializeApp({
+  credential: admin.credential.cert(
+    JSON.parse(process.env.GOOGLE_SERVICE_ACCOUNT_JSON)
+  )
+})
+
+const app = initializeApp({
+  apiKey: process.env.API_KEY,
+  projectId: "uasc-ceebc"
+})
+const auth = getAuth(app)
+
+const resetAllPasswords = async () => {
+  const firebaseUsers = await admin.auth().listUsers()
+  console.log("found user count", firebaseUsers.users.length)
+  for (const user of firebaseUsers.users) {
+    console.log(`Sending password reset for ${user.email}`)
+    await sendPasswordResetEmail(auth, user.email)
+  }
+}
+
+resetAllPasswords()
+
+console.log("exited script")
