37b8d2815c441259c28a56501d6ffc676d5a5e20
bcho892 <AKLbc70@gmail.com>
2024-06-28 22:33:49 +1200

    change message

--- client/src/pages/RefreshNotification/RefreshNotification.tsx
+++ client/src/pages/RefreshNotification/RefreshNotification.tsx
@@ -5,7 +5,7 @@ const RefreshNotification = () => {
   return (
     <div className="flex h-screen w-full items-center justify-center">
       <h2 className="text-dark-blue-100">
-        A new version of the site is ready. Please refresh the page
+        Something went wrong. Please refresh the page
       </h2>
     </div>
   )
