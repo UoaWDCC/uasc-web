b2e18a56777288849b140f6c6ae4eaa33b5f2794
Albert Sun <asun555@aucklanduni.ac.nz>
2024-06-29 21:27:18 +1200

    482 frontend user signup confirm details (#557)
    * Set up confirm details file
    
    * Added review details container and section titles
    
    * Added new page
    
    * Update to display confirm details in sign up
    
    * Added details on form
    
    * Finished main section of confirm details form
    
    * Added page title text
    
    * Fixed up some css
    
    * Updated all CSS to tailwindcss
    
    * Fixed logic error

---
+++ client/src/components/generic/ConfirmDetailsForm/ConfirmDetailsForm.story.tsx
@@ -0,0 +1,72 @@
+import type { Meta, StoryObj } from "@storybook/react"
+
+import ConfirmDetailsForm from "./ConfirmDetailsForm"
+
+const meta: Meta<typeof ConfirmDetailsForm> = {
+  component: ConfirmDetailsForm
+}
+
+export default meta
+
+type Story = StoryObj<typeof ConfirmDetailsForm>
+
+export const Default: Story = {
+  args: {
+    children: (
+      <>
+        <p>Form Container</p>
+        <p>Form Container</p>
+        <p>Form Container</p>
+        <p>Form Container</p>
+        <p>Form Container</p>
+        <p>Form Container</p>
+        <p>Form Container</p>
+        <p>Form Container</p>
+        <p>Form Container</p>
+        <p>Form Container</p>
+        <p>Form Container</p>
+        <p>Form Container</p>
+        <p>Form Container</p>
+        <p>Form Container</p>
+        <p>Form Container</p>
+        <p>Form Container</p>
+        <p>Form Container</p>
+        <p>Form Container</p>
+        <p>Form Container</p>
+        <p>Form Container</p>
+        <p>Form Container</p>
+        <p>Form Container</p>
+        <p>Form Container</p>
+        <p>Form Container</p>
+        <p>Form Container</p>
+        <p>Form Container</p>
+        <p>Form Container</p>
+        <p>Form Container</p>
+        <p>Form Container</p>
+        <p>Form Container</p>
+        <p>Form Container</p>
+        <p>Form Container</p>
+        <p>Form Container</p>
+        <p>Form Container</p>
+        <p>Form Container</p>
+        <p>Form Container</p>
+        <p>Form Container</p>
+        <p>Form Container</p>
+        <p>Form Container</p>
+        <p>Form Container</p>
+        <p>Form Container</p>
+        <p>Form Container</p>
+        <p>Form Container</p>
+        <p>Form Container</p>
+        <p>Form Container</p>
+        <p>Form Container</p>
+        <p>Form Container</p>
+        <p>Form Container</p>
+        <p>Form Container</p>
+        <p>Form Container</p>
+        <p>Form Container</p>
+        <p>Form Container</p>
+      </>
+    )
+  }
+}
