2f905123729a548812f9507ea60924bf724f518b
bcho892 <AKLbc70@gmail.com>
2024-07-03 20:12:55 +1200

    don't pass note instead

--- client/src/pages/Home/sections/utils/Pricing.ts
+++ client/src/pages/Home/sections/utils/Pricing.ts
@@ -1,6 +1,7 @@
 import { Pricing, PricingBannerContent } from "components/utils/types"
 
-export const pricingNote = ""
+export const pricingNote =
+  "*We have a discounted membership price on offer until Sunday 17th March so lock in now for a year of awesome memories!"
 
 export const pricingBannerContent: PricingBannerContent = {
   headline: "Great nightly rates",
