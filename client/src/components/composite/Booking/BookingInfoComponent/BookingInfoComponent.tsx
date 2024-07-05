9cfce7a9574a84cff832195631c81dab7be64b21
bcho892 <AKLbc70@gmail.com>
2024-07-05 13:23:23 +1200

    add confirmation

--- client/src/components/composite/Booking/BookingInfoComponent/BookingInfoComponent.tsx
+++ client/src/components/composite/Booking/BookingInfoComponent/BookingInfoComponent.tsx
@@ -1,4 +1,5 @@
 import { Link } from "react-router-dom"
+import { CHECK_IN_TIME, CHECK_OUT_TIME } from "utils/Constants"
 
 interface IBookingInfoProps {
   /**
@@ -56,10 +57,10 @@ const BookingInfoComponent = ({
         </p>
         <p>Please read UASC policy below before booking.</p>
         <h4>
-          Check in time: <strong>3:00pm</strong>
+          Check in time: <strong>{CHECK_IN_TIME}</strong>
         </h4>
         <h4>
-          Check out time: <strong>10:00am</strong>
+          Check out time: <strong>{CHECK_OUT_TIME}</strong>
         </h4>
         <h4 className="text-red font-bold italic">
           You must have a booking to stay at the lodge!
