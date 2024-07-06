f95a1cadd9a1acb11a2bf9e455499b55b201d0db
bcho892 <AKLbc70@gmail.com>
2024-07-06 19:58:55 +1200

    add test setup

--- uasc-next/jest.config.ts
+++ uasc-next/jest.config.ts
@@ -10,6 +10,7 @@ const createJestConfig = nextJest({
 const config: Config = {
   coverageProvider: "v8",
   testEnvironment: "jsdom",
+  setupFilesAfterEnv: ["<rootDir>/test-setup.ts"],
   moduleNameMapper: {
     "^.+\\.(svg)$": "<rootDir>/__mocks__/svg.ts",
     "\\.(css|less)$": "<rootDir>/__mocks__/css.ts"
