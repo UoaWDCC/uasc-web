6ac9414628654291ca80af8ff71e20bff53eed13
bcho892 <AKLbc70@gmail.com>
2024-07-06 19:41:56 +1200

    remove test setup

--- uasc-next/jest.config.ts
+++ uasc-next/jest.config.ts
@@ -10,7 +10,6 @@ const createJestConfig = nextJest({
 const config: Config = {
   coverageProvider: "v8",
   testEnvironment: "jsdom",
-  setupFilesAfterEnv: ["<rootDir>/test-setup.ts"],
   moduleNameMapper: {
     "^.+\\.(svg)$": "<rootDir>/__mocks__/svg.ts",
     "\\.(css|less)$": "<rootDir>/__mocks__/css.ts"
--- uasc-next/test-setup.ts
+++
@@ -1,7 +0,0 @@
-import "@testing-library/jest-dom"
-
-global.console = {
-  ...console,
-  // mute console.errors
-  error: jest.fn()
-}
