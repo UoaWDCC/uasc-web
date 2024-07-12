3ba3b3ed6c135e494feeb734f57833bf932d55c9
bcho892 <AKLbc70@gmail.com>
2024-07-12 15:26:24 +1200

    fix workflow and test

--- client/src/components/composite/SignUpForm/utils/utils.test.tsx
+++ client/src/components/composite/SignUpForm/utils/utils.test.tsx
@@ -1,6 +1,6 @@
 import { oneLevelUp, useCurrentStep } from "./Utils"
 import { renderHook } from "@testing-library/react"
-import { PAGES, PERSONAL_ROUTE_1 } from "./RouteNames"
+import { PAGES } from "./RouteNames"
 
 describe("level up function", () => {
   it("should return the correct path", () => {
@@ -10,24 +10,12 @@ describe("level up function", () => {
 
 describe("route validator", () => {
   it("should return unknown value for bad routes", () => {
-    jest.mock("react-router-dom", () => ({
-      ...jest.requireActual("react-router-dom"), // use actual for all non-hook parts
-      useParams: () => ({
-        step: "garbage"
-      })
-    }))
     const { result } = renderHook(useCurrentStep)
 
     expect(result.current).toEqual(PAGES.Unknown)
   })
 
   it("should return a known value for a good route", () => {
-    jest.mock("react-router-dom", () => ({
-      ...jest.requireActual("react-router-dom"), // use actual for all non-hook parts
-      useParams: () => ({
-        step: PERSONAL_ROUTE_1
-      })
-    }))
     const { result } = renderHook(() => useCurrentStep)
     expect(result.current).not.toEqual(PAGES.Unknown)
   })
