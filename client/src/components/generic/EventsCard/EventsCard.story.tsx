68218606ffe09da5001b09cd7892b67624c272d7
ZL <ray040406@gmail.com>
2024-07-13 18:13:57 +1200

    title

--- client/src/components/generic/EventsCard/EventsCard.story.tsx
+++ client/src/components/generic/EventsCard/EventsCard.story.tsx
@@ -11,7 +11,7 @@ type Story = StoryObj<typeof meta>
 export const DefaultEventsCard: Story = {
   tags: ["autodocs"],
   args: {
-    title: "Events Card Story",
+    title: "Outdoor Clubs Welcome Party",
     date: "THU 18/7 • 6pm",
     location: "Shadows Bar - 8 Alfred St, CBD, Auckland",
     content: `Get ready to kick off Re-O-Week with a bang!  Join us at Shadows Bar for the ultimate Outdoor Clubs Welcome Party, hosted by UoA Snowsports Club! Whether you're into hiking, biking, skiing, or just having a good time, this is the place to be!`
