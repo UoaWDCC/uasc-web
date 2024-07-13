24d64baa537678ccbe42423be4b7b17d5e9b14ab
zlrkw11 <64724157+zlrkw11@users.noreply.github.com>
2024-07-13 19:17:31 +1200

    618 events card (#627)
    * set up files
    
    * set up story
    
    * ran yarn
    
    * interface 4 props
    
    * structure of display
    
    * destructure
    
    * prettier
    
    * test
    
    * title style
    
    * margin for all, date
    
    * location
    
    * divider made
    
    * styled
    
    * spacing
    
    * top margin for location
    
    * removed divs
    
    * divider style
    
    * example text
    
    * title
    
    * responsive width
    
    * text centered for phones
    
    * button added
    
    * button + onClick handler
    
    * fixed according to BENSON's comment
    
    * optional prop set
    
    * styling padding

---
+++ client/src/components/generic/EventsCard/EventsCard.tsx
@@ -0,0 +1,35 @@
+import Button from "../FigmaButtons/FigmaButton"
+
+interface EventsCardProps {
+  date?: string
+  title: string
+  location?: string
+  content: React.ReactNode
+  onClick: () => void
+}
+
+type props = EventsCardProps
+
+const EventsCard = ({ date, title, location, content, onClick }: props) => {
+  const Divider = () => {
+    return <div className="bg-gray-3 mb-4 mt-4 h-[1px] w-full"></div>
+  }
+  return (
+    <div className="h-full w-full border p-8 text-center md:text-left">
+      <h5 className="font-bold">{date}</h5>
+
+      <h3 className="text-dark-blue-100 mt-1 font-bold">{title}</h3>
+
+      <div className="text-gray-4 mt-2">{location}</div>
+      <Divider />
+
+      <div className="text-left">{content}</div>
+
+      <div className="mt-4">
+        <Button onClick={onClick}>Sign Up!</Button>
+      </div>
+    </div>
+  )
+}
+
+export default EventsCard
