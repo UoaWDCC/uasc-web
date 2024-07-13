eb77a27514d23fcecf3981fb5533867ab523589e
ZL <ray040406@gmail.com>
2024-07-13 17:41:12 +1200

    structure of display

--- client/src/components/generic/EventsCard/EventsCard.tsx
+++ client/src/components/generic/EventsCard/EventsCard.tsx
@@ -5,8 +5,19 @@ interface EventsCardProps {
     content: React.ReactNode
 }
 
-const EventsCard = () => {
-  return <div className="border-1-black h-full w-[760px] border">card</div>
+type props = EventsCardProps
+
+const EventsCard = ({...props}: props) => {
+  return <div className="border-1-black h-full w-[760px] border">
+    <div className="">
+        {props.date}
+    </div>
+    <div>
+        <h2>{props.title}</h2>
+    </div>
+    <div>{props.location}</div>
+    <div>{props.content}</div>
+  </div>
 }
 
 export default EventsCard
