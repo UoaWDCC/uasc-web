cb5f4c37e3c72dab0d56c59ce40f7b504d0c42eb
ZL <ray040406@gmail.com>
2024-07-13 17:49:35 +1200

    prettier

--- client/src/components/generic/EventsCard/EventsCard.tsx
+++ client/src/components/generic/EventsCard/EventsCard.tsx
@@ -1,23 +1,23 @@
 interface EventsCardProps {
-    date: string
-    title: string
-    location: string
-    content: React.ReactNode
+  date: string
+  title: string
+  location: string
+  content: React.ReactNode
 }
 
 type props = EventsCardProps
 
-const EventsCard = ({date, title, location, content}: props) => {
-  return <div className="border-1-black h-full w-[760px] border">
-    <div className="">
-        {date}
-    </div>
-    <div>
+const EventsCard = ({ date, title, location, content }: props) => {
+  return (
+    <div className="border-1-black h-full w-[760px] border">
+      <div className="">{date}</div>
+      <div>
         <h2>{title}</h2>
+      </div>
+      <div>{location}</div>
+      <div>{content}</div>
     </div>
-    <div>{location}</div>
-    <div>{content}</div>
-  </div>
+  )
 }
 
 export default EventsCard
