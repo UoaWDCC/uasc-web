2a7b6df50546224aba6f6741150b1333125790ed
ZL <ray040406@gmail.com>
2024-07-13 17:36:55 +1200

    interface 4 props

--- client/src/components/generic/EventsCard/EventsCard.tsx
+++ client/src/components/generic/EventsCard/EventsCard.tsx
@@ -1,3 +1,10 @@
+interface EventsCardProps {
+    date: string
+    title: string
+    location: string
+    content: React.ReactNode
+}
+
 const EventsCard = () => {
   return <div className="border-1-black h-full w-[760px] border">card</div>
 }
