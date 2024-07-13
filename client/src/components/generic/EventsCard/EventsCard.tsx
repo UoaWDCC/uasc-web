229b884103e612f3e67114bce63bf3d6f5585efc
ZL <ray040406@gmail.com>
2024-07-13 17:58:19 +1200

    spacing

--- client/src/components/generic/EventsCard/EventsCard.tsx
+++ client/src/components/generic/EventsCard/EventsCard.tsx
@@ -8,7 +8,7 @@ interface EventsCardProps {
 type props = EventsCardProps
 
 const Divider = () => {
-  return <div className="bg-gray-3 h-[1px] w-full"></div>
+  return <div className="bg-gray-3 mb-4 mt-4 h-[1px] w-full"></div>
 }
 
 const EventsCard = ({ date, title, location, content }: props) => {
@@ -16,11 +16,13 @@ const EventsCard = ({ date, title, location, content }: props) => {
     <div className="border-1-black h-full w-[760px] border">
       <div className="m-4">
         <div className="">{date}</div>
+
         <div className="">
           <h3 className="text-dark-blue-100 font-bold">{title}</h3>
         </div>
-        <Divider />
         <div>{location}</div>
+        <Divider />
+
         <div>{content}</div>
       </div>
     </div>
