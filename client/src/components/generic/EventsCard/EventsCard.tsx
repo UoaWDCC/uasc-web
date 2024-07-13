50944bbe5d5003b2ac28bd59713a81216b27713e
ZL <ray040406@gmail.com>
2024-07-13 18:36:24 +1200

    fixed according to BENSON's comment

--- client/src/components/generic/EventsCard/EventsCard.tsx
+++ client/src/components/generic/EventsCard/EventsCard.tsx
@@ -5,14 +5,12 @@ interface EventsCardProps {
   title: string
   location: string
   content: React.ReactNode
+  onClick: () => void
 }
 
 type props = EventsCardProps
 
-const EventsCard = ({ date, title, location, content }: props) => {
-  const handleOnClick = () => {
-    console.log("Button has been clicked")
-  }
+const EventsCard = ({ date, title, location, content, onClick }: props) => {
   const Divider = () => {
     return <div className="bg-gray-3 mb-4 mt-4 h-[1px] w-full"></div>
   }
@@ -29,7 +27,7 @@ const EventsCard = ({ date, title, location, content }: props) => {
         <div className="text-left">{content}</div>
       </div>
       <div className="mx-4 my-8">
-        <Button onClick={handleOnClick}>Sign Up!</Button>
+        <Button onClick={onClick}>Sign Up!</Button>
       </div>
     </div>
   )
