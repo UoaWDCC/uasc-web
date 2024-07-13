5062e227aa2c45c83ca7f341269041b7453eff6a
ZL <ray040406@gmail.com>
2024-07-13 17:51:57 +1200

    title style

--- client/src/components/generic/EventsCard/EventsCard.tsx
+++ client/src/components/generic/EventsCard/EventsCard.tsx
@@ -11,8 +11,8 @@ const EventsCard = ({ date, title, location, content }: props) => {
   return (
     <div className="border-1-black h-full w-[760px] border">
       <div className="">{date}</div>
-      <div>
-        <h2>{title}</h2>
+      <div className="">
+        <h3 className="text-dark-blue-100 font-bold">{title}</h3>
       </div>
       <div>{location}</div>
       <div>{content}</div>
