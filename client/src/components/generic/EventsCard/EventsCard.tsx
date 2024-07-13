a87170b57778f21b8ff4f69588f16208ec4f010c
ZL <ray040406@gmail.com>
2024-07-13 17:56:38 +1200

    divider made

--- client/src/components/generic/EventsCard/EventsCard.tsx
+++ client/src/components/generic/EventsCard/EventsCard.tsx
@@ -7,6 +7,10 @@ interface EventsCardProps {
 
 type props = EventsCardProps
 
+const Divider = () => {
+  return <div className="h-1 w-full bg-black"></div>
+}
+
 const EventsCard = ({ date, title, location, content }: props) => {
   return (
     <div className="border-1-black h-full w-[760px] border">
@@ -15,6 +19,7 @@ const EventsCard = ({ date, title, location, content }: props) => {
         <div className="">
           <h3 className="text-dark-blue-100 font-bold">{title}</h3>
         </div>
+        <Divider />
         <div>{location}</div>
         <div>{content}</div>
       </div>
