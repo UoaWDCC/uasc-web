a41f47088e311c3165db578fe9b30d3266913562
bcho892 <AKLbc70@gmail.com>
2024-07-14 17:04:29 +1200

    update metadata

--- client/src/app/contact/page.tsx
+++ client/src/app/contact/page.tsx
@@ -4,6 +4,12 @@ import {
   CONTACT_DETAIL_GROQ_QUERY,
   ContactDetailItem
 } from "@/models/sanity/ContactDetail/Utils"
+import { Metadata } from "next"
+
+export const metadata: Metadata = {
+  title: "Contact UASC",
+  description: "Information about how to reach UASC"
+}
 
 const Page = async () => {
   const contactDetails = await sanityQuery<ContactDetailItem[]>(
