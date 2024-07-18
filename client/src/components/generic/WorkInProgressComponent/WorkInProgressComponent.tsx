9a10f488e8ccf814ed68a64b28c373e739c637bb
ZL <ray040406@gmail.com>
2024-07-18 22:18:56 +1200

    space styling

--- client/src/components/generic/WorkInProgressComponent/WorkInProgressComponent.tsx
+++ client/src/components/generic/WorkInProgressComponent/WorkInProgressComponent.tsx
@@ -2,10 +2,12 @@ import Lock from "@/assets/icons/lock.svg"
 import Link from "next/link"
 const WorkInProgressComponent = () => {
   return (
-    <div>
+    <div className="flex w-full flex-col items-center justify-center gap-4">
       <Lock />
       <h2>Events Page Unavailable</h2>
-      <Link href="https://www.facebook.com/UoAsnowsports" />
+      <Link href="https://www.facebook.com/UoAsnowsports">
+        Go to our Facebook events page
+      </Link>
     </div>
   )
 }
