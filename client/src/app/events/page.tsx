91f5fa15cb4a2ff30fe422009398fe47c9b4718d
ZL <ray040406@gmail.com>
2024-07-18 22:23:58 +1200

    styling, new icon, hover effect

--- client/src/app/events/page.tsx
+++ client/src/app/events/page.tsx
@@ -1,7 +1,7 @@
 import WorkInProgressComponent from "@/components/generic/WorkInProgressComponent/WorkInProgressComponent"
 const Events = () => {
   return (
-    <div className="flex w-full items-center justify-center">
+    <div className="flex h-screen items-center justify-center">
       <WorkInProgressComponent />
     </div>
   )
--- client/src/components/generic/WorkInProgressComponent/WorkInProgressComponent.tsx
+++ client/src/components/generic/WorkInProgressComponent/WorkInProgressComponent.tsx
@@ -3,9 +3,12 @@ import Link from "next/link"
 const WorkInProgressComponent = () => {
   return (
     <div className="flex w-full flex-col items-center justify-center gap-4">
-      <Lock />
-      <h2>Events Page Unavailable</h2>
-      <Link href="https://www.facebook.com/UoAsnowsports">
+      <Lock className="fill-dark-blue-100" />
+      <h2 className="italic">Events Page Unavailable</h2>
+      <Link
+        href="https://www.facebook.com/UoAsnowsports"
+        className="text-light-blue-100 hover:underline"
+      >
         Go to our Facebook events page
       </Link>
     </div>
