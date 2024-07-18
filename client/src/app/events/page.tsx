253f6a90955a71fb5a35053fbc880eea78ea7ad5
ZL <ray040406@gmail.com>
2024-07-18 22:36:42 +1200

    reusable component complete by separating link

--- client/src/app/events/page.tsx
+++ client/src/app/events/page.tsx
@@ -1,8 +1,15 @@
 import WorkInProgressComponent from "@/components/generic/WorkInProgressComponent/WorkInProgressComponent"
+import Link from "next/link"
 const Events = () => {
   return (
-    <div className="fixed flex h-screen w-full items-center justify-center">
+    <div className="fixed flex h-screen w-full flex-col items-center justify-center gap-4">
       <WorkInProgressComponent pageName="Events" />
+      <Link
+        href="https://www.facebook.com/UoAsnowsports"
+        className="text-light-blue-100 hover:underline"
+      >
+        Go to our Facebook to sign up for the events!
+      </Link>
     </div>
   )
 }
--- client/src/components/generic/WorkInProgressComponent/WorkInProgressComponent.tsx
+++ client/src/components/generic/WorkInProgressComponent/WorkInProgressComponent.tsx
@@ -1,5 +1,5 @@
 import Lock from "@/assets/icons/lock.svg"
-import Link from "next/link"
+
 interface WorkInProgressComponentProps {
   pageName: string
 }
@@ -10,12 +10,6 @@ const WorkInProgressComponent = ({ pageName }: props) => {
     <div className="flex w-full flex-col items-center justify-center gap-4">
       <Lock className="fill-dark-blue-100" />
       <h2 className="italic">{pageName} Page Unavailable</h2>
-      <Link
-        href="https://www.facebook.com/UoAsnowsports"
-        className="text-light-blue-100 hover:underline"
-      >
-        Go to our Facebook to sign up for the events!
-      </Link>
     </div>
   )
 }
