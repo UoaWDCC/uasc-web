456ba0371556c8406c4e746f97385c46428b07f9
Jeffery <61447509+jeffplays2005@users.noreply.github.com>
2024-07-19 14:50:32 +1200

    607 create an endpoint to demote non admin users (#643)
    * 607 create an endpoint to demote non admin users
    
    Created endpoint in AdminController
    Created new method in AuthService and its corresponding test, this method fetches all users(1000+)
    
    Created endpoint tests, the endpoint isn't tested on 1000+ members as the fetch users method is confirmed to be working with 1000+ in the tests for AuthService.
    
    * Edit AuthService test
    
    * Update from delete to patch
    
    * Update AuthService and tests
    
    Include a maxPagintion option.

--- server/src/middleware/__generated__/routes.ts
+++ server/src/middleware/__generated__/routes.ts
@@ -1125,6 +1125,36 @@ export function RegisterRoutes(app: Router) {
             }
         });
         // WARNING: This file was auto-generated with tsoa. Please do not modify it. Re-run tsoa to re-generate this file: https://github.com/lukeautry/tsoa
+        app.patch('/admin/users/demote-all',
+            authenticateMiddleware([{"jwt":["admin"]}]),
+            ...(fetchMiddlewares<RequestHandler>(AdminController)),
+            ...(fetchMiddlewares<RequestHandler>(AdminController.prototype.demoteAllUsers)),
+
+            function AdminController_demoteAllUsers(request: ExRequest, response: ExResponse, next: any) {
+            const args: Record<string, TsoaRoute.ParameterSchema> = {
+            };
+
+            // WARNING: This file was auto-generated with tsoa. Please do not modify it. Re-run tsoa to re-generate this file: https://github.com/lukeautry/tsoa
+
+            let validatedArgs: any[] = [];
+            try {
+                validatedArgs = templateService.getValidatedArgs({ args, request, response });
+
+                const controller = new AdminController();
+
+              templateService.apiHandler({
+                methodName: 'demoteAllUsers',
+                controller,
+                response,
+                next,
+                validatedArgs,
+                successStatus: 200,
+              });
+            } catch (err) {
+                return next(err);
+            }
+        });
+        // WARNING: This file was auto-generated with tsoa. Please do not modify it. Re-run tsoa to re-generate this file: https://github.com/lukeautry/tsoa
         app.post('/admin/users/add-coupon',
             authenticateMiddleware([{"jwt":["admin"]}]),
             ...(fetchMiddlewares<RequestHandler>(AdminController)),
