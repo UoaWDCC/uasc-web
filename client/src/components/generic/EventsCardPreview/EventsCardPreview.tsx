565543a17d82f96f7c0d3da12563a7f200b46864
ZL <ray040406@gmail.com>
2024-07-19 19:21:53 +1200

    structure

--- client/src/components/generic/EventsCardPreview/EventsCardPreview.tsx
+++ client/src/components/generic/EventsCardPreview/EventsCardPreview.tsx
@@ -1,6 +1,11 @@
 // 4 props: 3 string, 1 image
+
 const EventsCardPreview = () => {
-  return <div>events card preview </div>
+  return (
+    <div className="border-gray-3 flex w-full items-center justify-center border">
+      events card preview
+    </div>
+  )
 }
 
 export default EventsCardPreview
