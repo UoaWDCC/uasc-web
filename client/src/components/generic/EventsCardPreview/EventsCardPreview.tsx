a7cdd397ca3379507cce7f9d335d9828050d3124
ZL <ray040406@gmail.com>
2024-07-19 19:35:46 +1200

    props address

--- client/src/components/generic/EventsCardPreview/EventsCardPreview.tsx
+++ client/src/components/generic/EventsCardPreview/EventsCardPreview.tsx
@@ -1,14 +1,16 @@
 // 4 props: 3 string, 1 image
 import { EventsCardProps } from "../EventsCard/EventsCard"
 type props = EventsCardProps
-const EventsCardPreview = () => {
+const EventsCardPreview = ({ date, title, location }: props) => {
   return (
     <div className="border-gray-3 flex w-full items-center justify-center border p-14">
-      <div className="border-gray-3 border px-20 py-11">image</div>
+      <div className="border-gray-3 border px-20 py-11">
+        <p>Temporary Placeholder for image</p>
+      </div>
       <div className="mr-auto flex flex-col">
-        <h5>date and time</h5>
-        <h2>title</h2>
-        <p>location</p>
+        <h5>{date}</h5>
+        <h2>{title}</h2>
+        <p>{location}</p>
       </div>
     </div>
   )
