ed141263aebf018f0d965d461621719619ebfca1
ZL <ray040406@gmail.com>
2024-07-19 19:25:52 +1200

    frame

--- client/src/components/generic/EventsCardPreview/EventsCardPreview.tsx
+++ client/src/components/generic/EventsCardPreview/EventsCardPreview.tsx
@@ -2,8 +2,13 @@
 
 const EventsCardPreview = () => {
   return (
-    <div className="border-gray-3 flex w-full items-center justify-center border">
-      events card preview
+    <div className="border-gray-3 flex w-full items-center justify-center border p-14">
+      <div className="border-gray-3 border">image</div>
+      <div className="flex flex-col">
+        <h5>date and time</h5>
+        <h2>title</h2>
+        <p>location</p>
+      </div>
     </div>
   )
 }
