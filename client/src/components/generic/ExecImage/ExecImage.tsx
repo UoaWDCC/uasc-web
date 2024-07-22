105d8a68370cdd7a02d22ded7d79e73cf21a0be3
AzizP786 <m.azizp23@gmail.com>
2024-07-22 13:49:07 +1200

    adjusted colour css

--- client/src/components/generic/ExecImage/ExecImage.tsx
+++ client/src/components/generic/ExecImage/ExecImage.tsx
@@ -1,6 +1,5 @@
 import React from "react"
 import Image from "next/image"
-// import "./ExecImage.css"
 
 export interface ExecImageProps {
   src: string
@@ -19,7 +18,7 @@ const ExecImage: React.FC<ExecImageProps> = ({ src, alt, title, name }) => {
         height={163}
         className="h-full w-full object-cover transition-opacity duration-300"
       />
-      <div className="absolute left-0 top-0 h-full w-full bg-[rgba(40,61,135,0.8)] opacity-0 transition-opacity duration-300 group-hover:opacity-100"></div>
+      <div className="absolute left-0 top-0 h-full w-full bg-dark-blue-100 opacity-0 transition-opacity duration-300 group-hover:opacity-80"></div>
       <div className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 transform text-center opacity-0 transition-opacity duration-300 group-hover:opacity-100">
         <p className="text-[12px] font-bold uppercase tracking-wider text-white">
           {title}
