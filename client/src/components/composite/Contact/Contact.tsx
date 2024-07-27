44a011a1f61d5bf68e4438d642556b4d0c94dd07
zlrkw11 <64724157+zlrkw11@users.noreply.github.com>
2024-07-27 21:32:55 +1200

    Google map widget on contact page (#693)
    * implementation
    
    * fixed address

--- client/src/components/composite/Contact/Contact.tsx
+++ client/src/components/composite/Contact/Contact.tsx
@@ -13,6 +13,14 @@ const Contact = ({ items }: IContact) => {
         <div className="flex w-full max-w-[800px] flex-col gap-4 py-4">
           <h2 className="text-dark-blue-100 italic">Contact Us</h2>
           <ContactDetailPanel items={items} />
+          <div className="w-full">
+            <iframe
+              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3090.3296850757993!2d175.5528169760526!3d-39.235387071653925!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x6d6b01d610042f1d%3A0x1071f4930186b3c0!2sUniversity%20of%20Auckland%20Snowsports%20Club!5e0!3m2!1sen!2snz!4v1722071878514!5m2!1sen!2snz"
+              width="100%"
+              height="450"
+              loading="lazy"
+            ></iframe>
+          </div>
         </div>
       </FullPageBackgroundImage>
       <Footer />
