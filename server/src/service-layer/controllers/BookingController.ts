464656cbe2a90b49ec49a8a387492c28a75e1304
Jeffery <61447509+jeffplays2005@users.noreply.github.com>
2024-07-30 11:32:08 +1200

    608 Backend send confirmation on manual booking (#718)
    * Change /bookings/create-booking to take a single userid
    
    Changed tests and updated interfaces
    
    * Move create-bookings from BookingController to AdminController
    
    Moved corresponding tests too.
    
    Next objective is to update frontend
    
    * Change client to use /admin/bookings/create
    
    Changed client end to use the endpoint specified instead of /bookings/create-bookings
    
    * Edit AdminController to use an Array of dates & users
    
    Revert change to `UIdssByDateRangeResponse` interface
    
    * Add mail service for manual bookings
    
    Also fixed some weird conflicts
    
    * Add jest mock for nodemailer
    
    This fixes the error while running `yarn test`

--- server/src/service-layer/controllers/BookingController.ts
+++ server/src/service-layer/controllers/BookingController.ts
@@ -1,17 +1,13 @@
 import {
   AvailableDatesRequestModel,
-  BookingsByDateRangeRequestModel,
-  CreateBookingsRequestModel
+  BookingsByDateRangeRequestModel
 } from "service-layer/request-models/UserRequests"
 import { AvailableDatesResponse } from "service-layer/response-models/PaymentResponse"
 import { Timestamp } from "firebase-admin/firestore"
 
 import BookingDataService from "data-layer/services/BookingDataService"
 import BookingSlotService from "data-layer/services/BookingSlotsService"
-import {
-  AllUserBookingSlotsResponse,
-  UIdssByDateRangeResponse
-} from "service-layer/response-models/BookingResponse"
+import { AllUserBookingSlotsResponse } from "service-layer/response-models/BookingResponse"
 import { AllUserBookingsRequestBody } from "service-layer/request-models/BookingRequests"
 import {
   Controller,
@@ -42,87 +38,6 @@ import BookingUtils from "business-layer/utils/BookingUtils"
 
 @Route("bookings")
 export class BookingController extends Controller {
-  /**
-   * An admin method to create bookings for a list of users within a date range.
-   * @param requestBody - The date range and list of user ids to create bookings for.
-   * @returns A list of users and timestamps that were successfully added to the booking slots.
-   */
-  @SuccessResponse("200", "Bookings successfully created")
-  @Security("jwt", ["admin"])
-  @Post("create-bookings")
-  public async createBookings(
-    @Body() requestBody: CreateBookingsRequestModel
-  ): Promise<UIdssByDateRangeResponse> {
-    try {
-      const { startDate, endDate } = requestBody
-
-      /** Creating instances of the required services */
-      const bookingSlotService = new BookingSlotService()
-      const bookingDataService = new BookingDataService()
-
-      // Query to get all booking slots within date range
-      const bookingSlots =
-        await bookingSlotService.getBookingSlotsBetweenDateRange(
-          startDate,
-          endDate
-        )
-
-      /** The response data array */
-      const responseData: Array<{
-        date: Timestamp
-        users: string[]
-      }> = []
-
-      /** Iterating through each booking slot */
-      const bookingPromises = bookingSlots.map(async (slot) => {
-        let userIds = [...requestBody.userIds]
-        /** For every slotid add a booking for that id only if user doesn't already have a booking */
-        const userIdsPromises = userIds.map(async (userId) => {
-          const existingBookingsForUser =
-            await bookingDataService.getBookingsByUserId(userId)
-          if (
-            existingBookingsForUser.some(
-              (booking) => booking.booking_slot_id === slot.id
-            )
-          ) {
-            userIds = userIds.filter((id) => id !== userId) // Remove user from list if they already have a booking
-          } else {
-            await bookingDataService.createBooking({
-              user_id: userId,
-              booking_slot_id: slot.id,
-              stripe_payment_id: "manual_entry"
-            })
-          }
-        })
-
-        /** List of usersIds successfully added */
-        responseData.push({
-          date: slot.date,
-          users: userIds
-        })
-
-        await Promise.all(userIdsPromises)
-      })
-
-      await Promise.all(bookingPromises)
-
-      this.setStatus(200)
-
-      /**
-       * Returning the response data
-       *
-       * The filter is required to not include data that is null
-       * because of the early return in the map
-       */
-      return { data: responseData.filter((data) => !!data) }
-    } catch (e) {
-      console.error("Error in getBookingsByDateRange:", e)
-      this.setStatus(500)
-
-      return { error: "Something went wrong" }
-    }
-  }
-
   /**
    * Fetches all bookings for a user based on their UID.
    * @param request - The request object that includes the UserRecord.
