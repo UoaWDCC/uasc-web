afd1496ad24259101b54cac16b07b1cd1f8fa825
Troy <97351173+tmacsmee@users.noreply.github.com>
2024-08-06 15:23:15 +1200

    Restyle landing section to be consistent with Figma (#743)
    * feat: instagram icon in navbar
    
    * feat: video hero and restore navbar
    
    * feat: about section buttons
    
    * feat: benefit section cards
    
    * feat: member pricing touchups and navbar instagram link
    
    * fix: restore gear spinner transform origin
    
    * feat: increase pricing card height
    
    * fix: instagram button link, landing desc. font weight
    
    * fix: instagram link, feat: video compression and blurring
    
    * feat: video compression and placeholder

--- client/src/app/sections/BenefitSection.tsx
+++ client/src/app/sections/BenefitSection.tsx
@@ -1,5 +1,5 @@
-import MemberBenefitCard from "@/components/generic/MemberBenefitCards/MemberBenefitCard"
 import { Benefit } from "@/components/utils/types"
+import Image from "next/image"
 import HomeSectionHeading from "./utils/HomeSectionHeading"
 import HomeSectionWrapper from "./utils/HomeSectionWrapper"
 
@@ -11,13 +11,21 @@ const BenefitSection = ({ benefits }: IBenefitSection) => (
   <section>
     <HomeSectionWrapper>
       <HomeSectionHeading text="Member Benefits" />
-      <div className="mt-3 grid grid-cols-1 items-center gap-7 lg:grid-cols-2">
+      <div className="mt-16 grid grid-cols-1 items-center gap-12 sm:grid-cols-2 sm:px-6 lg:px-0">
         {benefits.map((benefit) => {
           const { text, image } = benefit
           return (
-            <span key={text} className="flex h-full w-full justify-center">
-              <MemberBenefitCard imageSrc={image} text={text} />
-            </span>
+            <div key={text} className="relative isolate h-64 w-full">
+              <Image
+                src={image || ""}
+                alt={text}
+                fill
+                className="-z-10 size-full object-cover"
+              />
+              <span className="bg-light-blue-100 absolute -top-6 w-fit max-w-[80%] px-6 py-4 text-lg font-bold text-white sm:-left-6">
+                {text}
+              </span>
+            </div>
           )
         })}
       </div>
