00a74d3040b1e5c3a61710fea71dd85be733f048
bcho892 <AKLbc70@gmail.com>
2024-08-19 11:59:10 +1200

    add undefined guard

--- client/src/app/bookings/page.tsx
+++ client/src/app/bookings/page.tsx
@@ -15,18 +15,18 @@ const BookingPage = async () => {
    * We assume there will be only one based on the way {@link LodgeInformation}
    * is set up in sanity
    */
-  const lodgeInfoSingleton = lodgeInfo[0]
+  const lodgeInfoSingleton: LodgeInformation | undefined = lodgeInfo[0]
 
   const RenderedContent = () => {
     return (
-      lodgeInfoSingleton.information && (
+      lodgeInfoSingleton?.information && (
         <PortableText value={lodgeInfoSingleton.information} />
       )
     )
   }
 
   const processedImages =
-    lodgeInfoSingleton.imageUrls?.map((item) =>
+    lodgeInfoSingleton?.imageUrls?.map((item) =>
       item.url
         ? new SanityImageUrl(item.url).autoFormat().width(700).toString()
         : ""
