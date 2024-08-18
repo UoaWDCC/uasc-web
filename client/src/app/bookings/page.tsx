ea8b81605b6c0663f14b69cbb0d1ae241e539f60
Benson Cho <100653148+bcho892@users.noreply.github.com>
2024-08-19 00:38:20 +1200

    660 frontend put lodge schemas on bookings page (#763)
    * create basic story and implement component on page
    
    * fetch data from sanity
    
    * update docs

--- client/src/app/bookings/page.tsx
+++ client/src/app/bookings/page.tsx
@@ -1,11 +1,46 @@
-"use client"
+import BookingInformationAndCreation from "@/components/composite/Booking/BookingInformationAndCreation/BookingInformationAndCreation"
+import { SanityImageUrl, sanityQuery } from "../../../sanity/lib/utils"
+import {
+  LODGE_INFORMATION_GROQ_QUERY,
+  LodgeInformation
+} from "@/models/sanity/LodgeInfo/Utils"
+import { PortableText } from "@portabletext/react"
 
-import { ProtectedCreateBookingSection } from "@/components/composite/Booking/BookingCreation/ProtectedCreateBookingSection"
+const BookingPage = async () => {
+  const lodgeInfo = await sanityQuery<LodgeInformation[]>(
+    LODGE_INFORMATION_GROQ_QUERY
+  )
 
-const BookingPage = () => {
+  /**
+   * We assume there will be only one based on the way {@link LodgeInformation}
+   * is set up in sanity
+   */
+  const lodgeInfoSingleton = lodgeInfo[0]
+
+  const RenderedContent = () => {
+    return (
+      lodgeInfoSingleton.information && (
+        <PortableText value={lodgeInfoSingleton.information} />
+      )
+    )
+  }
+
+  const processedImages =
+    lodgeInfoSingleton.imageUrls?.map((item) =>
+      item.url
+        ? new SanityImageUrl(item.url).autoFormat().width(700).toString()
+        : ""
+    ) || []
+
   return (
     <>
-      <ProtectedCreateBookingSection />
+      <BookingInformationAndCreation
+        enableNetworkRequests
+        lodgeInfoProps={{
+          children: <RenderedContent />,
+          images: processedImages
+        }}
+      />
     </>
   )
 }
--- client/src/models/sanity/LodgeInfo/Utils.ts
+++ client/src/models/sanity/LodgeInfo/Utils.ts
@@ -1,2 +1,13 @@
+import { PortableTextBlock } from "@portabletext/types"
+
 export const LODGE_INFORMATION_GROQ_QUERY =
-  `[_type == "lodge-information"]` as const
+  `*[_type == "lodge-information"]{"imageUrls": images[]{"url": asset->url}, ...}` as const
+
+type LodgeInformationImage = {
+  url?: string
+}
+
+export type LodgeInformation = {
+  imageUrls?: LodgeInformationImage[]
+  information?: PortableTextBlock[]
+}
