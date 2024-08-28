ff43a7952ec7e28a6caab8ad57e1f5e65b8fc02c
bcho892 <AKLbc70@gmail.com>
2024-08-29 10:23:12 +1200

    improve docs and add the handler to book the lodge button

--- client/src/components/composite/LodgeInfo/LodgeInfoComponent/LodgeInfoComponent.tsx
+++ client/src/components/composite/LodgeInfo/LodgeInfoComponent/LodgeInfoComponent.tsx
@@ -2,17 +2,35 @@ import Button from "@/components/generic/FigmaButtons/FigmaButton"
 import { ReactNode } from "react"
 
 interface ILodgeInfoComponent {
+  /**
+   * The **pre-rendered** children that should be displayed in the the container
+   * (This should generally be text content)
+   */
   children: ReactNode
+  /**
+   * Handler to be called when call to action button is clicked
+   *
+   * @example
+   * () => navigateToActualBookLodgeScreen()
+   */
   handleBookLodgeClick: () => void
 }
 
-const LodgeInfoComponent = ({ children }: ILodgeInfoComponent) => {
+/**
+ * @deprecated do not consume directly on page, use `LodgeInfo` instead
+ */
+const LodgeInfoComponent = ({
+  children,
+  handleBookLodgeClick
+}: ILodgeInfoComponent) => {
   return (
-    <div className="border-gray-3 flex h-full w-full flex-col justify-center rounded border bg-white px-2 py-12 pb-8 sm:px-6">
-      <div className="text-dark-blue-100 flex flex-col gap-4 lg:justify-center">
+    <div className="border-gray-3 flex h-full w-full flex-col justify-center rounded border bg-white px-2 py-12 pb-8 sm:px-3">
+      <div className="text-dark-blue-100 mb-3 flex flex-col gap-4 lg:justify-center">
         {children}
       </div>
-      <Button variant="inverted-default-sm">Book the lodge</Button>
+      <Button variant="inverted-default-sm" onClick={handleBookLodgeClick}>
+        Book the lodge
+      </Button>
     </div>
   )
 }
--- client/src/components/composite/LodgeInfo/LodgeInfoGallery/LodgeInfoGallery.tsx
+++ client/src/components/composite/LodgeInfo/LodgeInfoGallery/LodgeInfoGallery.tsx
@@ -1,13 +1,24 @@
 import Image from "next/image"
 
 interface ILodgeInfoGallery {
+  /**
+   * A list of srcs for all the images in the gallery.
+   *
+   * This should be pre-sorted
+   *
+   * @example
+   * ['https://image-url-1.com', 'https://image-url-2.com', 'https://image-url-3.com']
+   */
   imageUrls: string[]
 }
 
+/**
+ * Simple photo gallery that displays images in a single row (can be scrolled horizontally)
+ */
 const LodgeInfoGallery = ({ imageUrls = [] }: ILodgeInfoGallery) => {
   return (
     <>
-      <div className="flex justify-center space-x-4 overflow-x-auto p-4">
+      <div className="flex space-x-4 overflow-x-auto p-2">
         {imageUrls.map((url, index) => (
           <div key={index} className="flex-shrink-0">
             <Image
