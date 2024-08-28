6af9d192dfc8dd72e382a0124d3eab2193179cb4
bcho892 <AKLbc70@gmail.com>
2024-08-29 10:29:23 +1200

    constrain image max width

--- client/src/components/composite/LodgeInfo/LodgeInfoGallery/LodgeInfoGallery.tsx
+++ client/src/components/composite/LodgeInfo/LodgeInfoGallery/LodgeInfoGallery.tsx
@@ -4,7 +4,7 @@ interface ILodgeInfoGallery {
   /**
    * A list of srcs for all the images in the gallery.
    *
-   * This should be pre-sorted
+   * This should be **pre-sorted** and **unique**
    *
    * @example
    * ['https://image-url-1.com', 'https://image-url-2.com', 'https://image-url-3.com']
@@ -20,15 +20,18 @@ const LodgeInfoGallery = ({ imageSrcs = [] }: ILodgeInfoGallery) => {
     <>
       <div className="flex space-x-4 overflow-x-auto p-2">
         {imageSrcs.map((url, index) => (
-          <div key={index} className="flex-shrink-0">
-            <Image
-              src={url}
-              alt={`Photo ${index + 1}`}
-              width={400}
-              height={400}
-              className="rounded-sm shadow-lg"
-            />
-          </div>
+          <>
+            {/* We require a `max-width` style to constrain the image on smaller screens */}
+            <div key={url} className="max-w-[95vw] flex-shrink-0">
+              <Image
+                src={url}
+                alt={`Photo ${index + 1}`}
+                width={400}
+                height={400}
+                className="rounded-sm shadow-lg"
+              />
+            </div>
+          </>
         ))}
       </div>
     </>
