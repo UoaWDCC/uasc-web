0fe13f1d69f85e39017e254f85587d2c91744492
zlrkw11 <ray040406@gmail.com>
2024-09-06 16:54:52 +1200

    bg image

--- client/src/components/composite/Events/Events.tsx
+++ client/src/components/composite/Events/Events.tsx
@@ -8,17 +8,22 @@ interface IEventsPage {
 
 const EventsPage = ({ events }: IEventsPage) => {
   return (
-    <div>
-      {events.map((event) => (
-        <EventsCard
-          date={event.date}
-          title={event.title}
-          location={event.location}
-          content={event.content}
-          onClick={event.onClick}
-          key={event.date}
-        />
-      ))}
+    <div
+      className="bg-mountain-background-image relative z-10 flex min-h-[100vh] w-fit
+      min-w-full flex-col items-center bg-cover bg-top bg-no-repeat md:px-8"
+    >
+      <div>
+        {events.map((event) => (
+          <EventsCard
+            date={event.date}
+            title={event.title}
+            location={event.location}
+            content={event.content}
+            onClick={event.onClick}
+            key={event.date}
+          />
+        ))}
+      </div>
     </div>
   )
 }
