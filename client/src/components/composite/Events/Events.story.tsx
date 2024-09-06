96ab55ccc56ba8b2f96610e856ce7c39a8b9a543
zlrkw11 <ray040406@gmail.com>
2024-09-06 16:11:48 +1200

    story structure set up

---
+++ client/src/components/composite/Events/Events.story.tsx
@@ -0,0 +1,12 @@
+import type { Meta, StoryObj } from "@storybook/react"
+import EventsPage from "./Events"
+
+const meta: Meta<typeof EventsPage> = {
+  component: EventsPage
+}
+export default meta
+type Story = StoryObj<typeof meta>
+
+export const DefaultEventsPage: Story = {
+  tags: ["autodocs"]
+}
