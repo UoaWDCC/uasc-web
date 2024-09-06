7f884b2973864c0fb13a7cf9fd87d064d072057b
zlrkw11 <ray040406@gmail.com>
2024-09-06 16:20:08 +1200

    mock data

--- client/src/components/composite/Events/Events.tsx
+++ client/src/components/composite/Events/Events.tsx
@@ -1,7 +1,32 @@
 import EventsCard from "@/components/generic/EventsCard/EventsCard"
 
+const eventsList = [
+  { date: "f", title: "cock", location: "w", content: "fu", onClick: () => {} },
+  {
+    date: "f",
+    title: "cock",
+    location: "w",
+    content: "gay",
+    onClick: () => {}
+  },
+  {
+    date: "f",
+    title: "cock",
+    location: "w",
+    content: "ray",
+    onClick: () => {}
+  },
+  {
+    date: "f",
+    title: "cock",
+    location: "w",
+    content: "league",
+    onClick: () => {}
+  }
+]
+
 const EventsPage = () => {
-  return <div>Fuck</div>
+  return <div></div>
 }
 
 export default EventsPage
