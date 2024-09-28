5791b19b46696b69ad9f616930562764c8a41c63
Kartik-M24 <161120979+Kartik-M24@users.noreply.github.com>
2024-09-28 15:55:13 +1200

    Fixed TabContent.story and ran yarn-dev-server

--- client/src/components/composite/BookingsPolicyTabs/TabsContent.story.tsx
+++ client/src/components/composite/BookingsPolicyTabs/TabsContent.story.tsx
@@ -5,7 +5,70 @@ const meta: Meta = {
   component: PolicyTabs
 }
 
+const PoliciesContent = [
+  {
+    order: 0,
+    title: "LODGE BOOKINGS",
+    information: [
+      {
+        _key: "1",
+        _type: "block",
+        children: [
+          {
+            _key: "2",
+            _type: "span",
+            marks: [],
+            text: "This is the lodge bookings policy"
+          }
+        ],
+        markDefs: [],
+        style: "normal"
+      }
+    ]
+  },
+  {
+    order: 1,
+    title: "CANCELLATION",
+    information: [
+      {
+        _key: "3",
+        _type: "block",
+        children: [
+          {
+            _key: "4",
+            _type: "span",
+            marks: [],
+            text: "This is the cancellation policy"
+          }
+        ],
+        markDefs: [],
+        style: "normal"
+      }
+    ]
+  },
+  {
+    order: 2,
+    title: "BEHAVIOUR",
+    information: [
+      {
+        _key: "5",
+        _type: "block",
+        children: [
+          {
+            _key: "6",
+            _type: "span",
+            marks: [],
+            text: "This is the behaviour policy"
+          }
+        ],
+        markDefs: [],
+        style: "normal"
+      }
+    ]
+  }
+]
+
 export default meta
 export const DefaultPolicyTabs = () => {
-  return <PolicyTabs />
+  return <PolicyTabs policiesArray={PoliciesContent} />
 }
