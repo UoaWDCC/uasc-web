cba9bc160b64cbfbec25c6eb567023e7c9c1d571
jeffplays2005 <jeffery20050811@gmail.com>
2024-10-02 16:59:17 +1300

    Remove views in events view
    Not implemented yet

--- client/src/app/admin/events/page.tsx
+++ client/src/app/admin/events/page.tsx
@@ -1,13 +1,11 @@
 "use client"
 
-import WrappedAdminMemberView from "@/components/composite/Admin/AdminMemberView/ProtectedAdminMemberView"
 import { AdminHeading } from "../AdminHeading"
 
 export default function AdminEventsPage() {
   return (
     <>
       <AdminHeading title="Events" />
-      <WrappedAdminMemberView />
     </>
   )
 }
