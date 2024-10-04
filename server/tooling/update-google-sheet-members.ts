f4e4f0c20826c52ffb6d425e7fd55c358c8c9434
jeffplays2005 <jeffery20050811@gmail.com>
2024-10-05 00:29:41 +1300

    Finalise logic for update google sheet members script
    * Create `fetchUsers` method that fetches users from the backend
    * Create `getAllUsers` that fetches all the users from the backend using `fetchUsers`
    * Create `authenticateGoogle` method that returns a google auth client
      * Note that documentation will come soon...
    * Create `appendToGoogleSheet` and `clearSheet`
      * `appendToGoogleSheet` takes in the logs and auth client and updates the google sheet rows
      * `clearSheet` clears the google sheet so that `appendToGoogleSheet` doesn't overlap everything
    
    Note that the logic is still somewhat rough and could have improvements. Quite a lot of duplicate code and possibly better ways to update the google sheet instead of just appending and clearing every time.

--- server/tooling/update-google-sheet-members.ts
+++ server/tooling/update-google-sheet-members.ts
@@ -1,8 +1,18 @@
+import { CombinedUserData } from "service-layer/response-models/UserResponse"
+import { firestoreTimestampToDate } from "data-layer/adapters/DateUtils"
+
+import { google } from "googleapis"
 import dotenv from "dotenv"
 dotenv.config()
 
 const args = process.argv.slice(2)
 
+/**
+ * Fetches users from the backend
+ * @param token - The token to authenticate the request
+ * @param cursor - The cursor to fetch the next page of users
+ * @returns The data json object from the response
+ */
 async function fetchUsers(token: string, cursor?: string): Promise<any> {
   const res = await fetch(
     // Note that VITE_BACKEND_BASE_URL does have a slash at the end
@@ -23,8 +33,13 @@ async function fetchUsers(token: string, cursor?: string): Promise<any> {
   }
 }
 
-async function getAllUsers(token: string): Promise<any[]> {
-  const allUsers = []
+/**
+ * Fetches all users from the backend
+ * @param token - The token to authenticate the request
+ * @returns An array of all users
+ */
+async function getAllUsers(token: string): Promise<CombinedUserData[]> {
+  const allUsers: CombinedUserData[] = []
   let fetchedUsers = await fetchUsers(token)
   allUsers.push(...fetchedUsers.data)
   while (fetchedUsers.cursor) {
@@ -34,14 +49,127 @@ async function getAllUsers(token: string): Promise<any[]> {
   return allUsers
 }
 
+/**
+ * Creates google authentication client
+ * @returns The google auth client
+ */
+async function authenticateGoogle(): Promise<any> {
+  const { client_email, private_key } = JSON.parse(process.env.GOOGLE_API_JSON)
+  const auth = new google.auth.GoogleAuth({
+    credentials: {
+      client_email,
+      private_key
+    },
+    scopes: ["https://www.googleapis.com/auth/spreadsheets"]
+  })
+
+  const authClient = await auth.getClient()
+  return authClient
+}
+
+/**
+ * Append data to Google Sheets using API Key
+ * @param auth - The google auth client
+ * @param rows - The rows to append to the Google Sheet
+ * @returns The response from the Google Sheets API
+ */
+async function updateGoogleSheet(auth: any, rows: any[]) {
+  // Clear sheet first
+  await clearSheet(auth)
+
+  const sheets = google.sheets({
+    version: "v4",
+    auth
+  })
+
+  const request = {
+    spreadsheetId: process.env.GOOGLE_SHEET_ID,
+    range: "Sheet1!A1", // Adjust to your sheet and cell range
+    valueInputOption: "RAW",
+    insertDataOption: "INSERT_ROWS",
+    resource: {
+      values: rows
+    }
+  }
+
+  try {
+    const response = (await sheets.spreadsheets.values.append(request)).data
+    console.log(`${rows.length} rows added to the google sheet.`)
+    return response
+  } catch (err) {
+    return console.error("Failed to add rows to the google sheet.", err)
+  }
+}
+
+/**
+ * Clears google sheet data.
+ * @param auth - The google auth client
+ */
+async function clearSheet(auth: any) {
+  const sheets = google.sheets({
+    version: "v4",
+    auth
+  })
+
+  const request = {
+    spreadsheetId: process.env.GOOGLE_SHEET_ID,
+    range: "Sheet1" // Adjust to your sheet and cell range
+  }
+  try {
+    await sheets.spreadsheets.values.clear(request)
+    console.log(`Cleared google sheet.`)
+  } catch (err) {
+    console.error("Failed to clear the Google Sheet", err)
+  }
+}
+
 if (args.length === 0) {
   console.log("Token required to update google sheet members.")
 } else {
   const token = args[0]
 
   getAllUsers(token)
-    .then(async (allUsers: any) => {
-      console.log(allUsers)
+    .then(async (allUsers: CombinedUserData[]) => {
+      const rows = allUsers.map((user: CombinedUserData) => [
+        user.email,
+        user.first_name,
+        user.last_name,
+        user.phone_number,
+        firestoreTimestampToDate(user.date_of_birth).toISOString(),
+        user.membership,
+        user.dateJoined,
+        user.university_year,
+        user.gender,
+        user.does_racing,
+        user.does_ski,
+        user.does_snowboarding,
+        user.emergency_contact,
+        // Omit stripe id
+        user.dietary_requirements,
+        user.uid
+      ])
+      const categories = [
+        "Email",
+        "First name",
+        "Last name",
+        "Phone number",
+        "Date of birth",
+        "Membership",
+        "Date joined",
+        "University year",
+        "Gender",
+        "Does racing",
+        "Does ski",
+        "Does snowboarding",
+        "Emergency contact",
+        "Dietary requirements",
+        "UID"
+      ]
+      const auth = await authenticateGoogle()
+      await updateGoogleSheet(auth, [categories, ...rows])
+    })
+    .then(() => {
+      console.log("Successfully updated google sheet members.")
     })
     .catch((e) => {
       console.error(e)
