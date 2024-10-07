7a59dd7f589e7efd56c911fe9a80bbeb056df475
jeffplays2005 <jeffery20050811@gmail.com>
2024-10-07 15:06:22 +1300

    Remove all code related to event signups
    * Deleted event signup endpoint
    * Deleted SSE endpoint

--- server/src/service-layer/response-models/EventResponse.ts
+++ server/src/service-layer/response-models/EventResponse.ts
@@ -1,14 +1,6 @@
 import { CommonResponse, CursorPaginatedResponse } from "./CommonResponse"
 import { Event } from "data-layer/models/firebase"
 
-export interface EventSignupResponse extends CommonResponse {
-  data?: {
-    first_name: string
-    last_name: string
-    email: string
-  }
-}
-
 export interface GetAllEventsResponse
   extends CommonResponse,
     CursorPaginatedResponse {
