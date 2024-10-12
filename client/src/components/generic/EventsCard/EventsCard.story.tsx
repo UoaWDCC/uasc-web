643f4d2e48f30e44d8679cf23b69f893967738ae
ZL <ray040406@gmail.com>
2024-10-12 22:57:55 +1300

    storybook image added

--- client/src/components/generic/EventsCard/EventsCard.story.tsx
+++ client/src/components/generic/EventsCard/EventsCard.story.tsx
@@ -15,7 +15,7 @@ export const DefaultEventsCard: Story = {
     date: "THU 18/7 • 6pm",
     location: "Shadows Bar - 8 Alfred St, CBD, Auckland",
     content: `Get ready to kick off Re-O-Week with a bang!  Join us at Shadows Bar for the ultimate Outdoor Clubs Welcome Party, hosted by UoA Snowsports Club! Whether you're into hiking, biking, skiing, or just having a good time, this is the place to be!`,
-    image: ""
+    image: testImg.src
   }
 }
 
@@ -25,7 +25,8 @@ export const DefaultEventsCard2: Story = {
     title: "goon",
     date: "...",
     location: "goon cave",
-    content: `piss`
+    content: `piss`,
+    image: testImg.src
   }
 }
 
