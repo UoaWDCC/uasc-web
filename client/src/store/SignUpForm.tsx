71f67c45107c1f269efae0314d1602dbf0adefa3
bcho892 <AKLbc70@gmail.com>
2025-03-04 12:07:58 +1300

    Revert "adding regex for .com (#812)"
    This reverts commit 969768ed0ef78b3a9cd907bab5ef595ee471da3b.

--- client/src/store/SignUpForm.tsx
+++ client/src/store/SignUpForm.tsx
@@ -64,7 +64,7 @@ const actions = {
         const { email, confirmEmail, phone_number, emergency_contact } =
           getState()
         const validRegex =
-          /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-]{2,}$/
+          /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/
 
         if (!email.match(validRegex)) {
           invalidFields.push("Email")
